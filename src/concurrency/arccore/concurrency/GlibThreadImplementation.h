3b432e24ded8a373bb883ad23bdfd51924c44ee1
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2018-08-21 11:05:22 +0200

    [arccore] Déplace 'GlibThreadImplementation' dans 'arccore_concurrency'.

---
+++ src/concurrency/arccore/concurrency/GlibThreadImplementation.h
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------*/
+/* GlibThreadImplementation.h                                  (C) 2000-2018 */
+/*                                                                           */
+/* Implémentation de ITreadImplementation avec la 'Glib'.                    */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCCORE_CONCURRENCY_GLIBTHREADIMPLEMENTATION_H
+#define ARCCORE_CONCURRENCY_GLIBTHREADIMPLEMENTATION_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/concurrency/IThreadImplementation.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore
+{
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Implémentation de ITreadImplementation avec la 'Glib'.
+ */
+class ARCCORE_CONCURRENCY_EXPORT GlibThreadImplementation
+: public IThreadImplementation
+{
+ public:
+  GlibThreadImplementation();
+  ~GlibThreadImplementation() override;
+ public:
+  void initialize() override;
+ public:
+  ThreadImpl* createThread(IFunctor* f) override;
+  void joinThread(ThreadImpl* t) override;
+  void destroyThread(ThreadImpl* t) override;
+
+  void createSpinLock(Int64* spin_lock_addr) override;
+  void lockSpinLock(Int64* spin_lock_addr,Int64* scoped_spin_lock_addr) override;
+  void unlockSpinLock(Int64* spin_lock_addr,Int64* scoped_spin_lock_addr) override;
+
+  MutexImpl* createMutex() override;
+  void destroyMutex(MutexImpl*) override;
+  void lockMutex(MutexImpl* mutex) override;
+  void unlockMutex(MutexImpl* mutex) override;
+
+  Int64 currentThread() override;
+
+  IThreadBarrier* createBarrier() override;
+ private:
+  MutexImpl* m_global_mutex_impl;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // End namespace Arccore
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
