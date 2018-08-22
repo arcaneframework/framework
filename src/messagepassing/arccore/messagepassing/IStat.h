096cf447c9c94a8dce9167217cca98ab9a224b10
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2018-08-22 14:54:00 +0200

    [arccore] Déplace 'Parallel::Stat' dans 'arccore_messagepassing'.

---
+++ src/messagepassing/arccore/messagepassing/IStat.h
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------*/
+/* IStat.h                                                     (C) 2000-2018 */
+/*                                                                           */
+/* Statistiques sur le parallélisme.                                         */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCCORE_MESSAGEPASSING_ISTAT_H
+#define ARCCORE_MESSAGEPASSING_ISTAT_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/messagepassing/MessagePassingGlobal.h"
+
+#include "arccore/base/String.h"
+
+#include <iosfwd>
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore
+{
+
+namespace MessagePassing
+{
+
+//! Une statistique
+class OneStat
+{
+ public:
+  OneStat(const String& name)
+  : m_name(name), m_nb_msg(0), m_total_size(0), m_total_time(0.0)
+  {
+  }
+  void print(std::ostream& o);
+ public:
+  const String& name() const { return m_name; }
+  Int64 nbMessage() const { return m_nb_msg; }
+  Int64 totalSize() const { return m_total_size; }
+  double totalTime() const { return m_total_time; }
+  void addMessage(Int64 msg_size,double elapsed_time)
+  {
+    m_total_size += msg_size;
+    m_total_time += elapsed_time;
+    ++m_nb_msg;
+  }
+
+  
+ private:
+  String m_name; //!< Nom de la statistique
+  Int64 m_nb_msg; //!< Nombre de message envoyés.
+  Int64 m_total_size; //!< Taille total des messages envoyés
+  double m_total_time; //!< Temps total écoulé
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Statistiques sur le parallélisme.
+ * \todo rendre thread-safe
+ */
+class ARCCORE_MESSAGEPASSING_EXPORT IStat
+{
+ public:
+
+  //! Libère les ressources.
+  virtual ~IStat() {}
+
+ public:
+
+ public:
+
+  /*!
+   * \brief Ajoute une statistique.
+   * \param name nom de la statistique
+   * \param elapsed_time temps utilisé pour le message
+   * \param msg_size taille du message envoyé.
+   */
+  virtual void add(const String& name,double elapsed_time,Int64 msg_size) =0;
+  
+  //! Active ou désactive les statistiques
+  virtual void enable(bool is_enabled) =0;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // End namespace MessagePassing
+
+} // End namespace Arccore
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif  
+
---
+++ src/messagepassing/arccore/messagepassing/Stat.h
@@ -0,0 +1,93 @@
+/*---------------------------------------------------------------------------*/
+/* Stat.h                                                      (C) 2000-2018 */
+/*                                                                           */
+/* Statistiques sur le parallélisme.                                         */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCCORE_MESSAGEPASSING_STAT_H
+#define ARCCORE_MESSAGEPASSING_STAT_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/messagepassing/IStat.h"
+
+#include "arccore/base/String.h"
+
+#include <map>
+#include <iosfwd>
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore
+{
+
+namespace MessagePassing
+{
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Statistiques sur le parallélisme.
+ */
+class Stat
+: public IStat
+{
+ public:
+
+ public:
+
+  typedef std::map<String,OneStat*> OneStatMap;
+  typedef std::pair<String,OneStat*> OneStatValue;
+
+ public:
+
+  Stat();
+  //! Libère les ressources.
+  virtual ~Stat();
+
+ public:
+
+ public:
+
+  void add(const String& name,double elapsed_time,Int64 msg_size) override;
+  void enable(bool is_enabled) override { m_is_enabled = is_enabled; }
+
+  void print(std::ostream& o);
+
+  const OneStatMap& stats() const { return m_list; }
+
+ private:
+
+  bool m_is_enabled;
+  OneStatMap m_list;
+
+ private:
+
+  OneStat* _find(const String& name)
+  {
+    OneStatMap::const_iterator i = m_list.find(name);
+    if (i!=m_list.end()){
+      return i->second;
+    }
+    OneStat* os = new OneStat(name);
+    // Important: utiliser os.m_name car m_list stocke juste un
+    // pointeur sur la chaîne de caractère.
+    m_list.insert(OneStatValue(os->name(),os));
+    return os;
+  }
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // End namespace MessagePassing
+
+} // End namespace Arccore
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
