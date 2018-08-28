68a9fb4c4e86563d107155019b0307754cb69cbd
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2018-08-28 11:15:29 +0200

    [arccore] Ajoute classe MpiMessagePassingMng pour gérer le message passing avec MPI.

---
+++ src/messagepassingmpi/arccore/messagepassingmpi/MpiMessagePassingMng.h
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------*/
+/* MpiMessagePassingMng.h                                      (C) 2000-2018 */
+/*                                                                           */
+/* Implémentation MPI du gestionnaire des échanges de messages.              */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCCORE_MESSAGEPASSINGMPI_MPIMESSAGEPASSINGMNG_H
+#define ARCCORE_MESSAGEPASSINGMPI_MPIMESSAGEPASSINGMNG_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/messagepassingmpi/MessagePassingMpiGlobal.h"
+#include "arccore/messagepassing/MessagePassingMng.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore
+{
+namespace MessagePassing
+{
+namespace Mpi
+{
+
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Implémentation MPI du gestionnaire des échanges de messages.
+ */
+class ARCCORE_MESSAGEPASSINGMPI_EXPORT MpiMessagePassingMng
+: public MessagePassingMng
+{
+ public:
+  class BuildInfo
+  {
+   public:
+    Int32 commRank() const { return m_comm_rank; }
+    Int32 m_comm_rank;
+    Int32 commSize() const { return m_comm_size; }
+    Int32 m_comm_size;
+    IDispatchers* dispatchers() const { return m_dispatchers; }
+    IDispatchers* m_dispatchers;
+  }; 
+
+ public:
+
+  MpiMessagePassingMng(const BuildInfo& bi);
+  ~MpiMessagePassingMng() override;
+
+ public:
+
+ private:
+
+  MPI_Comm m_communicator;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // End namespace Mpi
+} // End namespace MessagePassing
+} // End namespace Arccore
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif  
