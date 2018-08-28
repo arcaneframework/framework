ad5ed45c1221b992f384d88c5fcb659bc523284f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2018-08-28 15:19:51 +0200

    [arccore] Implémente les wrappeurs manquants pour les messages.

--- src/messagepassing/arccore/messagepassing/Messages.h
+++ src/messagepassing/arccore/messagepassing/Messages.h
@@ -11,6 +11,7 @@
 #include "arccore/messagepassing/IMessagePassingMng.h"
 #include "arccore/messagepassing/IDispatchers.h"
 #include "arccore/messagepassing/ITypeDispatcher.h"
+#include "arccore/messagepassing/Request.h"
 #include "arccore/base/ArrayView.h"
 #include "arccore/collections/Array.h"
 
@@ -30,20 +31,36 @@ namespace MessagePassing
   { type* x = nullptr; pm->dispatchers()->dispatcher(x)->allGather(send_buf,recv_buf); } \
   inline void mpGather(IMessagePassingMng* pm,ConstArrayView<type> send_buf,ArrayView<type> recv_buf,Int32 rank) \
   { type* x = nullptr; pm->dispatchers()->dispatcher(x)->gather(send_buf,recv_buf,rank); } \
-  inline void mpAllGatherVariable(IMessagePassingMng* pm,ConstArrayView<type> send_buf,Array<type>& recv_buf) ; \
-  inline void mpGatherVariable(IMessagePassingMng* pm,ConstArrayView<type> send_buf,Array<type>& recv_buf,Int32 rank) ; \
+  inline void mpAllGatherVariable(IMessagePassingMng* pm,ConstArrayView<type> send_buf,Array<type>& recv_buf) \
+  { type* x = nullptr; pm->dispatchers()->dispatcher(x)->allGatherVariable(send_buf,recv_buf); } \
+  inline void mpGatherVariable(IMessagePassingMng* pm,ConstArrayView<type> send_buf,Array<type>& recv_buf,Int32 rank)\
+  { type* x = nullptr; pm->dispatchers()->dispatcher(x)->gatherVariable(send_buf,recv_buf,rank); } \
   inline void mpScatterVariable(IMessagePassingMng* pm,ConstArrayView<type> send_buf,ArrayView<type> recv_buf,Int32 root) ; \
-  inline type mpAllReduce(IMessagePassingMng* pm,eReduceType rt,type v) ; \
-  inline void mpAllReduce(IMessagePassingMng* pm,eReduceType rt,ArrayView<type> v) ; \
-  inline void mpBroadcast(IMessagePassingMng* pm,ArrayView<type> send_buf,Int32 rank) ; \
-  inline void mpSend(IMessagePassingMng* pm,ConstArrayView<type> values,Int32 rank) ; \
-  inline void mpReceive(IMessagePassingMng* pm,ArrayView<type> values,Int32 rank) ; \
-  inline Request mpSend(IMessagePassingMng* pm,ConstArrayView<type> values,Int32 rank,bool is_blocked) ; \
-  inline Request mpReceive(IMessagePassingMng* pm,ArrayView<type> values,Int32 rank,bool is_blocked) ; \
-  inline void mpAllToAll(IMessagePassingMng* pm,ConstArrayView<type> send_buf,ArrayView<type> recv_buf,Integer count) ; \
+  inline type mpAllReduce(IMessagePassingMng* pm,eReduceType rt,type v) \
+  { type* x = nullptr; return pm->dispatchers()->dispatcher(x)->allReduce(rt,v); }\
+  inline void mpAllReduce(IMessagePassingMng* pm,eReduceType rt,ArrayView<type> v) \
+  { type* x = nullptr; pm->dispatchers()->dispatcher(x)->allReduce(rt,v); }\
+  inline void mpBroadcast(IMessagePassingMng* pm,ArrayView<type> send_buf,Int32 rank) \
+  { type* x = nullptr; pm->dispatchers()->dispatcher(x)->broadcast(send_buf,rank); } \
+  inline void mpSend(IMessagePassingMng* pm,ConstArrayView<type> values,Int32 rank) \
+  { type* x = nullptr; pm->dispatchers()->dispatcher(x)->send(values,rank,true); } \
+  inline void mpReceive(IMessagePassingMng* pm,ArrayView<type> values,Int32 rank) \
+  { type* x = nullptr; pm->dispatchers()->dispatcher(x)->receive(values,rank,true); } \
+  inline Request mpSend(IMessagePassingMng* pm,ConstArrayView<type> values,Int32 rank,bool is_blocked) \
+  { type* x = nullptr; return pm->dispatchers()->dispatcher(x)->send(values,rank,is_blocked); } \
+  inline Request mpReceive(IMessagePassingMng* pm,ArrayView<type> values,Int32 rank,bool is_blocked)\
+  { type* x = nullptr; return pm->dispatchers()->dispatcher(x)->receive(values,rank,is_blocked); } \
+  inline void mpAllToAll(IMessagePassingMng* pm,ConstArrayView<type> send_buf,ArrayView<type> recv_buf,Integer count) \
+  { type* x = nullptr; return pm->dispatchers()->dispatcher(x)->allToAll(send_buf,recv_buf,count); } \
   inline void mpAllToAllVariable(IMessagePassingMng* pm,ConstArrayView<type> send_buf,Int32ConstArrayView send_count, \
                                  Int32ConstArrayView send_index,ArrayView<type> recv_buf, \
-                                 Int32ConstArrayView recv_count,Int32ConstArrayView recv_index)
+                                 Int32ConstArrayView recv_count,Int32ConstArrayView recv_index)\
+  { type* x = nullptr;\
+    auto d = pm->dispatchers()->dispatcher(x);\
+    d->allToAllVariable(send_buf,send_count,send_index,recv_buf,recv_count,recv_index); }
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 
 ARCCORE_GENERATE_MESSAGEPASSING_PROTOTYPE(char);
 ARCCORE_GENERATE_MESSAGEPASSING_PROTOTYPE(signed char);
