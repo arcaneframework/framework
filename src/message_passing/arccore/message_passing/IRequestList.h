a0a6ef3740781be437511bdbee221e2d430bf45a
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2020-03-28 08:04:36 +0100

    [message_passing] Créé une implémentation de base pour 'IRequestList'

--- src/message_passing/arccore/message_passing/IRequestList.h
+++ src/message_passing/arccore/message_passing/IRequestList.h
@@ -9,7 +9,7 @@
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#include "arccore/message_passing/Request.h"
+#include "arccore/message_passing/MessagePassingGlobal.h"
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
@@ -27,9 +27,29 @@ class ARCCORE_MESSAGEPASSING_EXPORT IRequestList
   virtual ~IRequestList() = default;
  public:
 
+  //! Ajoute la requête \a r à la liste
   virtual void addRequest(Request r) =0;
+
+  //! Nombre de requêtes
   virtual Integer nbRequest() const =0;
+
+  /*!
+   * \brief Attend ou test la complétion de une ou plusieurs requêtes.
+   *
+   * Cette méthode appelle removeDoneRequest() avant d'attendre la
+   * complétion. En retour, retourne le nombre de requêtes terminées.
+   * Il est possible de tester si une requête est terminée via la
+   * méthode isRequestDone();
+   */
   virtual Integer wait(eWaitType wait_type) =0;
+
+  //! Indique si la requête est terminée
+  virtual bool isRequestDone(Integer index) const =0;
+
+  //! \a index-ième requête de la liste
+  virtual Request request(Integer index) const =0;
+
+  //! Supprime de la liste les requêtes terminées
   virtual void removeDoneRequests() =0;
 };
 
---
+++ src/message_passing/arccore/message_passing/RequestListBase.h
@@ -0,0 +1,73 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+/*---------------------------------------------------------------------------*/
+/* RequestListBase.h                                           (C) 2000-2020 */
+/*                                                                           */
+/* Classe de base d'une liste de requêtes.                                   */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCCORE_MESSAGEPASSING_REQUESTLISTBASE_H
+#define ARCCORE_MESSAGEPASSING_REQUESTLISTBASE_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/message_passing/IRequestList.h"
+#include "arccore/message_passing/Request.h"
+#include "arccore/collections/Array.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore::MessagePassing::internal
+{
+/*!
+ * \internal
+ * \brief Classe de base d'une liste de requêtes.
+ */
+class ARCCORE_MESSAGEPASSING_EXPORT RequestListBase
+: public IRequestList
+{
+ public:
+
+  RequestListBase() = default;
+
+ public:
+
+  void addRequest(Request r) override { _addRequest(r); }
+  Integer nbRequest() const override { return m_requests.size(); }
+  void removeDoneRequests() override;
+  bool isRequestDone(Integer index) const override { return m_requests_done[index]; }
+  Request request(Integer index) const override { return m_requests[index]; }
+
+ protected:
+
+  virtual void _addRequest(Request r)
+  {
+    m_requests.add(r);
+    m_requests_done.add(false);
+  }
+  virtual void _removeRequestAtIndex(Integer pos)
+  {
+    m_requests.remove(pos);
+    m_requests_done.remove(pos);
+  }
+
+ protected:
+
+  ArrayView<Request> _requests() { return m_requests; }
+  ArrayView<bool> _requestsDone() { return m_requests_done; }
+
+ private:
+
+  UniqueArray<Request> m_requests;
+  UniqueArray<bool> m_requests_done;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // End namespace Arccore::MessagePassing
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif  
+
