4df79b149467b132434bce9d4d01bc46df3fd182
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2020-04-01 10:49:13 +0200

    [message_passing_mpi] Créé la classe 'MpiSerializeDispatcher' pour implémenter 'ISerializeDispatcher' via MPI.

---
+++ src/message_passing_mpi/arccore/message_passing_mpi/MpiSerializeDispatcher.cc
@@ -0,0 +1,37 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+/*---------------------------------------------------------------------------*/
+/* MpiSerializeDispatcher.cc                                   (C) 2000-2020 */
+/*                                                                           */
+/* Gestion des messages de sérialisation avec MPI.                           */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/message_passing_mpi/MpiSerializeDispatcher.h"
+#include "arccore/message_passing_mpi/MpiAdapter.h"
+#include "arccore/message_passing_mpi/MpiMessagePassingMng.h"
+#include "arccore/message_passing/Request.h"
+#include "arccore/base/NotImplementedException.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore::MessagePassing::Mpi
+{
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+MpiSerializeDispatcher::
+MpiSerializeDispatcher(IMessagePassingMng* parallel_mng, MpiAdapter* adapter)
+: m_parallel_mng(parallel_mng)
+, m_adapter(adapter)
+{
+}
+
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arccore::MessagePassing::Mpi
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
---
+++ src/message_passing_mpi/arccore/message_passing_mpi/MpiSerializeDispatcher.h
@@ -0,0 +1,41 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+/*---------------------------------------------------------------------------*/
+/* MpiSerializeDispatcher.h                                    (C) 2000-2020 */
+/*                                                                           */
+/* Gestion des messages de sérialisation avec MPI.                           */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCCORE_MESSAGEPASSINGMPI_MPISERIALIZEDISPATCHER_H
+#define ARCCORE_MESSAGEPASSINGMPI_MPISERIALIZEDISPATCHER_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/message_passing_mpi/MessagePassingMpiGlobal.h"
+#include "arccore/message_passing/ISerializeDispatcher.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore::MessagePassing::Mpi
+{
+class MpiSerializeDispatcher
+: public ISerializeDispatcher
+{
+ public:
+  MpiSerializeDispatcher(IMessagePassingMng* parallel_mng, MpiAdapter* adapter);
+
+ public:
+
+ private:
+  IMessagePassingMng* m_parallel_mng;
+  MpiAdapter* m_adapter;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arccore::MessagePassing::Mpi
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
