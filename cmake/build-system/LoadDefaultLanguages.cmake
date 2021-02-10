cc032a2772bfd54b7dfea0f235e8ae396be4caa4
gratienj <jean-marc.gratien@ifpen.fr>
2021-02-10 19:57:59 +0100

    update

--- cmake/build-system/LoadDefaultLanguages.cmake
+++ cmake/build-system/LoadDefaultLanguages.cmake
@@ -3,7 +3,7 @@
 
 loadLanguage(NAME axl PATH ${BUILD_SYSTEM_PATH}/languages)
 
-if(USE_ARCANE_V3)
+git add if(USE_ARCANE_V3)
   set(AXL2CC "${AXLSTAR_AXL2CC}")
   logStatus("AXL TOOLS : AXL2CC = ${AXLSTAR_AXL2CC} AXL2CCT4 = ${AXL2CCT4}")
 endif()
