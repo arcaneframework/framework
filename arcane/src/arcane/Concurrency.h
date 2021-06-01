1bd7a41893662ac72433bd5e8a4a727c2960e357
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-06-01 15:17:22 +0200

    [core] Déplace et renomme les méthodes 'Arcane::Parallel::Foreach()' et 'Arcane::Parallel::For()' dans le namespace Arcane.
    Les méthodes deviennent 'Arcane::ParallelForeach()' et
    'Arcane::ParallelFor()'. A terme, le namespace 'Parallel' pour la
    concurrence sera supprimé car il y a ambiguité avec le namespace 'Parallel'
    utilisé par exemple pour certaines méthodes de 'IParallelMng'.

--- arcane/src/arcane/Concurrency.h
+++ arcane/src/arcane/Concurrency.h
@@ -541,9 +541,159 @@ class ARCANE_CORE_EXPORT TaskFactory
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
+
 /*!
  * \ingroup Concurrency
+ *
+ * \brief Applique en concurrence la méthode \a function de l'instance
+ * \a instance sur la vue \a items_view avec les options \a options.
+ */
+template<typename InstanceType,typename ItemType> inline void
+ParallelForeach(const ItemVectorView& items_view,const ParallelLoopOptions& options,
+                InstanceType* instance,void (InstanceType::*function)(ItemVectorViewT<ItemType> items))
+{
+  ItemRangeFunctorT<InstanceType,ItemType> ipf(items_view,instance,function,options.grainSize());
+  // Recopie \a options et utilise la valeur de 'grain_size' retournée par \a ifp
+  ParallelLoopOptions loop_opt(options);
+  loop_opt.setGrainSize(ipf.blockGrainSize());
+  TaskFactory::executeParallelFor(0,ipf.nbBlock(),loop_opt,&ipf);
+}
+
+/*!
+ * \ingroup Concurrency
+ *
+ * \brief Applique en concurrence la méthode \a function de l'instance
+ * \a instance sur le groupe \a items avec les options \a options.
+ */
+template<typename InstanceType,typename ItemType> inline void
+ParallelForeach(const ItemGroup& items,const ParallelLoopOptions& options,InstanceType* instance,
+                void (InstanceType::*function)(ItemVectorViewT<ItemType> items))
+{
+  ParallelForeach(items.view(),options,instance,function);
+}
+
+/*!
+ * \ingroup Concurrency
+ * \brief Applique en concurrence la méthode \a function de l'instance
+ * \a instance sur la vue \a items_view.
+ */
+template<typename InstanceType,typename ItemType> inline void
+ParallelForeach(const ItemVectorView& items_view,InstanceType* instance,void (InstanceType::*function)(ItemVectorViewT<ItemType> items))
+{
+  ItemRangeFunctorT<InstanceType,ItemType> ipf(items_view,instance,function);
+  TaskFactory::executeParallelFor(0,ipf.nbBlock(),ipf.blockGrainSize(),&ipf);
+}
+
+/*!
+ * \ingroup Concurrency
+ * \brief Applique en concurrence la méthode \a function de l'instance
+ * \a instance sur le groupe \a items.
+ */
+template<typename InstanceType,typename ItemType> inline void
+ParallelForeach(const ItemGroup& items,InstanceType* instance,
+                void (InstanceType::*function)(ItemVectorViewT<ItemType> items))
+{
+  ParallelForeach(items.view(),instance,function);
+}
+
+/*!
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * \a instance sur la vue \a items_view avec les options \a options
+ * \ingroup Concurrency
+ */
+template<typename LambdaType> inline void
+ParallelForeach(const ItemVectorView& items_view,const ParallelLoopOptions& options,
+                const LambdaType& lambda_function)
+{
+  LambdaItemRangeFunctorT<LambdaType> ipf(items_view,lambda_function,options.grainSize());
+  // Recopie \a options et utilise la valeur de 'grain_size' retournée par \a ifp
+  ParallelLoopOptions loop_opt(options);
+  loop_opt.setGrainSize(ipf.blockGrainSize());
+  TaskFactory::executeParallelFor(0,ipf.nbBlock(),loop_opt,&ipf);
+}
+
+/*!
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * sur le groupe \a items avec les options \a options
+ * \ingroup Concurrency
+ */
+template<typename LambdaType> inline void
+ParallelForeach(const ItemGroup& items,const ParallelLoopOptions& options,const LambdaType& lambda_function)
+{
+  ParallelForeach(items.view(),options,lambda_function);
+}
+
+/*!
+ * \ingroup Concurrency
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * \a instance sur la vue \a items_view.
+ */
+template<typename LambdaType> inline void
+ParallelForeach(const ItemVectorView& items_view,const LambdaType& lambda_function)
+{
+  LambdaItemRangeFunctorT<LambdaType> ipf(items_view,lambda_function);
+  TaskFactory::executeParallelFor(0,ipf.nbBlock(),ipf.blockGrainSize(),&ipf);
+}
+
+/*!
+ * \ingroup Concurrency
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * sur le groupe \a items.
+ */
+template<typename LambdaType> inline void
+ParalellForeach(const ItemGroup& items,const LambdaType& lambda_function)
+{
+  ParallelForeach(items.view(),lambda_function);
+}
+
+/*!
+ * \ingroup Concurrency
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * sur l'intervalle d'itération [i0,i0+size].
+ */
+template<typename InstanceType> inline void
+ParallelFor(Integer i0,Integer size,InstanceType* itype,
+            void (InstanceType::*lambda_function)(Integer i0,Integer size))
+{
+  RangeFunctorT<InstanceType> ipf(itype,lambda_function);
+  TaskFactory::executeParallelFor(i0,size,&ipf);
+}
+
+/*!
+ * \ingroup Concurrency
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * sur l'intervalle d'itération [i0,i0+size] avec les options \a options.
+ */
+template<typename LambdaType> inline void
+ParalellFor(Integer i0,Integer size,const ParallelLoopOptions& options,
+            const LambdaType& lambda_function)
+{
+  LambdaRangeFunctorT<LambdaType> ipf(lambda_function);
+  TaskFactory::executeParallelFor(i0,size,options,&ipf);
+}
+
+/*!
+ * \brief Applique en concurrence la fonction lambda \a lambda_function
+ * sur l'intervalle d'itération [i0,i0+size]
+ */
+template<typename LambdaType> inline void
+ParallelFor(Integer i0,Integer size,const LambdaType& lambda_function)
+{
+  LambdaRangeFunctorT<LambdaType> ipf(lambda_function);
+  TaskFactory::executeParallelFor(i0,size,&ipf);
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \ingroup Concurrency
+ *
  * \brief Implémentation de la concurrence.
+ *
+ * Les méthodes de ce namespace sont obsolètes et doivent être remplacées
+ * par les méthodes équivalentes dans le namespace Arcane.
+ * Par exemple Arcane::Parallel::For() doit être remplacé par Arcane::ParallelFor()
+ * et Arcane::Parallel::Foreach() par Arcane::ParallelForeach().
  */
 namespace Parallel
 { 
@@ -563,7 +713,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(0,ipf.nbBlock(),loop_opt,&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la méthode \a function de l'instance
    * \a instance sur le groupe \a items avec les options \a options
    * \ingroup Concurrency
@@ -587,7 +737,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(0,ipf.nbBlock(),ipf.blockGrainSize(),&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la méthode \a function de l'instance
    * \a instance sur le groupe \a items.
    * \ingroup Concurrency
@@ -599,7 +749,7 @@ namespace Parallel
   }
 
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * \a instance sur la vue \a items_view avec les options \a options
    * \ingroup Concurrency
@@ -614,7 +764,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(0,ipf.nbBlock(),loop_opt,&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * sur le groupe \a items avec les options \a options
    * \ingroup Concurrency
@@ -625,7 +775,7 @@ namespace Parallel
     Foreach(items.view(),options,lambda_function);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * \a instance sur la vue \a items_view.
    * \ingroup Concurrency
@@ -637,7 +787,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(0,ipf.nbBlock(),ipf.blockGrainSize(),&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * sur le groupe \a items.
    * \ingroup Concurrency
@@ -676,7 +826,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(i0,size,options,&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * sur l'intervalle d'itération [i0,i0+size].
    * \ingroup Concurrency
@@ -689,7 +839,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(i0,size,grain_size,&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * sur l'intervalle d'itération [i0,i0+size].
    * \ingroup Concurrency
@@ -702,7 +852,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(i0,size,&ipf);
   }
 
-  /*! 
+  /*!
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * sur l'intervalle d'itération [i0,i0+size] avec les options \a options.
    * \ingroup Concurrency
@@ -714,7 +864,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(i0,size,options,&ipf);
   }
 
-  /*! 
+  /*!
    * \ingroup Concurrency
    * \brief Applique en concurrence la fonction lambda \a lambda_function
    * sur l'intervalle d'itération [i0,i0+size]
@@ -726,7 +876,7 @@ namespace Parallel
     TaskFactory::executeParallelFor(i0,size,&ipf);
   }
 
-}
+} // End namespace Parallel
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
