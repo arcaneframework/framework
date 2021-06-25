2f42199e4560a5e9c6ed33c5fa8dbfb32c377658
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-06-25 15:16:17 +0200

    [config] Ajoute dans le sous-répertoire '_common/build_all' la configuration nécessaire pour compiler toutes les composantes.
    Cela correspond à ce que faisait le dépot 'arcframework'.

---
+++ _common/build_all/cmake/FindArccore.cmake
@@ -0,0 +1,15 @@
+if (NOT Arccore_ROOT)
+  # Pour compatibilité
+  set(Arccore_ROOT "${ARCCORE_ROOT}")
+endif()
+if (NOT Arccore_ROOT)
+   message(FATAL_ERROR "Variable 'Arccore_ROOT' is not set")
+endif()
+if (NOT ARCCORE_EXPORT_TARGET)
+  # Indique qu'on souhaite exporter dans 'ArcaneTargets' les cibles des
+  # packages définies dans 'Arccon'.
+  set(ARCCON_EXPORT_TARGET ArcaneTargets)
+endif()
+
+add_subdirectory(${Arccore_ROOT} arccore)
+set(Arccore_FOUND YES)
