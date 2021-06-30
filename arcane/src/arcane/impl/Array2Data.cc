4573e5300bac69a488638bda70259b46e1ee111e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-06-30 10:30:24 +0200

    [arcane,impl] Déplace la définition des implémentations de 'IData' dans le fichier '.cc' au lieu du '.h'.
    Cela garantit que l'implémentation restera privée à Arcane.

--- arcane/src/arcane/impl/Array2Data.cc
+++ arcane/src/arcane/impl/Array2Data.cc
@@ -11,6 +11,11 @@
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
+#include "arccore/base/ReferenceCounterImpl.h"
+#include "arccore/base/Ref.h"
+
+#include "arcane/utils/Array2.h"
+#include "arcane/utils/NotSupportedException.h"
 #include "arcane/utils/Real2.h"
 #include "arcane/utils/Real2x2.h"
 #include "arcane/utils/Real3.h"
@@ -25,11 +30,14 @@
 #include "arcane/datatype/IDataOperation.h"
 #include "arcane/datatype/DataStorageTypeInfo.h"
 #include "arcane/datatype/DataStorageBuildInfo.h"
+#include "arcane/datatype/DataTypeTraits.h"
 
 #include "arcane/ISerializer.h"
+#include "arcane/IData.h"
+#include "arcane/IDataVisitor.h"
+
 #include "arcane/core/internal/IDataInternal.h"
 
-#include "arcane/impl/Array2Data.h"
 #include "arcane/impl/SerializedData.h"
 #include "arcane/impl/DataStorageFactory.h"
 
@@ -43,6 +51,112 @@ namespace
 {
   const Int64 SERIALIZE2_MAGIC_NUMBER = 0x12ff7789;
 }
+
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Donnée tableau bi-dimensionnel d'un type \a DataType
+ */
+template <class DataType>
+class Array2DataT
+: public ReferenceCounterImpl
+, public IArray2DataT<DataType>
+{
+  ARCCORE_DEFINE_REFERENCE_COUNTED_INCLASS_METHODS();
+  class Impl;
+  friend Impl;
+
+ public:
+
+  typedef Array2DataT<DataType> ThatClass;
+  typedef IArray2DataT<DataType> DataInterfaceType;
+
+ public:
+
+  explicit Array2DataT(ITraceMng* trace);
+  explicit Array2DataT(const DataStorageBuildInfo& dsbi);
+  Array2DataT(const Array2DataT<DataType>& rhs);
+  ~Array2DataT() override;
+
+ public:
+
+  Integer dimension() const override { return 2; }
+  Integer multiTag() const override { return 0; }
+  eDataType dataType() const override { return DataTypeTraitsT<DataType>::type(); }
+  void serialize(ISerializer* sbuf, IDataOperation* operation) override;
+  void serialize(ISerializer* sbuf, Int32ConstArrayView ids, IDataOperation* operation) override;
+  Array2<DataType>& value() override { return m_value; }
+  const Array2<DataType>& value() const override { return m_value; }
+  Array2View<DataType> view() override { return m_value; }
+  ConstArray2View<DataType> view() const override { return m_value; }
+  void resize(Integer new_size) override;
+  IData* clone() override { return cloneTrue(); }
+  IData* cloneEmpty() override { return cloneTrueEmpty(); }
+  Ref<IData> cloneRef() override { return makeRef(cloneTrue()); }
+  Ref<IData> cloneEmptyRef() override { return makeRef(cloneTrueEmpty()); }
+  DataStorageTypeInfo storageTypeInfo() const override;
+  DataInterfaceType* cloneTrue() override { return _cloneTrue(); }
+  DataInterfaceType* cloneTrueEmpty() override { return _cloneTrueEmpty(); }
+  Ref<DataInterfaceType> cloneTrueRef() override { auto* d = _cloneTrue(); return makeRef(d); }
+  Ref<DataInterfaceType> cloneTrueEmptyRef() override { auto* d = _cloneTrueEmpty(); return makeRef(d); }
+  void fillDefault() override;
+  void setName(const String& name) override;
+  const ISerializedData* createSerializedData(bool use_basic_type) const override;
+  Ref<ISerializedData> createSerializedDataRef(bool use_basic_type) const override;
+  void allocateBufferForSerializedData(ISerializedData* sdata) override;
+  void assignSerializedData(const ISerializedData* sdata) override;
+  void copy(const IData* data) override;
+  void swapValues(IData* data) override;
+  void computeHash(IHashAlgorithm* algo, ByteArray& output) const override;
+  void visit(IArray2DataVisitor* visitor)
+  {
+    visitor->applyVisitor(this);
+  }
+  void visit(IDataVisitor* visitor) override
+  {
+    visitor->applyDataVisitor(this);
+  }
+  void visitScalar(IScalarDataVisitor*) override
+  {
+    ARCANE_THROW(NotSupportedException, "Can not visit scalar data with array2 data");
+  }
+  void visitArray(IArrayDataVisitor*) override
+  {
+    ARCANE_THROW(NotSupportedException, "Can not visit array data with array2 data");
+  }
+  void visitArray2(IArray2DataVisitor* visitor) override
+  {
+    visitor->applyVisitor(this);
+  }
+  void visitMultiArray2(IMultiArray2DataVisitor*) override
+  {
+    ARCANE_THROW(NotSupportedException, "Can not visit multiarray2 data with array data");
+  }
+
+ public:
+
+  void swapValuesDirect(ThatClass* true_data);
+
+ public:
+
+  IArray2DataInternalT<DataType>* _internal() override { return m_internal; }
+
+ public:
+
+  static DataStorageTypeInfo staticStorageTypeInfo();
+
+ private:
+
+  UniqueArray2<DataType> m_value; //!< Donnée
+  ITraceMng* m_trace;
+  IArray2DataInternalT<DataType>* m_internal;
+
+ private:
+
+  IArray2DataT<DataType>* _cloneTrue() const { return new ThatClass(*this); }
+  IArray2DataT<DataType>* _cloneTrueEmpty() const { return new ThatClass(m_trace); }
+};
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
--- arcane/src/arcane/impl/ArrayData.cc
+++ arcane/src/arcane/impl/ArrayData.cc
@@ -5,13 +5,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ArrayData.cc                                                (C) 2000-2020 */
+/* ArrayData.cc                                                (C) 2000-2021 */
 /*                                                                           */
 /* Donnée du type 'Array'.                                                   */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#include "arcane/impl/ArrayData.h"
+#include "arccore/base/ReferenceCounterImpl.h"
+#include "arccore/base/Ref.h"
 
 #include "arcane/utils/Real2.h"
 #include "arcane/utils/Real2x2.h"
@@ -19,6 +20,7 @@
 #include "arcane/utils/Real3x3.h"
 #include "arcane/utils/IHashAlgorithm.h"
 #include "arcane/utils/NotImplementedException.h"
+#include "arcane/utils/NotSupportedException.h"
 #include "arcane/utils/ArgumentException.h"
 #include "arcane/utils/FatalErrorException.h"
 #include "arcane/utils/IndexOutOfRangeException.h"
@@ -27,8 +29,12 @@
 #include "arcane/datatype/DataStorageBuildInfo.h"
 #include "arcane/datatype/IDataOperation.h"
 #include "arcane/datatype/DataStorageTypeInfo.h"
+#include "arcane/datatype/DataTypeTraits.h"
 
 #include "arcane/ISerializer.h"
+#include "arcane/IData.h"
+#include "arcane/IDataVisitor.h"
+
 #include "arcane/core/internal/IDataInternal.h"
 
 #include "arcane/impl/SerializedData.h"
@@ -44,6 +50,112 @@ namespace
 {
   const Int64 SERIALIZE_MAGIC_NUMBER = 0x456ff989;
 }
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Donnée tableau d'un type \a T
+ */
+template<class DataType>
+class ArrayDataT
+: public ReferenceCounterImpl
+, public IArrayDataT<DataType>
+{
+  ARCCORE_DEFINE_REFERENCE_COUNTED_INCLASS_METHODS();
+  class Impl;
+  friend class Impl;
+
+ public:
+
+  typedef ArrayDataT<DataType> ThatClass;
+  typedef IArrayDataT<DataType> DataInterfaceType;
+
+ public:
+
+  explicit ArrayDataT(ITraceMng* trace);
+  explicit ArrayDataT(const DataStorageBuildInfo& dsbi);
+  ArrayDataT(const ArrayDataT<DataType>& rhs);
+  ~ArrayDataT() override;
+
+ public:
+
+  Integer dimension() const override { return 1; }
+  Integer multiTag() const override { return 0; }
+  eDataType dataType() const override { return DataTypeTraitsT<DataType>::type(); }
+  void serialize(ISerializer* sbuf,IDataOperation* operation) override;
+  void serialize(ISerializer* sbuf,Int32ConstArrayView ids,IDataOperation* operation) override;
+  Array<DataType>& value() override { return m_value; }
+  const Array<DataType>& value() const override { return m_value; }
+  ConstArrayView<DataType> view() const override { return m_value; }
+  ArrayView<DataType> view() override { return m_value; }
+  void resize(Integer new_size) override { m_value.resize(new_size); }
+  IData* clone() override { return cloneTrue(); }
+  IData* cloneEmpty() override { return cloneTrueEmpty(); }
+  Ref<IData> cloneRef() override { return makeRef(cloneTrue()); }
+  Ref<IData> cloneEmptyRef() override { return makeRef(cloneTrueEmpty()); }
+  DataStorageTypeInfo storageTypeInfo() const override;
+  DataInterfaceType* cloneTrue() override { return _cloneTrue(); }
+  DataInterfaceType* cloneTrueEmpty() override { return _cloneTrueEmpty(); }
+  Ref<DataInterfaceType> cloneTrueRef() override { auto* d = _cloneTrue(); return makeRef(d); }
+  Ref<DataInterfaceType> cloneTrueEmptyRef() override { auto* d = _cloneTrueEmpty(); return makeRef(d); }
+  void fillDefault() override;
+  void setName(const String& name) override;
+  const ISerializedData* createSerializedData(bool use_basic_type) const override;
+  Ref<ISerializedData> createSerializedDataRef(bool use_basic_type) const override;
+  void allocateBufferForSerializedData(ISerializedData* sdata) override;
+  void assignSerializedData(const ISerializedData* sdata) override;
+  void copy(const IData* data) override;
+  void swapValues(IData* data) override;
+  void computeHash(IHashAlgorithm* algo,ByteArray& output) const override;
+  void visit(IArrayDataVisitor* visitor) override
+  {
+    visitor->applyVisitor(this);
+  }
+  void visit(IDataVisitor* visitor) override
+  {
+    visitor->applyDataVisitor(this);
+  }
+  void visitScalar(IScalarDataVisitor*) override
+  {
+    ARCANE_THROW(NotSupportedException,"Can not visit scalar data with array data");
+  }
+  void visitArray(IArrayDataVisitor* visitor) override
+  {
+    visitor->applyVisitor(this);
+  }
+  void visitArray2(IArray2DataVisitor*) override
+  {
+    ARCANE_THROW(NotSupportedException,"Can not visit array2 data with array data");
+  }
+  void visitMultiArray2(IMultiArray2DataVisitor*) override
+  {
+    ARCANE_THROW(NotSupportedException,"Can not visit multiarray2 data with array data");
+  }
+
+ public:
+
+  void swapValuesDirect(ThatClass* true_data);
+
+ public:
+
+  IArrayDataInternalT<DataType>* _internal() override { return m_internal; }
+
+ public:
+
+  static DataStorageTypeInfo staticStorageTypeInfo();
+
+ private:
+
+  UniqueArray<DataType> m_value; //!< Donnée
+  ITraceMng* m_trace;
+  IArrayDataInternalT<DataType>* m_internal;
+
+ private:
+
+  void _serialize(ISerializer* sbuf,Span<const Int32> ids,IDataOperation* operation);
+  IArrayDataT<DataType>* _cloneTrue() const { return new ThatClass(*this); }
+  IArrayDataT<DataType>* _cloneTrueEmpty() const { return new ThatClass(m_trace); }
+};
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
