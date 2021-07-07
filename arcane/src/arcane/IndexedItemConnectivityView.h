0b3ccae8b662ad9e56275a824ab52b596de2b726
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-07-07 15:02:18 +0200

    [arcane,core] Déplace l'initialisation des 'IndexedItemConnectivityView' dans la classe de base et ajoute un accès générique via 'ItemLocalId'.

--- arcane/src/arcane/IndexedItemConnectivityView.h
+++ arcane/src/arcane/IndexedItemConnectivityView.h
@@ -26,20 +26,40 @@ namespace Arcane
 /*---------------------------------------------------------------------------*/
 /*!
  * \brief Classe de base d'une vue sur une connectivité non structurée.
+ *
+ * Comme toute les vues, les instances de cette classe sont temporaires et
+ * ne doivent pas être conservées entre deux évolutions du maillage.
  */
 class ARCANE_CORE_EXPORT IndexedItemConnectivityBaseView
 {
  public:
+  //! Nombre d'entités connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE Int32 nbItem(ItemLocalId lid) const { return m_nb_item[lid]; }
+  //! Liste des entités connectées à l'entité \a lid
+  ARCCORE_HOST_DEVICE ItemLocalIdView<Item> items(ItemLocalId lid) const
+  {
+    const Int32* ptr = & m_list_data[m_indexes[lid]];
+    return { reinterpret_cast<const ItemLocalId*>(ptr), m_nb_item[lid] };
+  }
+ public:
+  //! Initialise la vue
+  void init(SmallSpan<const Int32> nb_item,SmallSpan<const Int32> indexes,
+            SmallSpan<const Int32> list_data)
+  {
+    m_indexes = indexes;
+    m_nb_item = nb_item;
+    m_list_data = list_data;
+  }
  protected:
   SmallSpan<const Int32> m_nb_item;
   SmallSpan<const Int32> m_indexes;
+  SmallSpan<const Int32> m_list_data;
 };
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 /*!
- * \brief Vue sur une connectivité non structurée entre deux entités.
+ * \brief Vue spécialisée sur une connectivité non structurée entre deux entités.
  */
 template<typename ItemType1,typename ItemType2>
 class IndexedItemConnectivityGenericView
@@ -52,20 +72,12 @@ class IndexedItemConnectivityGenericView
   using ItemLocalId2 = typename ItemType2::LocalIdType;
   using ItemLocalIdViewType = ItemLocalIdView<ItemType2>;
  public:
-  void init(SmallSpan<const Int32> nb_item,SmallSpan<const Int32> indexes,
-            SmallSpan<const Int32> list_data)
-  {
-    m_indexes = indexes;
-    m_nb_item = nb_item;
-    m_list = { reinterpret_cast<const ItemLocalId2*>(list_data.data()), list_data.size() };
-  }
- public:
+  //! Liste des entités connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE ItemLocalIdViewType items(ItemLocalId1 lid) const
   {
-    return { m_list.data() + m_indexes[lid], m_nb_item[lid] };
+    const Int32* ptr = & m_list_data[m_indexes[lid]];
+    return { reinterpret_cast<const ItemLocalId2*>(ptr), m_nb_item[lid] };
   }
- protected:
-  ItemLocalIdViewType m_list;
 };
 
 /*---------------------------------------------------------------------------*/
@@ -82,7 +94,9 @@ class IndexedItemConnectivityView<ItemType,Node>
   using ItemLocalIdType = typename ItemType::LocalIdType;
   using ItemLocalIdViewType = typename BaseClass::ItemLocalIdViewType;
  public:
+  //! Nombre de noeuds connectés à l'entité \a lid
   ARCCORE_HOST_DEVICE Int32 nbNode(ItemLocalIdType lid) const { return BaseClass::nbItem(lid); }
+  //! Liste des noeuds connectés à l'entité \a lid
   ARCCORE_HOST_DEVICE ItemLocalIdViewType nodes(ItemLocalIdType lid) const { return BaseClass::items(lid); }
 };
 
@@ -100,7 +114,9 @@ class IndexedItemConnectivityView<ItemType,Edge>
   using ItemLocalIdType = typename ItemType::LocalIdType;
   using ItemLocalIdViewType = typename BaseClass::ItemLocalIdViewType;
  public:
+  //! Nombre d'arêtes connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE Int32 nbEdge(ItemLocalIdType lid) const { return BaseClass::nbItem(lid); }
+  //! Liste des arêtes connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE ItemLocalIdViewType edges(ItemLocalIdType lid) const { return BaseClass::items(lid); }
 };
 
@@ -118,7 +134,9 @@ class IndexedItemConnectivityView<ItemType,Face>
   using ItemLocalIdType = typename ItemType::LocalIdType;
   using ItemLocalIdViewType = typename BaseClass::ItemLocalIdViewType;
  public:
+  //! Nombre de faces connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE Int32 nbFace(ItemLocalIdType lid) const { return BaseClass::nbItem(lid); }
+  //! Liste des faces connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE ItemLocalIdViewType faces(ItemLocalIdType lid) const { return BaseClass::items(lid); }
 };
 
@@ -136,7 +154,9 @@ class IndexedItemConnectivityView<ItemType,Cell>
   using ItemLocalIdType = typename ItemType::LocalIdType;
   using ItemLocalIdViewType = typename BaseClass::ItemLocalIdViewType;
  public:
+  //! Nombre de mailles connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE Int32 nbCell(ItemLocalIdType lid) const { return BaseClass::nbItem(lid); }
+  //! Liste des mailles connectées à l'entité \a lid
   ARCCORE_HOST_DEVICE ItemLocalIdViewType cells(ItemLocalIdType lid) const { return BaseClass::items(lid); }
 };
 
