c4f472f2ed2995dc6e614646fcd71d4d6f854f59
Cedric Chevalier <cedric.chevalier@cea.fr>
2021-07-26 15:21:41 +0200

    Merge Alien's CMake into framework

--- src/cmake/LoadAlienConfigFile.cmake
+++
@@ -1,9 +0,0 @@
-configure_file(
-        ${PROJECT_SOURCE_DIR}/AlienConfig.h.in
-        ${PROJECT_BINARY_DIR}/alien/AlienConfig.h
-)
-
-install(
-        FILES ${PROJECT_BINARY_DIR}/alien/AlienConfig.h
-        DESTINATION include/alien
-)
