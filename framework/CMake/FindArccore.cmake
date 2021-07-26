e18a7a3a80b3ca4a1b89074ed32a5a095c019d91
Cedric Chevalier <cedric.chevalier@cea.fr>
2021-07-26 17:22:14 +0200

    Out of tree Arccore and Arccon
    variable ALIENDEV_FORCE_COMPILE_FRAMEWORK forces to compile Arccore and Arccon.
    Embedded Arccon and Arccore can be selected by setting ARCCON_SRC_DIR and ARCCORE_SRC_DIR.
    
    for @cedricga91

--- framework/CMake/FindArccore.cmake
+++ framework/CMake/FindArccore.cmake
@@ -1,6 +1,7 @@
-if (NOT ARCCORE_ROOT)
-   message(FATAL_ERROR "Variable 'ARCCON_ROOT' is not set")
+if(Arccore_FOUND)
+  return()
 endif()
+
 if (NOT ARCCORE_EXPORT_TARGET)
   # Indique qu'on souhaite exporter dans 'ArcaneTargets' les cibles des
   # définies dans 'Arccore'.
@@ -9,5 +10,5 @@ endif()
 
 # add directory only once !
 if (NOT TARGET arccore_full)
-  add_subdirectory(${ARCCORE_ROOT} arccore)
+  add_subdirectory(${ARCCORE_SRC_DIR} arccore)
 endif(NOT TARGET arccore_full)
