4b50372c6e9f7734dc843faa1bd0b025e88a7343
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-09-27 14:44:57 +0200

    [arcane,accelerator] Utilise dans l'implémentation thread des noyaux de calcul la parallélisation des boucles imbriquées.

--- arcane/src/arcane/accelerator/RunCommandLoop.h
+++ arcane/src/arcane/accelerator/RunCommandLoop.h
@@ -61,21 +61,31 @@ _applyGenericLoop(RunCommand& command,LoopBoundType<N> bounds,const Lambda& func
     break;
   case eExecutionPolicy::Thread:
     launch_info.beginExecute();
-    Integer my_size = CheckedConvert::toInteger(bounds.extent(0));
-    Integer lower_bound = CheckedConvert::toInteger(bounds.lowerBound(0));
-    ParallelLoopOptions loop_options;
-    Integer nb_thread = TaskFactory::nbAllowedThread();
-    if (nb_thread==0)
-      nb_thread = 1;
-    loop_options.setGrainSize(my_size/nb_thread);
-    //std::cout << "DO_PARALLEL_FOR n=" << my_size << "\n";
-    // TODO: implementer en utilisant une boucle 2D ou 3D qui existe dans TBB
-    // ou alors déterminer l'interval à la main
-    arcaneParallelFor(lower_bound,my_size,loop_options,[&](Integer begin, Integer size)
-    {
-      //std::cout << "DO_PARALLEL_FOR_IMPL begin=" << begin << " size=" << size << "\n";
-      impl::applyGenericLoopParallel(begin,begin+size,bounds,func);
-    });
+    if constexpr (N==1){
+      Integer my_size = CheckedConvert::toInteger(bounds.extent(0));
+      Integer lower_bound = CheckedConvert::toInteger(bounds.lowerBound(0));
+      ParallelLoopOptions loop_options;
+      Integer nb_thread = TaskFactory::nbAllowedThread();
+      if (nb_thread==0)
+        nb_thread = 1;
+      loop_options.setGrainSize(my_size/nb_thread);
+      //std::cout << "DO_PARALLEL_FOR n=" << my_size << "\n";
+      // TODO: implementer en utilisant une boucle 2D ou 3D qui existe dans TBB
+      // ou alors déterminer l'interval à la main
+      arcaneParallelFor(lower_bound,my_size,loop_options,[&](Integer begin, Integer size)
+      {
+        //std::cout << "DO_PARALLEL_FOR_IMPL begin=" << begin << " size=" << size << "\n";
+        impl::applyGenericLoopParallel(begin,begin+size,bounds,func);
+      });
+    }
+    else{
+      auto xfunc = [&] (const ComplexLoopRanges<N>& sub_bounds)
+                   {
+                     impl::applyGenericLoopSequential(sub_bounds,func);
+                   };
+      LambdaMDRangeFunctor<N,decltype(xfunc)> ipf(xfunc);
+      TaskFactory::executeParallelFor(bounds,&ipf);
+    }
     break;
   }
   launch_info.endExecute();
