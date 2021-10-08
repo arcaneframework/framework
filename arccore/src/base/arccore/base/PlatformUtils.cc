30cad89ef91be8390cbed9b2b39ac24f8371d17e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-10-08 13:55:31 +0200

    [arccore] Remove support for version of 'Glib2' older than 2.32.

--- arccore/src/base/arccore/base/PlatformUtils.cc
+++ arccore/src/base/arccore/base/PlatformUtils.cc
@@ -1,6 +1,6 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2020 IFPEN-CEA
+// Copyright 2000-2021 IFPEN-CEA
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* PlatformUtils.cc                                            (C) 2000-2017 */
+/* PlatformUtils.cc                                            (C) 2000-2021 */
 /*                                                                           */
 /* Fonctions utilitaires dépendant de la plateforme.                         */
 /*---------------------------------------------------------------------------*/
@@ -726,26 +726,6 @@ bool _getHasColorTerminal()
 extern "C++" ARCCORE_BASE_EXPORT void Platform::
 platformInitialize()
 {
-  // Initialise la glib.
-
-#if GLIB_CHECK_VERSION(2,32,0)
-  // A partir de 2.32.0, il ne faut rien faire pour initialiser les threads avec la glib.
-#else
-  // g_thread_init ne doit etre appelé qu'une seule fois.
-  // Si on est appelé depuis le runtime mono, l'init a déjà été
-  // faite. Il vaut mieux utiliser la fonction g_thread_get_initialized
-  // pour savoir si les threads ont déjà été initialisés mais
-  // cette version n'existe que à partir de la glib 2.20
-  // Pour info, sur RHEL6, la version est 2.28 et sur RHEL7 c'est 2.54
-#if GLIB_CHECK_VERSION(2,20,0)
-    if (!g_thread_get_initialized())
-      g_thread_init(0);
-#else
-    if (!g_thread_supported())
-      g_thread_init(0);
-#endif
-#endif
-
   // Pour l'instant, la seule initialisation spécifique dépend
   // des processeurs i386. Elle consiste à changer la valeur par
   // défaut des flags de la FPU pour générer une exception
