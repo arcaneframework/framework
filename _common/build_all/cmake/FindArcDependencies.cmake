a9da0c502f34157afab72bf35637f9f017969e4b
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2022-01-03 15:28:30 +0100

    [config] Affiche un message d'erreur explicite si on n'a pas cloné le sous-module 'dependencies'.
    Le message d'erreur indique la commande 'git' à réaliser pour cloner le
    sous-module.

--- _common/build_all/cmake/FindArcDependencies.cmake
+++ _common/build_all/cmake/FindArcDependencies.cmake
@@ -1,5 +1,25 @@
-if (NOT ARCDEPENDENCIES_ROOT)
-   message(FATAL_ERROR "Variable 'ARCDEPENDENCIES_ROOT' is not set")
+﻿if (NOT ARCDEPENDENCIES_ROOT)
+  message(FATAL_ERROR "Variable 'ARCDEPENDENCIES_ROOT' is not set")
 endif()
-add_subdirectory(${ARCDEPENDENCIES_ROOT} dependencies)
+
+# Vérifie que le répertoire 'dependencies' n'est pas vide.
+# Si c'est le cas cela signifie qu'on n'a pas cloner les sous-modules
+if (EXISTS "${ARCDEPENDENCIES_ROOT}/CMakeLists.txt")
+  add_subdirectory(${ARCDEPENDENCIES_ROOT} dependencies)
+else()
+  message(FATAL_ERROR
+    "Directory 'dependencies' is empty. "
+    "You need to clone this git submodule with the following command:\n"
+    "  cd ${CMAKE_SOURCE_DIR}\n"
+    "  git submodule update --init\n"
+    )
+endif()
+
 set(ArcDependencies_FOUND YES)
+
+# ----------------------------------------------------------------------------
+# Local Variables:
+# tab-width: 2
+# indent-tabs-mode: nil
+# coding: utf-8-with-signature
+# End:
