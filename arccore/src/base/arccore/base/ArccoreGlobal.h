5fb1172ab9177fe733e78624a9dad7f946d1fd96
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2022-01-04 15:41:31 +0100

    [arccore,base] Ajoute inclusion de 'cassert' lorsqu'on compile avec 'hipSYCL'.

--- arccore/src/base/arccore/base/ArccoreGlobal.h
+++ arccore/src/base/arccore/base/ArccoreGlobal.h
@@ -124,6 +124,11 @@ typedef ARCCORE_TYPE_INT64 Int64;
 #define ARCCORE_DEVICE_CODE
 #if defined(__CUDA_ARCH__)
 #define ARCCORE_DEVICE_TARGET_CUDA
+#if defined(__HIPSYCL__)
+// Nécessaire pour assert() par exemple dans arccoreCheckAt()
+// TODO: regarder si cela est aussi nécessaire pour AMD HIP.
+#include <cassert>
+#endif
 #endif
 #endif
 
