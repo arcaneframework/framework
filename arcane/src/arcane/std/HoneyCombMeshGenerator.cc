4c40d65eab8fc32a23fee1e3b4ec43ce410ec34b
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2022-02-08 14:35:11 +0100

    [arcane,std] Ne duplique plus les noeuds des mailles dans la création du maillage en nid d'abeille.

--- arcane/src/arcane/std/HoneyCombMeshGenerator.cc
+++ arcane/src/arcane/std/HoneyCombMeshGenerator.cc
@@ -16,8 +16,11 @@
 #include "arcane/IMeshBuilder.h"
 #include "arcane/IPrimaryMesh.h"
 #include "arcane/IItemFamily.h"
+#include "arcane/IParallelMng.h"
 
 #include "arcane/std/HoneyComb2DMeshGenerator_axl.h"
+
+#include <map>
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
@@ -27,20 +30,137 @@ namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
+/*!
+ *
+ * \brief Classe d'aide pour générer un maillage séquentiel en se basant
+ * sur la connectivité est les coordonnées des noeuds.
+ */
+class SimpleSequentialMeshBuilder
+: public TraceAccessor
+{
+ private:
+
+  class Real3Compare
+  {
+   public:
+
+    bool operator()(const Real3& a, const Real3& b) const
+    {
+      if (!math::isNearlyEqualWithEpsilon(a.x, b.x, m_epsilon))
+        return a.x < b.x;
+      if (!math::isNearlyEqualWithEpsilon(a.y, b.y, m_epsilon))
+        return a.y < b.y;
+      if (!math::isNearlyEqualWithEpsilon(a.z, b.z, m_epsilon))
+        return a.z < b.z;
+      return false;
+    }
+
+   private:
+
+    Real m_epsilon = 1e-14;
+  };
+
+ public:
+
+  explicit SimpleSequentialMeshBuilder(IMesh* pm)
+  : TraceAccessor(pm->traceMng())
+  , m_mesh(pm)
+  {
+  }
+
+ public:
+
+  //! Ajoute ou récupère l'uniqueId() du noeud de coordonnées \a coord.
+  Int32 addNode(Real3 coord)
+  {
+    auto p = m_nodes_coord_map.find(coord);
+    if (p != m_nodes_coord_map.end())
+      return p->second;
+    Int32 v = m_nodes_coordinates.size();
+    m_nodes_coordinates.add(coord);
+    m_nodes_coord_map.insert(std::make_pair(coord, v));
+    return v;
+  }
+
+  Int32 addCell(Int32 type, ConstArrayView<Real3> nodes_coords)
+  {
+    m_cells_infos.add(type);
+    const Int32 v = m_nb_cell;
+    m_cells_infos.add(v);
+    ++m_nb_cell;
+    for (Real3 coord : nodes_coords)
+      m_cells_infos.add(addNode(coord));
+    return v;
+  }
+  ConstArrayView<Int64> cellsInfos() const { return m_cells_infos; }
+  Int32 nbCell() const { return m_nb_cell; }
+
+  void setNodesCoordinates() const
+  {
+    Int32 nb_node = m_nodes_coordinates.size();
+    UniqueArray<Int64> unique_ids(nb_node);
+    for (Integer i = 0; i < nb_node; ++i)
+      unique_ids[i] = i;
+    UniqueArray<Int32> local_ids(nb_node);
+    IItemFamily* node_family = m_mesh->nodeFamily();
+    node_family->itemsUniqueIdToLocalId(local_ids, unique_ids);
+
+    VariableNodeReal3& mesh_nodes_coordinates(m_mesh->nodesCoordinates());
+    ItemInternalList nodes(node_family->itemsInternal());
+
+    for (Integer i = 0; i < nb_node; ++i) {
+      Node node{ nodes[local_ids[i]] };
+      mesh_nodes_coordinates[node] = m_nodes_coordinates[i];
+    }
+  }
+
+ private:
+
+  using CoordMap = std::map<Real3, Int32, Real3Compare>;
+
+  IMesh* m_mesh = nullptr;
+  CoordMap m_nodes_coord_map;
+  Int32 m_nb_cell = 0;
+  UniqueArray<Int64> m_cells_infos;
+  //! Correspondante uid->coord pour les noeuds
+  UniqueArray<Real3> m_nodes_coordinates;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 
 class HoneyComb2DMeshGenerator
 : public TraceAccessor
 {
  public:
 
-  HoneyComb2DMeshGenerator(IPrimaryMesh* mesh);
+  //! Infos sur une ligne d'hexagone
+  class CellLineInfo
+  {
+   public:
+
+    CellLineInfo(Real2 first_center, Int32 nb_cell)
+    : m_first_center(first_center)
+    , m_nb_cell(nb_cell)
+    {}
+
+   public:
+
+    Real2 m_first_center;
+    Int32 m_nb_cell;
+  };
 
  public:
 
-  void generateMesh(Real pitch, Integer nb_ring)
+  explicit HoneyComb2DMeshGenerator(IPrimaryMesh* mesh);
+
+ public:
+
+  void generateMesh(Real2 origin, Real pitch, Integer nb_ring)
   {
     m_pitch = pitch;
     m_nb_ring = nb_ring;
+    m_origin = origin;
     _buildCells();
   }
 
@@ -49,11 +169,13 @@ class HoneyComb2DMeshGenerator
   IPrimaryMesh* m_mesh = nullptr;
   Real m_pitch = 0.0;
   Integer m_nb_ring = 0;
+  Real2 m_origin;
 
  private:
 
-  void _buildCellNodes(Real x, Real y, ArrayView<Real2> coords);
+  void _buildCellNodes(Real x, Real y, ArrayView<Real3> coords);
   void _buildCells();
+  void _buildCells2();
 };
 
 /*---------------------------------------------------------------------------*/
@@ -70,17 +192,17 @@ HoneyComb2DMeshGenerator(IPrimaryMesh* mesh)
 /*---------------------------------------------------------------------------*/
 
 void HoneyComb2DMeshGenerator::
-_buildCellNodes(Real x, Real y, ArrayView<Real2> coords)
+_buildCellNodes(Real x, Real y, ArrayView<Real3> coords)
 {
   Real pitch = m_pitch;
   Real p = 0.5 * pitch;
   Real q = 0.5 * pitch / (math::sqrt(3.0));
-  coords[0] = { x + q, y + p };
-  coords[1] = { x - q, y + p };
-  coords[2] = { x - 2 * q, y };
-  coords[3] = { x - q, y - p };
-  coords[4] = { x + q, y - p };
-  coords[5] = { x + 2 * q, y };
+  coords[0] = { x + q, y + p, 0.0 };
+  coords[1] = { x - q, y + p, 0.0 };
+  coords[2] = { x - 2 * q, y, 0.0 };
+  coords[3] = { x - q, y - p, 0.0 };
+  coords[4] = { x + q, y - p, 0.0 };
+  coords[5] = { x + 2 * q, y, 0.0 };
 }
 
 /*---------------------------------------------------------------------------*/
@@ -89,14 +211,28 @@ _buildCellNodes(Real x, Real y, ArrayView<Real2> coords)
 void HoneyComb2DMeshGenerator::
 _buildCells()
 {
-  // Construit les mailles
-  // Pour l'instant les noeuds sont dupliqués entre les mailles
+  // En parallèle, seul le rang 0 construit le maillage
+  IParallelMng* pm = m_mesh->parallelMng();
+  if (pm->commRank() == 0)
+    _buildCells2();
+  else {
+    m_mesh->setDimension(2);
+    m_mesh->allocateCells(0, Int64ConstArrayView(), true);
+  }
+}
 
-  UniqueArray<Real2> cells_center;
+void HoneyComb2DMeshGenerator::
+_buildCells2()
+{
+  SimpleSequentialMeshBuilder simple_mesh_builder(m_mesh);
+
   const Real pitch = m_pitch;
   const Integer nb_ring = m_nb_ring;
 
   info() << "Build Hexagonal 2D Cells nb_layer=" << nb_ring;
+
+  UniqueArray<Real3> cell_nodes_coords(6);
+  UniqueArray<CellLineInfo> cells_line_info;
 
   // u1, u2 and u3 are the directional vectors following the
   // directions of the hexagon
@@ -104,60 +240,39 @@ _buildCells()
   Real2 u2(0.5 * pitch * math::sqrt(3.0), 0.5 * pitch);
   Real2 u3(pitch * 0.0, pitch * 1.0);
 
+  // Mailles au dessus du centre
   for (Integer i = (-nb_ring + 1); i < 1; ++i) {
     Real x0 = u3[0] * i - u1[0] * (nb_ring - 1);
     Real y0 = u3[1] * i - u1[1] * (nb_ring - 1);
-    Integer numberOfCellsPerLine = 2 * nb_ring - 1 + i;
-    //for j in range(numberOfCellsPerLine):
-    for (Integer j = 0; j < numberOfCellsPerLine; ++j) {
-      cells_center.add({ x0, y0 });
-      x0 += u1[0];
-      y0 += u1[1];
-      //info() << "ADD_CELL xy=" << cells_center.back();
-    }
+    Real2 pos(x0, y0);
+    pos += m_origin;
+    Integer nb_cell_in_line = 2 * nb_ring - 1 + i;
+    cells_line_info.add(CellLineInfo(pos, nb_cell_in_line));
   }
 
+  // Mailles en dessous du centre
   for (Integer i = 1; i < nb_ring; ++i) {
     Real x0{ u2[0] * i - u1[0] * (nb_ring - 1) };
     Real y0{ u2[1] * i - u1[1] * (nb_ring - 1) };
-    Integer numberOfCellsPerLine = 2 * nb_ring - 1 - i;
-    for (Integer j = 0; j < numberOfCellsPerLine; ++j) {
-      cells_center.add({ x0, y0 });
-      x0 += u1[0];
-      y0 += u1[1];
-      //info() << "ADD_CELL xy=" << cells_center.back();
+    Real2 pos(x0, y0);
+    pos += m_origin;
+    Integer nb_cell_in_line = 2 * nb_ring - 1 - i;
+    cells_line_info.add(CellLineInfo(pos, nb_cell_in_line));
+  }
+
+  for (const CellLineInfo& cli : cells_line_info) {
+    Real2 xy = cli.m_first_center;
+    for (Integer j = 0, n = cli.m_nb_cell; j < n; ++j) {
+      _buildCellNodes(xy.x, xy.y, cell_nodes_coords);
+      simple_mesh_builder.addCell(IT_Hexagon6, cell_nodes_coords);
+      xy += u1;
     }
   }
 
-  // Créé les mailles (les noeuds sont dupliqués)
-  Integer nb_cell = cells_center.size();
-  UniqueArray<Int64> cells_infos;
-  cells_infos.reserve(nb_cell * 8);
-  for (Integer i = 0; i < nb_cell; ++i) {
-    // Type
-    cells_infos.add(IT_Hexagon6);
-    // UID
-    cells_infos.add(i);
-    for (Integer j = 0; j < 6; ++j)
-      cells_infos.add((i * 6) + j);
-  }
   m_mesh->setDimension(2);
-  m_mesh->allocateCells(nb_cell, cells_infos, true);
+  m_mesh->allocateCells(simple_mesh_builder.nbCell(), simple_mesh_builder.cellsInfos(), true);
 
-  // Positionne les coordonnées des noeuds
-  {
-    VariableNodeReal3& nodes_coordinates(m_mesh->nodesCoordinates());
-    ItemInternalList cells(m_mesh->cellFamily()->itemsInternal());
-    UniqueArray<Real2> coords(6);
-    // Suppose qu'on n'a pas changé l'ordre entre les localId() et uniqueId()
-    for (Integer i = 0; i < nb_cell; ++i) {
-      Real2 center = cells_center[i];
-      Cell cell{ cells[i] };
-      _buildCellNodes(center.x, center.y, coords);
-      for (Integer j = 0; j < 6; ++j)
-        nodes_coordinates[cell.node(j)] = Real3(coords[j].x, coords[j].y, 0.0);
-    }
-  }
+  simple_mesh_builder.setNodesCoordinates();
 }
 
 /*---------------------------------------------------------------------------*/
@@ -170,7 +285,7 @@ class HoneyComb2DMeshGeneratorService
 {
  public:
 
-  HoneyComb2DMeshGeneratorService(const ServiceBuildInfo& sbi)
+  explicit HoneyComb2DMeshGeneratorService(const ServiceBuildInfo& sbi)
   : ArcaneHoneyComb2DMeshGeneratorObject(sbi)
   {}
 
@@ -189,7 +304,7 @@ class HoneyComb2DMeshGeneratorService
       ARCANE_FATAL("Invalid valid value '{0}' for pitch (should be > 0.0)", pitch);
     if (nb_layer <= 0)
       ARCANE_FATAL("Invalid valid value '{0}' for 'nb-layer' (should be > 0)", nb_layer);
-    g.generateMesh(pitch, nb_layer);
+    g.generateMesh(options()->origin(), pitch, nb_layer);
   }
 
  private:
