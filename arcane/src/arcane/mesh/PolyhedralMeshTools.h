7ea8b2c81f002531349aea3805746627f85b76ff
sdcm <stephane.de-chaisemartin@ifpen.fr>
2022-06-22 10:11:42 +0200

    [mesh] Add cell to nodes connectivity in polyhedral vtk reader.

--- arcane/src/arcane/mesh/PolyhedralMeshTools.h
+++ arcane/src/arcane/mesh/PolyhedralMeshTools.h
@@ -53,11 +53,16 @@ namespace PolyhedralMeshTools
 
     Integer nbNodes();
 
+    Arcane::Int64ConstArrayView cellNodes();
+
+    Int32ConstArrayView cellNbNodes();
+
    private:
     const String& m_filename;
     vtkNew<vtkUnstructuredGridReader> m_vtk_grid_reader;
-    UniqueArray<Int64> m_cell_uids, m_node_uids, m_face_uids, m_edge_uids, m_face_node_uids, m_edge_node_uids;
-    UniqueArray<Int32> m_face_nb_nodes;
+    UniqueArray<Int64> m_cell_uids, m_node_uids, m_face_uids, m_edge_uids;
+    UniqueArray<Int64> m_face_node_uids, m_edge_node_uids, m_cell_node_uids;
+    UniqueArray<Int32> m_face_nb_nodes, m_cell_nb_nodes;
 
     bool _findFace(UniqueArray<Int64> face_nodes);
     bool _findEdge(UniqueArray<Int64> edge_nodes);
