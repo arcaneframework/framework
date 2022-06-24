acfb425a62beef3d89c9fa4bcb2b158304215cd2
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2022-06-24 12:39:40 +0200

    [arcane,materials] Ajoute include de '.h' pour compatibilité avec l'existant.

--- arcane/ceapart/src/arcane/materials/IMeshMaterialMng.h
+++ arcane/ceapart/src/arcane/materials/IMeshMaterialMng.h
@@ -1,2 +1,4 @@
 ﻿#include "arcane/materials/MaterialsGlobal.h"
 #include "arcane/core/materials/IMeshMaterialMng.h"
+// Ajoute pour compatibilité avec l'existant mais ce n'est pas utile normalement
+#include "arcane/materials/IMeshBlock.h"
