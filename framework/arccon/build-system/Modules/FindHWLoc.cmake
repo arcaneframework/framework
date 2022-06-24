4eb0fd5a6b56da1272ad9924a1996b08e7e04660
Cédric Chevalier <cedric.chevalier@cea.fr>
2022-06-24 16:09:05 +0200

    [all] Remove arccon (#152)

--- framework/arccon/build-system/Modules/FindHWLoc.cmake
+++
@@ -1,2 +0,0 @@
-# Find the HWLoc library
-arccon_find_legacy_package(NAME HWLoc LIBRARIES hwloc HEADERS hwloc.h)
--- framework/arccon/build-system/Modules/FindOtf2.cmake
+++
@@ -1,3 +0,0 @@
-# Find the OTF2 includes and library
-include(${CMAKE_CURRENT_LIST_DIR}/../commands/commands.cmake)
-arccon_find_legacy_package(NAME Otf2 LIBRARIES otf2 HEADERS otf2/otf2.h)
