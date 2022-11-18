9fb6e78e15455710cb25f5213f7fb0b5f561793c
guignont <thomas.guignon@ifpen.fr>
2022-11-18 13:56:57 +0100

    - add csr redistributor tests

--- test/Tests/Options.h
+++ test/Tests/Options.h
@@ -15,23 +15,36 @@ options(int argc, char** argv, boost::program_options::options_description& opti
 {
   boost::program_options::options_description desc;
 
-  desc.add_options()("solver-package",
-      boost::program_options::value<std::string>()->default_value("petsc"),
-      "solver package name")("solver",
-      boost::program_options::value<std::string>()->default_value("bicgs"),
-      "solver algo name")("precond",
-      boost::program_options::value<std::string>()->default_value("none"),
-      "preconditioner id diag ilu ddml poly")("max-iter",
-      boost::program_options::value<int>()->default_value(1000), "max iterations")("tol",
-      boost::program_options::value<double>()->default_value(1.e-10),
-      "solver tolerance")("niter", boost::program_options::value<int>()->default_value(1),
-      "nb of tests for perf measure")("kernel",
-      boost::program_options::value<std::string>()->default_value("CPUCBLAS"),
-      "mcgsolver kernel name")("output-level",
-      boost::program_options::value<int>()->default_value(0),
-      "output level")("redist-strategy",
-      boost::program_options::value<std::string>()->default_value("pair"),
-      "redistribution strategy: valid values are : pair or unique");
+  desc.add_options()
+      ("solver-package",
+        boost::program_options::value<std::string>()->default_value("petsc"),
+        "solver package name")
+      ("solver",
+        boost::program_options::value<std::string>()->default_value("bicgs"),
+        "solver algo name")
+      ("precond",
+        boost::program_options::value<std::string>()->default_value("none"),
+        "preconditioner id diag ilu ddml poly")
+      ("max-iter",
+        boost::program_options::value<int>()->default_value(1000), "max iterations")
+      ("tol",
+        boost::program_options::value<double>()->default_value(1.e-10),
+        "solver tolerance")
+      ("niter",
+        boost::program_options::value<int>()->default_value(1),
+        "nb of tests for perf measure")
+      ("kernel",
+        boost::program_options::value<std::string>()->default_value("CPUCBLAS"),
+        "mcgsolver kernel name")
+      ("output-level",
+        boost::program_options::value<int>()->default_value(0),
+        "output level")
+      ("redist-strategy",
+        boost::program_options::value<std::string>()->default_value("pair"),
+        "redistribution strategy: valid values are : pair or unique")
+      ("redist-method",
+        boost::program_options::value<std::string>()->default_value("dok"),
+        "redistribution method: valid values are : dok or csr");
 
   // Ajout des options de configuration des solveurs linéaires
   options.add(desc);
