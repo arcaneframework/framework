ac1aff509e994b625d0e20b93c19b27f02e52397
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2022-12-13 08:40:02 +0100

    [arcane,tests] Ajoute test pour les variable multi-dim à 3 dimensions.

--- arcane/src/arcane/tests/MDVariableUnitTest.cc
+++ arcane/src/arcane/tests/MDVariableUnitTest.cc
@@ -37,9 +37,9 @@ class ItemVariableArrayAsMDRefT;
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-template <typename ItemType, typename DataType, typename MDDimType>
+template <typename ItemType, typename DataType, typename Extents>
 class ItemVariableArrayAsMDRefDynamicBaseT
-: public VariableRef
+: public MeshVariableRef
 {
  private:
 
@@ -67,21 +67,22 @@ class ItemVariableArrayAsMDRefDynamicBaseT
  public:
 
   using UnderlyingVariableType = MeshVariableArrayRefT<ItemType, DataType>;
-  using MDSpanType = MDSpan<DataType, MDDimType, RightLayout>;
+  using MDSpanType = MDSpan<DataType, Extents, RightLayout>;
   using ItemLocalIdType = typename ItemType::LocalIdType;
 
  public:
 
   explicit ItemVariableArrayAsMDRefDynamicBaseT(const VariableBuildInfo& b)
-  : VariableRef()
+  : MeshVariableRef()
   , m_underlying_var(b)
   {
-    _internalAssignVariable(m_underlying_var);
+    _internalInit(m_underlying_var.variable());
   }
 
   UnderlyingVariableType& underlyingVariable() { return m_underlying_var; }
 
  protected:
+ public:
 
   void updateFromInternal() override
   {
@@ -89,11 +90,30 @@ class ItemVariableArrayAsMDRefDynamicBaseT
     // associée sur les entités n'est pas forcément remise à jour.
     IData* data = this->m_underlying_var.variable()->data();
     ArrayShape shape = data->shape();
-    //std::cout << "SHAPE=" << shape.dimensions() << "\n";
+    const Int32 nb_rank = Extents::rank();
+    //std::cout << "SHAPE=" << shape.dimensions() << " internal_rank=" << nb_rank << "\n";
     auto* true_data = m_underlying_var.trueData();
     auto array_view = true_data->view();
     Int32 dim0_size = array_view.dim1Size();
-    m_mdspan = MDSpanType(array_view.data(), { dim0_size, shape.dimension(0), shape.dimension(1) });
+
+    ArrayShape shape_with_item;
+    shape_with_item.setNbDimension(nb_rank);
+    shape_with_item.setDimension(0, dim0_size);
+    Int32 nb_orig_shape = shape.nbDimension();
+    for (Int32 i = 0; i < nb_orig_shape; ++i) {
+      shape_with_item.setDimension(i + 1, shape.dimension(i));
+    }
+    // Si la forme est plus petite que notre rang, remplit les dimensions
+    // supplémentaires par la valeur 1.
+    for (Int32 i = (nb_orig_shape + 1); i < nb_rank; ++i) {
+      shape_with_item.setDimension(i, 1);
+    }
+
+    //new_extents = ArrayExtentsBase<Extents>::fromSpan(shape.dimensions());
+    ArrayExtents<Extents> new_extents = ArrayExtentsBase<Extents>::fromSpan(shape_with_item.dimensions());
+    //m_mdspan = impl::_buildSpan<Rank, MDSpanType>(array_view.data(), dim0_size, shape);
+    // MDSpanType(array_view.data(), { dim0_size, shape.dimension(0), shape.dimension(1) });
+    m_mdspan = MDSpanType(array_view.data(), new_extents);
   }
 
  protected:
@@ -105,11 +125,11 @@ class ItemVariableArrayAsMDRefDynamicBaseT
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-template <typename ItemType, typename DataType>
-class ItemVariableArrayAsMDRefT<ItemType, DataType, MDDim2>
-: public ItemVariableArrayAsMDRefDynamicBaseT<ItemType, DataType, typename MDDim2::AddedFirstExtentsType<DynExtent>>
+template <typename ItemType, typename DataType, typename Extents>
+class ItemVariableArrayAsMDRefT
+: public ItemVariableArrayAsMDRefDynamicBaseT<ItemType, DataType, typename Extents::AddedFirstExtentsType<DynExtent>>
 {
-  using AddedFirstExtentsType = typename MDDim2::AddedFirstExtentsType<DynExtent>;
+  using AddedFirstExtentsType = typename Extents::AddedFirstExtentsType<DynExtent>;
 
  public:
 
@@ -125,22 +145,37 @@ class ItemVariableArrayAsMDRefT<ItemType, DataType, MDDim2>
 
  public:
 
+  template <typename X = Extents, typename = std::enable_if_t<X::rank() == 2, void>>
   DataType& operator()(ItemLocalIdType id, Int32 i1, Int32 i2)
   {
     return this->m_mdspan(id.localId(), i1, i2);
   }
+
+  template <typename X = Extents, typename = std::enable_if_t<X::rank() == 2, void>>
   const DataType& operator()(ItemLocalIdType id, Int32 i1, Int32 i2) const
   {
     return this->m_mdspan(id.localId(), i1, i2);
   }
 
+  template <typename X = Extents, typename = std::enable_if_t<X::rank() == 3, void>>
+  DataType& operator()(ItemLocalIdType id, Int32 i, Int32 j, Int32 k)
+  {
+    return this->m_mdspan(id.localId(), i, j, k);
+  }
+
+  template <typename X = Extents, typename = std::enable_if_t<X::rank() == 3, void>>
+  const DataType& operator()(ItemLocalIdType id, Int32 i, Int32 j, Int32 k) const
+  {
+    return this->m_mdspan(id.localId(), i, j, k);
+  }
+
+  template <typename X = Extents, typename = std::enable_if_t<X::nb_dynamic == 2, void>>
   void reshape(Int32 dim1, Int32 dim2)
   {
     ArrayShape shape;
     shape.setNbDimension(2);
     shape.setDimension(0, dim1);
     shape.setDimension(1, dim2);
-    //this->m_underlying_var.variable()->data()->setShape(shape);
     this->m_underlying_var.resizeAndReshape(shape);
   }
 };
@@ -213,6 +248,7 @@ _testCustomVariable()
 {
   info() << "TEST CUSTOM VARIABLE";
   using MyVariableRef2 = ItemVariableArrayAsMDRefT<Cell, Real, MDDim2>;
+  using MyVariableRef3 = ItemVariableArrayAsMDRefT<Cell, Real, MDDim3>;
 
   MyVariableRef2 my_var(VariableBuildInfo(mesh(), "TestCustomVar"));
   my_var.reshape(3, 4);
@@ -221,7 +257,13 @@ _testCustomVariable()
     my_var(icell, 1, 2) = 3.0;
     Real x = my_var(icell, 1, 2);
     if (x != 3.0)
-      ARCANE_FATAL("Bad value");
+      ARCANE_FATAL("Bad value (2)");
+  }
+  MyVariableRef3 my_var3(VariableBuildInfo(mesh(), "TestCustomVar"));
+  ENUMERATE_ (Cell, icell, allCells()) {
+    Real x = my_var3(icell, 1, 2, 0);
+    if (x != 3.0)
+      ARCANE_FATAL("Bad value (3)");
   }
 }
 
