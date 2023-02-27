4924fd34715e809e90f2a5bcf20b7d1311097ea5
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-02-27 14:30:17 +0100

    [arcane,impl] Utilise 'MemoryView' dans les implémentation de 'IBufferCopier'.

--- arcane/src/arcane/impl/IBufferCopier.h
+++ arcane/src/arcane/impl/IBufferCopier.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* IBufferCopier.h                                             (C) 2000-2021 */
+/* IBufferCopier.h                                             (C) 2000-2023 */
 /*                                                                           */
 /* Interface pour la copie de buffer.                                        */
 /*---------------------------------------------------------------------------*/
@@ -15,7 +15,8 @@
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#include "arcane/GroupIndexTable.h"
+#include "arcane/utils/MemoryView.h"
+#include "arcane/core/GroupIndexTable.h"
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
@@ -57,44 +58,31 @@ class IBufferCopier
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
 template<typename SimpleType>
 class DirectBufferCopier
 : public IBufferCopier<SimpleType>
 {
  public:
-  
+
   void copyFromBufferOne(Int32ConstArrayView indexes,
                          ConstArrayView<SimpleType> buffer,
                          ArrayView<SimpleType> var_value) override
   {
-    Integer n = indexes.size();
-#ifdef ARCANE_CHECK
-    for( Integer i=0; i<n; ++i )
-      var_value[indexes[i]] = buffer[i];
-#else
-    const SimpleType* ARCANE_RESTRICT r_buf = buffer.data();
-    const Int32* ARCANE_RESTRICT r_idx = indexes.data();
-    SimpleType* ARCANE_RESTRICT r_var_value = var_value.data();
-    for( Integer i=0; i<n; ++i )
-      r_var_value[r_idx[i]] = r_buf[i];
-#endif 
+    MemoryView from(buffer);
+    MutableMemoryView to(var_value);
+    from.copyToIndexesHost(to,indexes);
   }
-  
+
   void copyToBufferOne(Int32ConstArrayView indexes,
                        ArrayView<SimpleType> buffer,
                        ConstArrayView<SimpleType> var_value) override
-  { 
-    Integer n = indexes.size();
-#ifdef ARCANE_CHECK
-    for( Integer i=0; i<n; ++i )
-      buffer[i] = var_value[indexes[i]];
-#else
-    SimpleType* ARCANE_RESTRICT r_buf = buffer.data();
-    const Int32* ARCANE_RESTRICT r_idx = indexes.data();
-    const SimpleType* ARCANE_RESTRICT r_var_value = var_value.data();
-    for( Integer i=0; i<n; ++i )
-      r_buf[i] = r_var_value[r_idx[i]];
-#endif
+  {
+    MemoryView from(var_value);
+    MutableMemoryView to(buffer);
+    to.copyFromIndexesHost(from,indexes);
   }
 
   void copyFromBufferMultiple(Int32ConstArrayView indexes,
@@ -102,13 +90,9 @@ class DirectBufferCopier
                               ArrayView<SimpleType> var_value,
                               Integer dim2_size) override
   {
-    Integer n = indexes.size();
-    for( Integer i=0; i<n; ++i ){
-      Integer zindex = i*dim2_size;
-      Integer zci = indexes[i]*dim2_size;
-      for( Integer z=0; z<dim2_size; ++ z )
-        var_value[zci+z] = buffer[zindex+z];
-    }
+    MemoryView from(buffer,dim2_size);
+    MutableMemoryView to(var_value,dim2_size);
+    from.copyToIndexesHost(to,indexes);
   }
 
   void copyToBufferMultiple(Int32ConstArrayView indexes,
@@ -116,13 +100,9 @@ class DirectBufferCopier
                             ConstArrayView<SimpleType> var_value,
                             Integer dim2_size) override
   {
-    Integer n = indexes.size();
-    for( Integer i=0; i<n; ++i ){
-      Integer zindex = i*dim2_size;
-      Integer zci = indexes[i]*dim2_size;
-      for( Integer z=0; z<dim2_size; ++ z )
-        buffer[zindex+z] = var_value[zci+z];
-    }
+    MemoryView from(var_value,dim2_size);
+    MutableMemoryView to(buffer,dim2_size);
+    to.copyFromIndexesHost(from,indexes);
   }
 };
 
@@ -133,44 +113,36 @@ template<typename SimpleType>
 class TableBufferCopier
 : public IBufferCopier<SimpleType>
 {
-public:
-  
+ public:
+
   TableBufferCopier(GroupIndexTable* table) : m_table(table) {}
-  ~TableBufferCopier() {}
 
   void copyFromBufferOne(Int32ConstArrayView indexes,
                          ConstArrayView<SimpleType> buffer,
                          ArrayView<SimpleType> var_value) override
   {
-    Integer n = indexes.size();
-    GroupIndexTable& table = *m_table;
-    for( Integer i=0; i<n; ++i ) 
-      var_value[table[indexes[i]]] = buffer[i];
+    UniqueArray<Int32> final_indexes;
+    _buildFinalIndexes(final_indexes,indexes);
+    m_base_copier.copyFromBufferOne(final_indexes,buffer,var_value);
   }
-  
+
   void copyToBufferOne(Int32ConstArrayView indexes,
                        ArrayView<SimpleType> buffer,
                        ConstArrayView<SimpleType> var_value) override
-  { 
-    Integer n = indexes.size();
-    GroupIndexTable& table = *m_table;
-    for( Integer i=0; i<n; ++i )
-      buffer[i] = var_value[table[indexes[i]]];
+  {
+    UniqueArray<Int32> final_indexes;
+    _buildFinalIndexes(final_indexes,indexes);
+    m_base_copier.copyToBufferOne(final_indexes,buffer,var_value);
   }
-   
+
   void copyFromBufferMultiple(Int32ConstArrayView indexes,
                               ConstArrayView<SimpleType> buffer,
                               ArrayView<SimpleType> var_value,
                               Integer dim2_size) override
   {
-    Integer n = indexes.size();
-    GroupIndexTable& table = *m_table;
-    for( Integer i=0; i<n; ++i ){
-      Integer zindex = i*dim2_size;
-      Integer zci = table[indexes[i]]*dim2_size;
-      for( Integer z=0; z<dim2_size; ++ z )
-        var_value[zci+z] = buffer[zindex+z];
-    }
+    UniqueArray<Int32> final_indexes;
+    _buildFinalIndexes(final_indexes,indexes);
+    m_base_copier.copyFromBufferMultiple(final_indexes,buffer,var_value,dim2_size);
   }
 
   void copyToBufferMultiple(Int32ConstArrayView indexes,
@@ -178,17 +150,28 @@ public:
                             ConstArrayView<SimpleType> var_value,
                             Integer dim2_size) override
   {
-    Integer n = indexes.size();
-    GroupIndexTable& table = *m_table;
-    for( Integer i=0; i<n; ++i ){
-      Integer zindex = i*dim2_size;
-      Integer zci = table[indexes[i]]*dim2_size;
-      for( Integer z=0; z<dim2_size; ++ z )
-        buffer[zindex+z] = var_value[zci+z];
-    }
+    UniqueArray<Int32> final_indexes;
+    _buildFinalIndexes(final_indexes,indexes);
+    m_base_copier.copyToBufferMultiple(final_indexes,buffer,var_value,dim2_size);
   }
+
  private:
+
   GroupIndexTable* m_table;
+  DirectBufferCopier<SimpleType> m_base_copier;
+
+ private:
+
+  void _buildFinalIndexes(Array<Int32>& final_indexes,ConstArrayView<Int32> orig_indexes)
+  {
+    // TODO: utiliser des buffer de taille fixe pour ne pas avoir à faire
+    // d'allocation
+    GroupIndexTable& table = *m_table;
+    Int32 n = orig_indexes.size();
+    final_indexes.resize(n);
+    for( Int32 i=0; i<n; ++i )
+      final_indexes[i] = table[orig_indexes[i]];
+  }
 };
 
 /*---------------------------------------------------------------------------*/
