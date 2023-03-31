07f531430f269047a44db82295a4d75818393e90
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2023-03-31 14:24:32 +0200

    [arcane,std] Add support of name symbols in output directory name

--- arcane/src/arcane/std/SimpleTableWriterHelper.h
+++ arcane/src/arcane/std/SimpleTableWriterHelper.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* SimpleTableWriterHelper.h                                   (C) 2000-2022 */
+/* SimpleTableWriterHelper.h                                   (C) 2000-2023 */
 /*                                                                           */
 /* Classe permettant d'écrire un SimpleTableInternal dans un fichier.        */
 /* Simplifie l'utilisation de l'écrivain en gérant le multiprocessus et les  */
@@ -39,6 +39,7 @@ class SimpleTableWriterHelper
 : public ISimpleTableWriterHelper
 {
  public:
+
   SimpleTableWriterHelper(const Ref<ISimpleTableReaderWriter>& simple_table_reader_writer)
   : m_simple_table_internal(simple_table_reader_writer->internal())
   , m_simple_table_reader_writer(simple_table_reader_writer)
@@ -46,7 +47,9 @@ class SimpleTableWriterHelper
   , m_name_table_without_computation("")
   , m_root()
   , m_name_table_computed(false)
-  , m_name_table_once_process(false)
+  , m_name_output_directory_computed(false)
+  , m_name_table_one_file_by_ranks_permited(false)
+  , m_name_output_directory_one_file_by_ranks_permited(false)
   {
     if (simple_table_reader_writer.isNull())
       ARCANE_FATAL("La réference passée en paramètre est Null.");
@@ -59,13 +62,16 @@ class SimpleTableWriterHelper
   , m_name_table_without_computation("")
   , m_root()
   , m_name_table_computed(false)
-  , m_name_table_once_process(false)
+  , m_name_output_directory_computed(false)
+  , m_name_table_one_file_by_ranks_permited(false)
+  , m_name_output_directory_one_file_by_ranks_permited(false)
   {
   }
 
   virtual ~SimpleTableWriterHelper() = default;
 
  public:
+
   bool init(const Directory& root_directory, const String& table_name, const String& directory_name) override;
 
   void print(Integer rank) override;
@@ -104,19 +110,30 @@ class SimpleTableWriterHelper
   void setReaderWriter(const Ref<ISimpleTableReaderWriter>& simple_table_reader_writer) override;
 
  protected:
-  void _computeName();
+
+  void _computeTableName();
+  void _computeOutputDirectory();
+  String _computeName(String name, bool& one_file_by_ranks_permited);
 
  protected:
+
   Ref<SimpleTableInternal> m_simple_table_internal;
   Ref<ISimpleTableReaderWriter> m_simple_table_reader_writer;
 
   String m_name_output_directory;
+  String m_name_output_directory_without_computation;
   String m_name_table_without_computation;
 
   Directory m_root;
 
   bool m_name_table_computed;
-  bool m_name_table_once_process;
+  bool m_name_output_directory_computed;
+
+  // Booleens permettant de savoir si le nom de fichier ou le nom de dossier
+  // possède le symbole "@proc_id@" qui permet l'écriture de fichiers par
+  // plusieurs processus.
+  bool m_name_table_one_file_by_ranks_permited;
+  bool m_name_output_directory_one_file_by_ranks_permited;
 };
 
 /*---------------------------------------------------------------------------*/
