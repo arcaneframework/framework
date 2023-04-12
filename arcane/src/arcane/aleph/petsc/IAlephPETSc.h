59108778a821788602ed4501805d27a14af2c1b4
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-04-12 13:45:17 +0200

    [arcane,aleph/petsc] Nettoyage de 'IAlephPETSc.h' et 'IAlephPETSc.cc'.

--- arcane/src/arcane/aleph/petsc/IAlephPETSc.h
+++ arcane/src/arcane/aleph/petsc/IAlephPETSc.h
@@ -4,23 +4,30 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
-/*****************************************************************************
- * IAlephPETSc.h                                                    (C) 2013 *
- *****************************************************************************/
-#ifndef _ALEPH_INTERFACE_PETSC_H_
-#define _ALEPH_INTERFACE_PETSC_H_
+/*---------------------------------------------------------------------------*/
+/* IAlephPETSc.h                                               (C) 2000-2023 */
+/*                                                                           */
+/* Interface de Aleph pour PETSc.                                            */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCANE_ALEPH_IALEPHPETSC_H
+#define ARCANE_ALEPH_IALEPHPETSC_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_BEGIN_NAMESPACE
+namespace Arcane
+{
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-class AlephTopologyPETSc : public IAlephTopology
+class AlephTopologyPETSc
+: public IAlephTopology
 {
  public:
+
   AlephTopologyPETSc(ITraceMng* tm,
                      AlephKernel* kernel,
                      Integer index,
@@ -40,94 +47,104 @@ class AlephTopologyPETSc : public IAlephTopology
     }
     PetscInitializeNoArguments();
   }
-  ~AlephTopologyPETSc()
+  ~AlephTopologyPETSc() override
   {
     debug() << "\33[1;5;32m\t\t\t[~AlephTopologyPETSc]\33[0m";
   }
 
  public:
-  void backupAndInitialize() {}
-  void restore() {}
+
+  void backupAndInitialize() override {}
+  void restore() override {}
 };
 
-class AlephVectorPETSc : public IAlephVector
+class AlephVectorPETSc
+: public IAlephVector
 {
  public:
+
   AlephVectorPETSc(ITraceMng*, AlephKernel*, Integer);
-  void AlephVectorCreate(void);
-  void AlephVectorSet(const double*, const int*, Integer);
-  int AlephVectorAssemble(void);
-  void AlephVectorGet(double*, const int*, Integer);
-  void writeToFile(const String);
+  void AlephVectorCreate(void) override;
+  void AlephVectorSet(const double*, const int*, Integer) override;
+  int AlephVectorAssemble(void) override;
+  void AlephVectorGet(double*, const int*, Integer) override;
+  void writeToFile(const String) override;
   Real LinftyNorm(void);
 
  public:
+
   Vec m_petsc_vector;
   PetscInt jSize, jUpper, jLower;
 };
 
-class AlephMatrixPETSc : public IAlephMatrix
+class AlephMatrixPETSc
+: public IAlephMatrix
 {
  public:
+
   AlephMatrixPETSc(ITraceMng*, AlephKernel*, Integer);
-  void AlephMatrixCreate(void);
-  void AlephMatrixSetFilled(bool);
-  int AlephMatrixAssemble(void);
-  void AlephMatrixFill(int, int*, int*, double*);
+  void AlephMatrixCreate(void) override;
+  void AlephMatrixSetFilled(bool) override;
+  int AlephMatrixAssemble(void) override;
+  void AlephMatrixFill(int, int*, int*, double*) override;
   Real LinftyNormVectorProductAndSub(AlephVector*, AlephVector*);
   bool isAlreadySolved(AlephVectorPETSc*, AlephVectorPETSc*,
                        AlephVectorPETSc*, Real*, AlephParams*);
   int AlephMatrixSolve(AlephVector*, AlephVector*,
-                       AlephVector*, Integer&, Real*, AlephParams*);
-  void writeToFile(const String);
+                       AlephVector*, Integer&, Real*, AlephParams*) override;
+  void writeToFile(const String) override;
 
  private:
+
   Mat m_petsc_matrix;
   KSP m_ksp_solver;
 };
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
-class PETScAlephFactoryImpl : public AbstractService
+class PETScAlephFactoryImpl
+: public AbstractService
 , public IAlephFactoryImpl
 {
  public:
-  PETScAlephFactoryImpl(const ServiceBuildInfo& sbi)
+
+  explicit PETScAlephFactoryImpl(const ServiceBuildInfo& sbi)
   : AbstractService(sbi)
   {}
-  ~PETScAlephFactoryImpl()
+  ~PETScAlephFactoryImpl() override
   {
-    for (auto* v : m_IAlephVectors )
+    for (auto* v : m_IAlephVectors)
       delete v;
-    for (auto* v : m_IAlephMatrixs )
+    for (auto* v : m_IAlephMatrixs)
       delete v;
-    for (auto* v : m_IAlephTopologys )
+    for (auto* v : m_IAlephTopologys)
       delete v;
   }
 
  public:
-  virtual void initialize() {}
-  virtual IAlephTopology* createTopology(ITraceMng* tm,
-                                         AlephKernel* kernel,
-                                         Integer index,
-                                         Integer nb_row_size)
+
+  void initialize() override {}
+  IAlephTopology* createTopology(ITraceMng* tm,
+                                 AlephKernel* kernel,
+                                 Integer index,
+                                 Integer nb_row_size) override
   {
     IAlephTopology* new_topology = new AlephTopologyPETSc(tm, kernel, index, nb_row_size);
     m_IAlephTopologys.add(new_topology);
     return new_topology;
   }
-  virtual IAlephVector* createVector(ITraceMng* tm,
-                                     AlephKernel* kernel,
-                                     Integer index)
+  IAlephVector* createVector(ITraceMng* tm,
+                             AlephKernel* kernel,
+                             Integer index) override
   {
     IAlephVector* new_vector = new AlephVectorPETSc(tm, kernel, index);
     m_IAlephVectors.add(new_vector);
     return new_vector;
   }
 
-  virtual IAlephMatrix* createMatrix(ITraceMng* tm,
-                                     AlephKernel* kernel,
-                                     Integer index)
+  IAlephMatrix* createMatrix(ITraceMng* tm,
+                             AlephKernel* kernel,
+                             Integer index) override
   {
     IAlephMatrix* new_matrix = new AlephMatrixPETSc(tm, kernel, index);
     m_IAlephMatrixs.add(new_matrix);
@@ -135,6 +152,7 @@ class PETScAlephFactoryImpl : public AbstractService
   }
 
  private:
+
   UniqueArray<IAlephVector*> m_IAlephVectors;
   UniqueArray<IAlephMatrix*> m_IAlephMatrixs;
   UniqueArray<IAlephTopology*> m_IAlephTopologys;
@@ -143,9 +161,9 @@ class PETScAlephFactoryImpl : public AbstractService
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_END_NAMESPACE
+} // namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#endif // _ALEPH_INTERFACE_PETSC_H_
+#endif
