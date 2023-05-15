79768dd3267e46ee08010ebeee3c1d3d899a868a
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-05-15 13:53:56 +0200

    [arcane,c#tools] Supprime code plus utilisé.

--- arcane/tools/Arcane.ExecDrivers/Arcane.ExecDrivers.Common/CustomMpiDriver.cs
+++ arcane/tools/Arcane.ExecDrivers/Arcane.ExecDrivers.Common/CustomMpiDriver.cs
@@ -1,26 +1,13 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 using Arcane.ExecDrivers.Common;
 
 namespace Arcane.ExecDrivers.Common
 {
-  // Informations de configuration.
-  // Les champs de cette classe sont initialisés via JSON
-  class Config
-  {
-    public string MpiDistrib { get; set; }
-    public string MpiRoot { get; set; }
-    public string MpiPrefix { get; set; }
-  }
-
   class CustomMpiDriver : ICustomExecDriver
   {
     bool m_is_init;
-    Config m_config;
-    Assembly m_json_assembly;
-    string m_config_path;
 
     public string Name { get { return "CustomMpiDriver"; } }
 
@@ -28,35 +15,9 @@ namespace Arcane.ExecDrivers.Common
     {
       if (m_is_init)
         return;
-      // Avec '.Net Core 3.0', il faut charger explicitement la DLL
-      // de 'NewtonSoft.Json' (car cette classe est chargée dynamiquement)
-      {
-        Assembly a = Assembly.GetExecutingAssembly();
-        string assembly_path = Path.GetDirectoryName(a.Location);
-        m_config_path = Path.Combine(assembly_path, "ArcaneCea.config");
-        Console.WriteLine("LOCATION_PATH={0}", m_config_path);
-        string json_assembly_path = Path.Combine(assembly_path,"Newtonsoft.Json.dll");
-        Console.WriteLine("Trying loading newtonsoft.json={0}", json_assembly_path);
-        m_json_assembly = Utils.LoadAssembly(json_assembly_path);
-        Console.WriteLine("Newtonsoft? {0}",m_json_assembly!=null);
-      }
-
       string host_name = Environment.MachineName;
-      _ReadConfig();
-      Console.WriteLine("TRY CUSTOM MPI_LAUNCHER machine={0} mpi_root={1}", host_name, m_config.MpiRoot);
+      Console.WriteLine("TRY CUSTOM MPI_LAUNCHER machine={0}");
       m_is_init = true;
-    }
-
-    void _ReadConfig()
-    {
-      // Lit le fichier de configuration 'ArcaneCea.config' au format JSON
-      // qui doit se trouver dans le même répertoire que cette assembly.
-
-      Newtonsoft.Json.JsonSerializer ser = new Newtonsoft.Json.JsonSerializer();
-      using (StreamReader sr = new StreamReader(m_config_path)) {
-        Newtonsoft.Json.JsonTextReader r = new Newtonsoft.Json.JsonTextReader(sr);
-        m_config = (Config)ser.Deserialize(r, typeof(Config));
-      }
     }
 
     public bool HandleMpiLauncher(Arcane.ExecDrivers.Common.ExecDriverProperties p, string mpi_launcher)
