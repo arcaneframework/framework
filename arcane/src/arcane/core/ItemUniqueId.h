e96552c15ef05b4bf8d6f2d1f62208ca2818bf49
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-06-12 09:59:08 +0200

    [arcane,core] Nettoyage de 'ItemUniqueId'.
    Déplace la classe 'ItemUniqueId' les opérateurs '==', '<', '>','<<' et
    '!='.
    
    Rend accessible sur accélérateur le constructeur et les opérateurs de
    conversion vers Int64.

--- arcane/src/arcane/core/ItemUniqueId.h
+++ arcane/src/arcane/core/ItemUniqueId.h
@@ -1,29 +1,28 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ItemUniqueId.h                                              (C) 2000-2018 */
+/* ItemUniqueId.h                                              (C) 2000-2023 */
 /*                                                                           */
 /* Type d'un identifiant unique pour une entité.                             */
 /*---------------------------------------------------------------------------*/
-#ifndef ARCANE_ITEMUNIQUEID_H
-#define ARCANE_ITEMUNIQUEID_H
+#ifndef ARCANE_CORE_ITEMUNIQUEID_H
+#define ARCANE_CORE_ITEMUNIQUEID_H
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
 #include "arcane/utils/HashFunction.h"
+
 #include "arcane/ArcaneTypes.h"
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-
-ARCANE_BEGIN_NAMESPACE
+namespace Arcane
+{
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
@@ -34,13 +33,16 @@ ARCANE_BEGIN_NAMESPACE
  */
 class ARCANE_CORE_EXPORT ItemUniqueId
 {
-public:
-  ItemUniqueId() : m_unique_id(NULL_ITEM_ID) {}
-  explicit ItemUniqueId(Int64 uid) : m_unique_id(uid) {}
-  //ostream& operator<<(ostream& o) const;
-  operator Int64() const { return m_unique_id; }
+ public:
+
+  ItemUniqueId() = default;
+  constexpr ARCCORE_HOST_DEVICE explicit ItemUniqueId(Int64 uid)
+  : m_unique_id(uid)
+  {}
+
+  constexpr ARCCORE_HOST_DEVICE operator Int64() const { return m_unique_id; }
   ARCANE_DEPRECATED operator Int32() const { return asInt32(); }
-  Int64 asInt64() const { return m_unique_id; }
+  constexpr ARCCORE_HOST_DEVICE Int64 asInt64() const { return m_unique_id; }
   Int32 asInt32() const;
   Integer asInteger() const
   {
@@ -50,8 +52,141 @@ public:
     return asInt32();
 #endif
   }
-private:
-  Int64 m_unique_id;
+
+ public:
+
+  friend inline bool operator<(ItemUniqueId lhs, ItemUniqueId rhs)
+  {
+    return lhs.asInt64() < rhs.asInt64();
+  }
+  friend inline bool operator<(Int64 lhs, ItemUniqueId rhs)
+  {
+    return lhs < rhs.asInt64();
+  }
+  friend inline bool operator<(ItemUniqueId lhs, Int32 rhs)
+  {
+    return lhs.asInt64() < rhs;
+  }
+  friend inline bool operator<(Int32 lhs, ItemUniqueId rhs)
+  {
+    return lhs < rhs.asInt64();
+  }
+  friend inline bool operator<(ItemUniqueId lhs, Int64 rhs)
+  {
+    return lhs.asInt64() < rhs;
+  }
+
+  friend inline bool operator<=(ItemUniqueId lhs, ItemUniqueId rhs)
+  {
+    return lhs.asInt64() <= rhs.asInt64();
+  }
+  friend inline bool operator<=(Int64 lhs, ItemUniqueId rhs)
+  {
+    return lhs <= rhs.asInt64();
+  }
+  friend inline bool operator<=(ItemUniqueId lhs, Int64 rhs)
+  {
+    return lhs.asInt64() <= rhs;
+  }
+  friend inline bool operator<=(Int32 lhs, ItemUniqueId rhs)
+  {
+    return lhs <= rhs.asInt64();
+  }
+  friend inline bool operator<=(ItemUniqueId lhs, Int32 rhs)
+  {
+    return lhs.asInt64() <= rhs;
+  }
+
+  friend inline bool operator>(ItemUniqueId lhs, ItemUniqueId rhs)
+  {
+    return lhs.asInt64() > rhs.asInt64();
+  }
+  friend inline bool operator>(Int64 lhs, ItemUniqueId rhs)
+  {
+    return lhs > rhs.asInt64();
+  }
+  friend inline bool operator>(ItemUniqueId lhs, Int64 rhs)
+  {
+    return lhs.asInt64() > rhs;
+  }
+  friend inline bool operator>(Int32 lhs, ItemUniqueId rhs)
+  {
+    return lhs > rhs.asInt64();
+  }
+  friend inline bool operator>(ItemUniqueId lhs, Int32 rhs)
+  {
+    return lhs.asInt64() > rhs;
+  }
+
+  friend inline bool operator>=(ItemUniqueId lhs, ItemUniqueId rhs)
+  {
+    return lhs.asInt64() >= rhs.asInt64();
+  }
+  friend inline bool operator>=(Int64 lhs, ItemUniqueId rhs)
+  {
+    return lhs >= rhs.asInt64();
+  }
+  friend inline bool operator>=(ItemUniqueId lhs, Int64 rhs)
+  {
+    return lhs.asInt64() >= rhs;
+  }
+  friend inline bool operator>=(Int32 lhs, ItemUniqueId rhs)
+  {
+    return lhs >= rhs.asInt64();
+  }
+  friend inline bool operator>=(ItemUniqueId lhs, Int32 rhs)
+  {
+    return lhs.asInt64() >= rhs;
+  }
+
+  friend inline bool operator!=(ItemUniqueId lhs, ItemUniqueId rhs)
+  {
+    return lhs.asInt64() != rhs.asInt64();
+  }
+  friend inline bool operator!=(Int64 lhs, ItemUniqueId rhs)
+  {
+    return lhs != rhs.asInt64();
+  }
+  friend inline bool operator!=(ItemUniqueId lhs, Int64 rhs)
+  {
+    return lhs.asInt64() != rhs;
+  }
+  friend inline bool operator!=(Int32 lhs, ItemUniqueId rhs)
+  {
+    return lhs != rhs.asInt64();
+  }
+  friend inline bool operator!=(ItemUniqueId lhs, Int32 rhs)
+  {
+    return lhs.asInt64() != rhs;
+  }
+
+  friend inline bool operator==(ItemUniqueId lhs, ItemUniqueId rhs)
+  {
+    return lhs.asInt64() == rhs.asInt64();
+  }
+  friend inline bool operator==(Int64 lhs, ItemUniqueId rhs)
+  {
+    return lhs == rhs.asInt64();
+  }
+  friend inline bool operator==(ItemUniqueId lhs, Int64 rhs)
+  {
+    return lhs.asInt64() == rhs;
+  }
+  friend inline bool operator==(Int32 lhs, ItemUniqueId rhs)
+  {
+    return lhs == rhs.asInt64();
+  }
+  friend inline bool operator==(ItemUniqueId lhs, Int32 rhs)
+  {
+    return lhs.asInt64() == rhs;
+  }
+
+  friend ARCANE_CORE_EXPORT std::ostream&
+  operator<<(std::ostream& o, const ItemUniqueId&);
+
+ private:
+
+  Int64 m_unique_id = NULL_ITEM_ID;
 };
 
 /*---------------------------------------------------------------------------*/
@@ -64,161 +199,34 @@ private:
  * \internal
  * \brief Spécialisation pour les Int64
  */
-template<>
+template <>
 class HashTraitsT<ItemUniqueId>
 {
  public:
+
   typedef ItemUniqueId KeyTypeConstRef;
   typedef ItemUniqueId& KeyTypeRef;
   typedef ItemUniqueId KeyTypeValue;
   typedef Int64 HashValueType;
   typedef TrueType Printable;
+
  public:
+
   static Int64 hashFunction(ItemUniqueId key)
-    {
-      return IntegerHashFunctionT<Int64>::hashfunc(key);
-    }
+  {
+    return IntegerHashFunctionT<Int64>::hashfunc(key);
+  }
 };
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_CORE_EXPORT std::ostream&
-operator<<(std::ostream& o,const ItemUniqueId&);
-
-inline bool operator<(ItemUniqueId lhs,ItemUniqueId rhs)
-{
-  return lhs.asInt64() < rhs.asInt64();
-}
-inline bool operator<(Int64 lhs,ItemUniqueId rhs)
-{
-  return lhs < rhs.asInt64();
-}
-inline bool operator<(ItemUniqueId lhs,Int32 rhs)
-{
-  return lhs.asInt64() < rhs;
-}
-inline bool operator<(Int32 lhs,ItemUniqueId rhs)
-{
-  return lhs < rhs.asInt64();
-}
-inline bool operator<(ItemUniqueId lhs,Int64 rhs)
-{
-  return lhs.asInt64() < rhs;
-}
-
-inline bool operator<=(ItemUniqueId lhs,ItemUniqueId rhs)
-{
-  return lhs.asInt64() <= rhs.asInt64();
-}
-inline bool operator<=(Int64 lhs,ItemUniqueId rhs)
-{
-  return lhs <= rhs.asInt64();
-}
-inline bool operator<=(ItemUniqueId lhs,Int64 rhs)
-{
-  return lhs.asInt64() <= rhs;
-}
-inline bool operator<=(Int32 lhs,ItemUniqueId rhs)
-{
-  return lhs <= rhs.asInt64();
-}
-inline bool operator<=(ItemUniqueId lhs,Int32 rhs)
-{
-  return lhs.asInt64() <= rhs;
-}
-
-inline bool operator>(ItemUniqueId lhs,ItemUniqueId rhs)
-{
-  return lhs.asInt64() > rhs.asInt64();
-}
-inline bool operator>(Int64 lhs,ItemUniqueId rhs)
-{
-  return lhs > rhs.asInt64();
-}
-inline bool operator>(ItemUniqueId lhs,Int64 rhs)
-{
-  return lhs.asInt64() > rhs;
-}
-inline bool operator>(Int32 lhs,ItemUniqueId rhs)
-{
-  return lhs > rhs.asInt64();
-}
-inline bool operator>(ItemUniqueId lhs,Int32 rhs)
-{
-  return lhs.asInt64() > rhs;
-}
-
-inline bool operator>=(ItemUniqueId lhs,ItemUniqueId rhs)
-{
-  return lhs.asInt64() >= rhs.asInt64();
-}
-inline bool operator>=(Int64 lhs,ItemUniqueId rhs)
-{
-  return lhs >= rhs.asInt64();
-}
-inline bool operator>=(ItemUniqueId lhs,Int64 rhs)
-{
-  return lhs.asInt64() >= rhs;
-}
-inline bool operator>=(Int32 lhs,ItemUniqueId rhs)
-{
-  return lhs >= rhs.asInt64();
-}
-inline bool operator>=(ItemUniqueId lhs,Int32 rhs)
-{
-  return lhs.asInt64() >= rhs;
-}
-
-inline bool operator!=(ItemUniqueId lhs,ItemUniqueId rhs)
-{
-  return lhs.asInt64() != rhs.asInt64();
-}
-inline bool operator!=(Int64 lhs,ItemUniqueId rhs)
-{
-  return lhs != rhs.asInt64();
-}
-inline bool operator!=(ItemUniqueId lhs,Int64 rhs)
-{
-  return lhs.asInt64() != rhs;
-}
-inline bool operator!=(Int32 lhs,ItemUniqueId rhs)
-{
-  return lhs != rhs.asInt64();
-}
-inline bool operator!=(ItemUniqueId lhs,Int32 rhs)
-{
-  return lhs.asInt64() != rhs;
-}
-
-inline bool operator==(ItemUniqueId lhs,ItemUniqueId rhs)
-{
-  return lhs.asInt64() == rhs.asInt64();
-}
-inline bool operator==(Int64 lhs,ItemUniqueId rhs)
-{
-  return lhs == rhs.asInt64();
-}
-inline bool operator==(ItemUniqueId lhs,Int64 rhs)
-{
-  return lhs.asInt64() == rhs;
-}
-inline bool operator==(Int32 lhs,ItemUniqueId rhs)
-{
-  return lhs == rhs.asInt64();
-}
-inline bool operator==(ItemUniqueId lhs,Int32 rhs)
-{
-  return lhs.asInt64() == rhs;
-}
-
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_END_NAMESPACE
+} // namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
 #endif
-
