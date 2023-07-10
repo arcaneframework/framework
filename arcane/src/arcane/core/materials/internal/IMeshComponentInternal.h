6883ccb9dbb8fa32184944ef0e8bdde55bd9e110
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-07-10 13:52:01 +0200

    [arcane,materials] Crée une interface 'IMeshComponentInternal' pour gérer l'API interne à Arcane de 'IMeshComponent'.

---
+++ arcane/src/arcane/core/materials/internal/IMeshComponentInternal.h
@@ -0,0 +1,51 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* IMeshComponentInternal.h                                    (C) 2000-2023 */
+/*                                                                           */
+/* API interne Arcane de 'IMeshComponent'.                                   */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCANE_CORE_MATERIALS_INTERNAL_IMESHCOMPONENTINTERNAL_H
+#define ARCANE_CORE_MATERIALS_INTERNAL_IMESHCOMPONENTINTERNAL_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arcane/core/ItemTypes.h"
+#include "arcane/core/materials/MaterialsCoreGlobal.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arcane::Materials
+{
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief API interne Arcane de 'IMeshComponent'.
+ */
+class ARCANE_CORE_EXPORT IMeshComponentInternal
+{
+ public:
+
+  virtual ~IMeshComponentInternal() = default;
+
+ public:
+
+  //! Vue sur les mailles du composant.
+  virtual ConstArrayView<ComponentItemInternal*> itemsInternalView() const = 0;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arcane::Materials
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
