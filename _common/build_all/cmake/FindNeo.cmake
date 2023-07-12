221306604d98a848b24e758a89b5830e57885c60
sdcm <stephane.de-chaisemartin@ifpen.fr>
2023-07-12 16:18:42 +0200

    [Config] Neo is not yet plugged into full framework compilation.

--- _common/build_all/cmake/FindNeo.cmake
+++ _common/build_all/cmake/FindNeo.cmake
@@ -1,16 +1,16 @@
-﻿if (NOT Arctools_ROOT)
-    # Pour compatibilité
-    set(Arctools_ROOT "${ARCTOOLS_ROOT}")
-endif()
-if (NOT Arctools_ROOT)
-    message(FATAL_ERROR "Variable 'Arctools_ROOT' is not set")
-endif()
-
-set(Neo_ROOT ${Arctools_ROOT}/neo)
-
-if(NOT Neo_ALREADY_FOUND)
-    add_subdirectory(${Neo_ROOT} neo)
-    message(STATUS "Neo FOUND")
-    set(Neo_ALREADY_FOUND YES PARENT_SCOPE)
-endif()
-set(Neo_FOUND YES)
+﻿#if (NOT Arctools_ROOT)
+#    # Pour compatibilité
+#    set(Arctools_ROOT "${ARCTOOLS_ROOT}")
+#endif()
+#if (NOT Arctools_ROOT)
+#    message(FATAL_ERROR "Variable 'Arctools_ROOT' is not set")
+#endif()
+#
+#set(Neo_ROOT ${Arctools_ROOT}/neo)
+#
+#if(NOT Neo_ALREADY_FOUND)
+#    add_subdirectory(${Neo_ROOT} neo)
+#    message(STATUS "Neo FOUND")
+#    set(Neo_ALREADY_FOUND YES PARENT_SCOPE)
+#endif()
+#set(Neo_FOUND YES)
