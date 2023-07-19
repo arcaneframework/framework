608dfe132a81609ce849d554c03cc9896a17e33f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-07-19 14:59:41 +0200

    [arcane,samples] Corrige exécution de l'exemple 'eos' si le wrapper C# n'est pas disponible.
    Ajoute aussi dans le LD_LIBRARY_PATH des tests direct en C# le chemin vers
    les bibliothèques Arcane (sinon les '.so' ne sont pas trouvé).

--- arcane/samples_build/samples/eos/main.cc
+++ arcane/samples_build/samples/eos/main.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* main.cc                                                     (C) 2000-2022 */
+/* main.cc                                                     (C) 2000-2023 */
 /*                                                                           */
 /* Main eos sample.                                                          */
 /*---------------------------------------------------------------------------*/
@@ -23,10 +23,5 @@ main(int argc,char* argv[])
   auto& app_build_info = ArcaneLauncher::applicationBuildInfo();
   app_build_info.setCodeName("EOS");
   app_build_info.setCodeVersion(VersionInfo(1,0,0));
-  // Positionne le chemin absolu de la DLL C# en considérant qu'elle
-  // se trouve dans le répertoire de l'exécutable
-  auto& dotnet_info = ArcaneLauncher::dotNetRuntimeInitialisationInfo();
-  String exe_dir = ArcaneLauncher::getExeDirectory();
-  dotnet_info.setMainAssemblyName(Directory(exe_dir).file("PerfectGas.dll"));
   return ArcaneLauncher::run();
 }
