2139984dab3fd112ebea912d13518daac5509ada
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-08-02 19:34:50 +0200

    [arcane,c#tools] Supprime code plus utilisé pour le chargement des assemblys.
    On utilise toujours 'System.Runtime.Loader'. L'ancien code n'est plus
    utilisé depuis un an.

--- arcane/tools/Arcane.ExecDrivers/Arcane.ExecDrivers.Common/NetCoreIndirectAssemblyLoader.cs
+++ arcane/tools/Arcane.ExecDrivers/Arcane.ExecDrivers.Common/NetCoreIndirectAssemblyLoader.cs
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
@@ -11,65 +11,16 @@ namespace Arcane.ExecDrivers.Common
 {
   public class NetCoreIndirectAssemblyLoader : Utils.IAssemblyLoader
   {
-    MethodInfo m_get_default_method;
-    MethodInfo m_load_assembly_from_path_method;
-    public NetCoreIndirectAssemblyLoader(MethodInfo method1,MethodInfo method2)
-    {
-      m_get_default_method = method1;
-      m_load_assembly_from_path_method = method2;
-    }
     public Assembly Load(string name)
     {
-      const bool do_new = true;
-      if (do_new){
-        Console.WriteLine($"Loading assembly [.NetCore3 direct with AssemblyLoadContext] '{name}'");
-        return AssemblyLoadContext.Default.LoadFromAssemblyPath(name);
-      }
-
-      // Le code suivant n'est plus utilisé (septembre 2022) maintenant
-      // qu'on utilise le package nuget 'system.runtime.loader'.
-      // On le garde temporairement en cas de problème.
-      Console.WriteLine($"Loading assembly [.NetCore3 Indirect] '{name}'");
-      object default_context =  m_get_default_method.Invoke(null, new object [0]);
-      if (default_context==null)
-        return null;
-      object a = m_load_assembly_from_path_method.Invoke(default_context,new object[] { name });
-      if (a != null)
-        return a as Assembly;
-      return null;
+      Console.WriteLine($"Loading assembly [.NetCore3 direct with AssemblyLoadContext] '{name}'");
+      return AssemblyLoadContext.Default.LoadFromAssemblyPath(name);
     }
 
-    /*!
-     * \brief Détecte si on peut utiliser le chargement via 'AssemblyLoadContext'.
-     *
-     * Pour cela, détecte si la méthode
-     * 'System.Runtime.Loader.AssemblyLoadContext.Defaut.LoadFromAssemblyPath' pour
-     * charger les assembly est disponible. Cela est nécessaire avec '.NetCore 3' car la
-     * méthode 'Assembly.LoadPath' ne fonctionne pas car elle est spécifique au framework.
-     */
+    //! Créé une instance pour charger une assembly.
     public static Utils.IAssemblyLoader CheckCreateLoader()
     {
-      Type type = Type.GetType("System.Runtime.Loader.AssemblyLoadContext");
-      if (type==null)
-        return null;
-
-      Console.WriteLine($"Has 'AssemblyLoadContext' type={type}");
-      BindingFlags common_flags = BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.FlattenHierarchy;
-      string method_name = "get_Default";
-      MethodInfo method = type.GetMethod(method_name, common_flags | BindingFlags.Static);
-      if (method == null){
-        Console.WriteLine($"Can not find static method named '{method_name}' in type '{type}'");
-        return null;
-      }
-      string method2_name = "LoadFromAssemblyPath";
-      MethodInfo method2 = type.GetMethod(method2_name,common_flags | BindingFlags.Instance);
-      if (method2 == null){
-        Console.WriteLine($"Can not find static method named '{method2_name}' in type '{type}'");
-        return null;
-      }
-
-      Console.WriteLine($"Found valid method '{method2_name}' in type '{type}'");
-      return new NetCoreIndirectAssemblyLoader(method,method2);
+      return new NetCoreIndirectAssemblyLoader();
     }
   }
 }
