62e06de9d87615df6ec37e40a14d8889f172ec89
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-09-14 15:58:04 +0200

    [alien,arcane_interface] Use modern Arcane in tests initialization.

--- alien/ArcaneInterface/test/AlienBench/main.cc
+++ alien/ArcaneInterface/test/AlienBench/main.cc
@@ -1,38 +1,28 @@
-#include <iostream>
-#include "arcane_packages.h"
-#include "arcane/utils/PlatformUtils.h"
-#include "arcane/utils/NotSupportedException.h"
+#include "arcane/utils/CommandLineArguments.h"
+#include "arcane/utils/Exception.h"
+
 #include "arcane/launcher/ArcaneLauncher.h"
-#include <arcane/utils/VersionInfo.h>
-#include <arcane/impl/ArcaneMain.h>
-
-#include <arcane/impl/ArcaneMain.h>
-
-#define USEALIEN_USE_MPI
-
-#ifdef USEALIEN_USE_MPI
-#define MPICH_SKIP_MPICXX 1
-#include <mpi.h>
-#endif // USEALIEN_USE_MPI
 
 using namespace Arcane;
+
+namespace
+{
+int _runTest(int argc, char* argv[])
+{
+  ArcaneLauncher::init(CommandLineArguments(&argc,&argv));
+  auto& app_build_info = ArcaneLauncher::applicationBuildInfo();
+  app_build_info.setCodeName("AlienBench");
+  app_build_info.setCodeVersion(VersionInfo(1,0,0));
+  return ArcaneLauncher::run();
+}
+}
 
 int
 main(int argc, char* argv[])
 {
   int r = 0;
-#ifdef USEALIEN_USE_MPI
-  MPI_Init(&argc, &argv);
-#endif // USEALIEN_USE_MPI
-  ArcaneMain::arcaneInitialize();
-  {
-    ApplicationInfo app_info(&argc, &argv, "AlienBench", VersionInfo(1, 0, 0));
-    r = ArcaneMain::arcaneMain(app_info);
-  }
-  ArcaneMain::arcaneFinalize();
-#ifdef USEALIEN_USE_MPI
-  MPI_Finalize();
-#endif // USEALIEN_USE_MPI
-
+  int r2 = arcaneCallFunctionAndCatchException([&](){ r = _runTest(argc,argv); });
+  if (r2!=0)
+    return r2;
   return r;
 }
