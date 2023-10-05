597bf55d8ff551357390ae7fb02f91c5e72867e2
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-10-05 21:08:04 +0200

    [arcane,cartesianmesh] Implémente en 2D séquentiel la création des mailles grossières

--- arcane/src/arcane/cartesianmesh/CartesianMeshCoarsening.cc
+++ arcane/src/arcane/cartesianmesh/CartesianMeshCoarsening.cc
@@ -16,7 +16,11 @@
 #include "arcane/utils/FatalErrorException.h"
 
 #include "arcane/core/IMesh.h"
+#include "arcane/core/ItemGroup.h"
 #include "arcane/core/IParallelMng.h"
+#include "arcane/core/CartesianGridDimension.h"
+#include "arcane/core/IMeshModifier.h"
+#include "arcane/core/SimpleSVGMeshExporter.h"
 
 #include "arcane/cartesianmesh/ICartesianMesh.h"
 #include "arcane/cartesianmesh/CellDirectionMng.h"
@@ -48,10 +52,13 @@ coarseCartesianMesh()
   if (nb_patch != 1)
     ARCANE_FATAL("This method is only valid for 1 patch (nb_patch={0})", nb_patch);
 
+  if (!mesh->isAmrActivated())
+    ARCANE_FATAL("AMR is not activated for this case");
+
   // TODO: Supprimer les mailles fantômes puis les reconstruire
   // TODO: Mettre à jour les informations dans CellDirectionMng
   // de ownNbCell(), globalNbCell(), ...
-  
+
   Integer nb_dir = mesh->dimension();
   if (nb_dir != 2)
     ARCANE_FATAL("This method is only valid for 2D mesh");
@@ -68,6 +75,78 @@ coarseCartesianMesh()
       ARCANE_FATAL("Invalid number of cells ({0}) for direction {1}. Should be a multiple of 2",
                    nb_own_cell, idir);
   }
+
+  CellDirectionMng cdm_x(m_cartesian_mesh->cellDirection(0));
+  CellDirectionMng cdm_y(m_cartesian_mesh->cellDirection(1));
+
+  const Int64 global_nb_cell_x = cdm_x.globalNbCell();
+  const Int64 global_nb_cell_y = cdm_y.globalNbCell();
+  CartesianGridDimension refined_grid_dim(global_nb_cell_x, global_nb_cell_y);
+  CartesianGridDimension coarse_grid_dim(global_nb_cell_x / 2, global_nb_cell_y / 2);
+  const Int64 coarse_grid_cell_offset = 10000; // TODO: à calculer automatiquement
+  CartesianGridDimension::CellUniqueIdComputer2D refined_cell_uid_computer(refined_grid_dim.getCellComputer2D(0));
+  CartesianGridDimension::NodeUniqueIdComputer2D refined_node_uid_computer(refined_grid_dim.getNodeComputer2D(0));
+  CartesianGridDimension::CellUniqueIdComputer2D coarse_cell_uid_computer(coarse_grid_dim.getCellComputer2D(coarse_grid_cell_offset));
+  CartesianGridDimension::FaceUniqueIdComputer2D coarse_face_uid_computer(coarse_grid_dim.getFaceComputer2D(coarse_grid_cell_offset));
+
+  // Pour les mailles et faces grossières, les noeuds existent déjà
+  // On ne peut donc pas utiliser la connectivité cartésienne de la grille grossière
+  // pour eux (on pourra le faire lorsque l'AMR par patch avec duplication sera active)
+  // En attendant on utilise la numérotation de la grille raffinée.
+
+  // TODO: Calculer le nombre de faces et de mailles et allouer en conséquence.
+  UniqueArray<Int64> faces_infos;
+  UniqueArray<Int64> cells_infos;
+  Int32 nb_coarse_face = 0;
+  Int32 nb_coarse_cell = 0;
+  ENUMERATE_ (Cell, icell, mesh->ownCells()) {
+    Cell cell = *icell;
+    Int64 cell_uid = cell.uniqueId();
+    Int64x3 cell_xy = refined_cell_uid_computer.compute(cell_uid);
+    const Int64 cell_x = cell_xy.x;
+    const Int64 cell_y = cell_xy.y;
+    // Comme on déraffine par 2, ne prend que les mailles dont les coordoonnées
+    // topologiques sont paires
+    if ((cell_x % 2) != 0 || (cell_y % 2) != 0)
+      continue;
+    info() << "CELLCoarse uid=" << cell_uid << " x=" << cell_x << " y=" << cell_y;
+    const Int64 coarse_cell_x = cell_x / 2;
+    const Int64 coarse_cell_y = cell_y / 2;
+    std::array<Int64, 4> node_uids;
+    node_uids[0] = refined_node_uid_computer.compute(cell_x + 0, cell_y + 0);
+    node_uids[1] = refined_node_uid_computer.compute(cell_x + 2, cell_y + 0);
+    node_uids[2] = refined_node_uid_computer.compute(cell_x + 2, cell_y + 2);
+    node_uids[3] = refined_node_uid_computer.compute(cell_x + 0, cell_y + 2);
+    info() << "CELLNodes uid=" << node_uids[0] << ' ' << node_uids[1]
+           << ' ' << node_uids[2] << ' ' << node_uids[3];
+    std::array<Int64, 4> coarse_face_uids = coarse_face_uid_computer.computeForCell(coarse_cell_x, coarse_cell_y);
+    const ItemTypeInfo* cell_type = cell.typeInfo();
+    // Ajoute les 4 faces
+    for (Int32 z = 0; z < 4; ++z) {
+      ItemTypeInfo::LocalFace lface = cell_type->localFace(z);
+      faces_infos.add(IT_Line2);
+      faces_infos.add(coarse_face_uids[z]);
+      faces_infos.add(node_uids[lface.node(0)]);
+      faces_infos.add(node_uids[lface.node(1)]);
+      ++nb_coarse_face;
+    }
+    // Ajoute la maille
+    {
+      cells_infos.add(IT_Quad4);
+      cells_infos.add(coarse_cell_uid_computer.compute(coarse_cell_x, coarse_cell_y));
+      for (Int32 z = 0; z < 4; ++z)
+        cells_infos.add(node_uids[z]);
+      ++nb_coarse_cell;
+    }
+  }
+
+  mesh->modifier()->addFaces(nb_coarse_face, faces_infos);
+  mesh->modifier()->addCells(nb_coarse_cell, cells_infos);
+  mesh->modifier()->endUpdate();
+
+  std::ofstream ofile("mesh_coarse.svg");
+  SimpleSVGMeshExporter writer(ofile);
+  writer.write(mesh->allCells());
 }
 
 /*---------------------------------------------------------------------------*/
