d322a40ba2680931980056a94eae1d7f9b31cef0
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-10-11 12:02:02 +0200

    [arcane] Corrige date copyright.

--- arcane/src/arcane/accelerator/AcceleratorGlobal.h
+++ arcane/src/arcane/accelerator/AcceleratorGlobal.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* AcceleratorGlobal.h                                         (C) 2000-2021 */
+/* AcceleratorGlobal.h                                         (C) 2000-2023 */
 /*                                                                           */
 /* Déclarations générales pour le support des accélérateurs.                 */
 /*---------------------------------------------------------------------------*/
