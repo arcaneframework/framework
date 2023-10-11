40f6b96143fc76fa39d7ca61f486986fec420dad
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-10-11 08:21:17 +0200

    [arcane,accelerator] Ajoute classes pour convertir avec vérification les 'RunQueue::platformStream()' en stream native.

---
+++ arcane/src/arcane/accelerator/CommonUtils.h
@@ -0,0 +1,62 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* CommonUtils.h                                               (C) 2000-2023 */
+/*                                                                           */
+/* Fonctions/Classes utilitaires communes à tout les runtimes.               */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCANE_ACCELERATOR_COMMONUTILS_H
+#define ARCANE_ACCELERATOR_COMMONUTILS_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arcane/accelerator/AcceleratorGlobal.h"
+#include "arcane/accelerator/core/RunQueue.h"
+
+#if defined(ARCANE_COMPILING_HIP)
+#include <hip/hip_runtime.h>
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arcane::Accelerator::impl
+{
+
+#if defined(ARCANE_COMPILING_CUDA)
+class ARCANE_ACCELERATOR_EXPORT CudaUtils
+{
+ public:
+
+  static cudaStream_t toNativeStream(RunQueue* queue);
+};
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#if defined(ARCANE_COMPILING_HIP)
+class ARCANE_ACCELERATOR_EXPORT HipUtils
+{
+ public:
+
+  static hipStream_t toNativeStream(RunQueue* queue);
+};
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arcane::Accelerator::impl
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
