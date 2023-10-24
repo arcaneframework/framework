5ae36e4c02ad933c99844159b1f750c9ba4d4221
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2023-10-24 20:38:37 +0200

    [arcane,cartesianmesh,mesh,core,impl] Correct copyright and encoding

--- arcane/src/arcane/core/IMainFactory.h
+++ arcane/src/arcane/core/IMainFactory.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* IMainFactory.h                                              (C) 2000-2020 */
+/* IMainFactory.h                                              (C) 2000-2023 */
 /*                                                                           */
 /* Interface des AbstractFactory d'Arcane.                                   */
 /*---------------------------------------------------------------------------*/
