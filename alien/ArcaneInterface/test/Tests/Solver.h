a2ab282dc52c63d3d8a6201eb1937a6c7db2160d
guignont <thomas.guignon@ifpen.fr>
2023-11-02 17:39:45 +0100

    - add headers

--- alien/ArcaneInterface/test/Tests/Solver.h
+++ alien/ArcaneInterface/test/Tests/Solver.h
@@ -1,4 +1,10 @@
-﻿#ifndef TESTS_REFSEMANTICMVHANDLERS_SOLVERCONFIGURATION_H
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TESTS_REFSEMANTICMVHANDLERS_SOLVERCONFIGURATION_H
 #define TESTS_REFSEMANTICMVHANDLERS_SOLVERCONFIGURATION_H
 
 #include <memory>
