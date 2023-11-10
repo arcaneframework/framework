72f89f576b4b1810e34bced31391a9060013026b
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-11-10 17:04:26 +0100

    [arcane,utils] Ajoute implémentation de l'algorithme 'SHA1'.

---
+++ arcane/src/arcane/utils/SHA1HashAlgorithm.h
@@ -0,0 +1,64 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* SHA1HashAlgorithm.h                                         (C) 2000-2023 */
+/*                                                                           */
+/* Calcule la fonction de hashage SHA-1.                                     */
+/*---------------------------------------------------------------------------*/
+#ifndef ARCANE_UTILS_SHA1HASHALGORITHM_H
+#define ARCANE_UTILS_SHA1HASHALGORITHM_H
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arcane/utils/IHashAlgorithm.h"
+#include "arcane/utils/String.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arcane::SHA1Algorithm
+{
+class SHA1;
+}
+
+namespace Arcane
+{
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Implémentation de l'algorithme SHA-1.
+ */
+class ARCANE_UTILS_EXPORT SHA1HashAlgorithm
+: public IHashAlgorithm
+{
+ public:
+
+  void computeHash(ByteConstArrayView input, ByteArray& output) override;
+  void computeHash64(Span<const Byte> input, ByteArray& output) override;
+  void computeHash64(Span<const std::byte> input, ByteArray& output) override;
+  String name() const override { return "SHA1"; }
+  Int32 hashSize() const override { return 20; }
+
+ protected:
+
+  void _initialize(SHA1Algorithm::SHA1&);
+
+ private:
+
+  void _computeHash64(Span<const std::byte> input, ByteArray& output);
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arcane
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
