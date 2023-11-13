5173b8279dbf80428675ede4499ada1e89a05a10
guignont <thomas.guignon@ifpen.fr>
2023-11-13 15:28:07 +0100

    - Fix blas search - remove conflicting cblas api

--- alien/standalone/src/core/alien/kernels/simple_csr/algebra/alien_cblas.h
+++ alien/standalone/src/core/alien/kernels/simple_csr/algebra/alien_cblas.h
@@ -1,29 +1,10 @@
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
-
-float cblas_sdot(const int n, const float* x, const int incx, const float* y,
-                 const int incy);
-double cblas_ddot(const int n, const double* x, const int incx, const double* y,
-                  const int incy);
-void cblas_saxpy(const int n, const float alpha, const float* x, const int incx,
-                 float* y, const int incy);
-void cblas_daxpy(const int n, const double alpha, const double* x,
-                 const int incx, double* y, const int incy);
-void cblas_scopy(const int n, const float* x, const int incx, float* y,
-                 const int incy);
-void cblas_dcopy(const int n, const double* x, const int incx, double* y,
-                 const int incy);
-void cblas_sscal(const int n, const float alpha, float* x, const int incx);
-void cblas_dscal(const int n, const double alpha, double* x, const int incx);
-double cblas_dasum(const int n, const double* x, const int incx);
-double cblas_dnrm2(const int n, const double* x, const int incx);
-
-#ifdef __cplusplus
-}
-#endif // __cplusplus
+#ifdef ALIEN_USE_MKL
+#include <mkl_cblas.h>
+#else
+#include <clas.h>
+#endif
 
 namespace cblas
 {
