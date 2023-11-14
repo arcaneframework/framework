98964051ef5834ce3c183697bba78d0e75ac2c15
guignont <thomas.guignon@ifpen.fr>
2023-11-14 13:28:06 +0100

    - fix header and encoding

--- alien/standalone/src/core/alien/kernels/simple_csr/algebra/alien_cblas.h
+++ alien/standalone/src/core/alien/kernels/simple_csr/algebra/alien_cblas.h
@@ -1,3 +1,8 @@
+﻿/// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
 #pragma once
 
 #ifdef ALIEN_USE_MKL
