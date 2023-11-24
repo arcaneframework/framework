f6ba9ceed0e2d16af896d222b67be158034a8cdd
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-11-24 21:30:24 +0100

    [arcane] Corrige copyright.

--- arcane/src/arcane/core/datatype/RealArray2Variant.cc
+++ arcane/src/arcane/core/datatype/RealArray2Variant.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* RealArray2Variant.cc                                        (C) 2000-2022 */
+/* RealArray2Variant.cc                                        (C) 2000-2023 */
 /*                                                                           */
 /* Variant pouvant contenir les types ConstArray2View, Real2x2 et Real3x3.   */
 /*---------------------------------------------------------------------------*/
