58dbb4c61134bfe3b82da9c2619bd2b8d49b45c7
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-12-13 08:25:21 +0100

    [arcane,utils] Ajoute constructeur de 'Vector3' à partir d'un 'std::array' ou d'un 'std::initializer_list'.

--- arcane/src/arcane/utils/Vector3.h
+++ arcane/src/arcane/utils/Vector3.h
@@ -29,10 +29,10 @@ namespace Arcane
 /*!
  * \brief Classe gérant un vecteur de dimension 3 de type \a T
  *
- * Les 3 éléments du vecteur peuvent être accéder par \a x, \a y ou \a z.
+ * Les 3 éléments du vecteur peuvent être accédés par \a x, \a y ou \a z.
  */
 template <typename T>
-class ARCANE_UTILS_EXPORT Vector3
+class Vector3
 {
  public:
 
@@ -61,6 +61,29 @@ class ARCANE_UTILS_EXPORT Vector3
   , z(v)
   {
   }
+
+  //! Construit l'instance avec le tableau \a v
+  constexpr explicit Vector3(const std::array<T, 3>& v)
+  : x(v[0])
+  , y(v[1])
+  , z(v[2])
+  {
+  }
+
+  //! Construit l'instance avec la liste \a v
+  constexpr Vector3(std::initializer_list<T> v)
+  {
+    _setFromList(v);
+  }
+
+  //! Positionne l'instance avec la liste \a v
+  constexpr Vector3& operator=(std::initializer_list<T> v)
+  {
+    _setFromList(v);
+    return (*this);
+  }
+
+ public:
 
   friend constexpr ARCCORE_HOST_DEVICE bool
   operator<(const Vector3<T>& v1, const Vector3<T>& v2)
@@ -98,6 +121,19 @@ class ARCANE_UTILS_EXPORT Vector3
   void _print(std::ostream& o) const
   {
     o << '{' << x << ',' << y << ',' << z << '}';
+  }
+  constexpr void _setFromList(std::initializer_list<T> v)
+  {
+    auto s = v.size();
+    auto ptr = v.begin();
+    if (s > 0)
+      x = *ptr;
+    ++ptr;
+    if (s > 1)
+      y = *ptr;
+    ++ptr;
+    if (s > 2)
+      z = *ptr;
   }
 };
 
