70e2182bb7dacdc11de394f7e4e68c7e24e5b08d
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-12-30 15:46:52 +0100

    [arccore,message_passing_mpi] Ajoute support pour lancer des tests avec MPI.

---
+++ arccore/src/message_passing_mpi/tests/TestMain.cc
@@ -0,0 +1,62 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+#include <gtest/gtest.h>
+
+#include "arccore/message_passing_mpi/MessagePassingMpiGlobal.h"
+
+#include <iostream>
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arccore::MessagePassing::Mpi
+{
+MPI_Comm global_mpi_comm_world = MPI_COMM_NULL;
+}
+
+using namespace Arccore::MessagePassing::Mpi;
+
+namespace
+{
+char*** global_argv = nullptr;
+int* global_argc = nullptr;
+
+class MPIEnvironment
+: public ::testing::Environment
+{
+ public:
+
+  void SetUp() override
+  {
+    std::cout << "SETUP MPI\n";
+    int mpi_error = MPI_Init(global_argc, global_argv);
+    global_mpi_comm_world = MPI_COMM_WORLD;
+    ASSERT_EQ(mpi_error,MPI_SUCCESS);
+  }
+  void TearDown() override
+  {
+    int mpi_error = MPI_Finalize();
+    ASSERT_EQ(mpi_error,MPI_SUCCESS);
+  }
+};
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+int main(int argc, char* argv[])
+{
+  global_argc = &argc;
+  global_argv = &argv;
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::AddGlobalTestEnvironment(new MPIEnvironment());
+  return RUN_ALL_TESTS();
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
---
+++ arccore/src/message_passing_mpi/tests/TestMpi.cc
@@ -0,0 +1,45 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+#include <gtest/gtest.h>
+
+#include "arccore/message_passing_mpi/StandaloneMpiMessagePassingMng.h"
+#include "arccore/base/Ref.h"
+#include "arccore/collections/Array.h"
+#include "arccore/message_passing/Messages.h"
+
+#include "TestMain.h"
+
+#include <iostream>
+
+using namespace Arccore;
+using namespace Arccore::MessagePassing;
+using namespace Arccore::MessagePassing::Mpi;
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+TEST(MessagePassingMpi, Simple)
+{
+  Ref<IMessagePassingMng> pm(StandaloneMpiMessagePassingMng::createRef(global_mpi_comm_world));
+  std::cout << "Rank=" << pm->commRank() << "\n";
+  if (pm->commSize() == 2) {
+    UniqueArray<Int32> send_buf = { 1, 7, -4 };
+    if (pm->commRank() == 0)
+      mpSend(pm.get(), send_buf, 1);
+    else {
+      UniqueArray<Int32> receive_buf(3);
+      mpReceive(pm.get(), receive_buf, 0);
+      ASSERT_EQ(receive_buf.size(), 3);
+      ASSERT_EQ(receive_buf[0], 1);
+      ASSERT_EQ(receive_buf[1], 7);
+      ASSERT_EQ(receive_buf[2], -4);
+    }
+  }
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
