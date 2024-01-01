0559f707f9f4fc55c6960c4fc6e9c36e50488ff9
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-01-01 12:32:31 +0100

    [arcane,impl] Ajoute implémentation de 'IDataOperation' et 'IDataVisitor' pour les nouveaux types.

--- arcane/src/arcane/impl/DataOperation.h
+++ arcane/src/arcane/impl/DataOperation.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* DataOperation.h                                             (C) 2000-2021 */
+/* DataOperation.h                                             (C) 2000-2024 */
 /*                                                                           */
 /* Opération sur une donnée.                                                 */
 /*---------------------------------------------------------------------------*/
@@ -21,6 +21,8 @@
 #include "arcane/utils/Real3.h"
 #include "arcane/utils/Real2x2.h"
 #include "arcane/utils/Real3x3.h"
+#include "arcane/utils/Float16.h"
+#include "arcane/utils/BFloat16.h"
 
 #include "arcane/datatype/IDataOperation.h"
 
@@ -44,53 +46,71 @@ class DataOperationT
   DataOperationT() {}
   DataOperationT(const DataOperator& op) : m_operator(op) {}
 
+ private:
+
+  template<typename DataType> void
+  _applyView(ArrayView<DataType> output,ConstArrayView<DataType> input)
+  {
+    for( Int32 i=0, n=input.size(); i<n; ++i )
+      output[i] = m_operator(output[i],input[i]);
+  }
+
  public:
 
   void apply(ByteArrayView output,ByteConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(RealArrayView output,RealConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Int32ArrayView output,Int32ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Int16ArrayView output,Int16ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Int64ArrayView output,Int64ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Real2ArrayView output,Real2ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Real3ArrayView output,Real3ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Real2x2ArrayView output,Real2x2ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
   void apply(Real3x3ArrayView output,Real3x3ConstArrayView input) override
   {
-    for( Integer i=0, n=input.size(); i<n; ++i )
-      output[i] = m_operator(output[i],input[i]);
+    _applyView(output,input);
   }
+  void apply(ArrayView<Int8> output,ConstArrayView<Int8> input) override
+  {
+    _applyView(output,input);
+  }
+  void apply(ArrayView<Float16> output,ConstArrayView<Float16> input) override
+  {
+    _applyView(output,input);
+  }
+  void apply(ArrayView<BFloat16> output,ConstArrayView<BFloat16> input) override
+  {
+    _applyView(output,input);
+  }
+  void apply(ArrayView<Float32> output,ConstArrayView<Float32> input) override
+  {
+    _applyView(output,input);
+  }
+
+ private:
 
   template<typename DataType> void
   _applySpan(Span<DataType> output,Span<const DataType> input)
@@ -98,6 +118,9 @@ class DataOperationT
     for( Int64 i=0, n=input.size(); i<n; ++i )
       output[i] = m_operator(output[i],input[i]);
   }
+
+ public:
+
   void applySpan(Span<Byte> output,Span<const Byte> input) override
   {
     _applySpan(output,input);
@@ -134,7 +157,25 @@ class DataOperationT
   {
     _applySpan(output,input);
   }
+  void applySpan(Span<Int8> output,Span<const Int8> input) override
+  {
+    _applySpan(output,input);
+  }
+  void applySpan(Span<Float16> output,Span<const Float16> input) override
+  {
+    _applySpan(output,input);
+  }
+  void applySpan(Span<BFloat16> output,Span<const BFloat16> input) override
+  {
+    _applySpan(output,input);
+  }
+  void applySpan(Span<Float32> output,Span<const Float32> input) override
+  {
+    _applySpan(output,input);
+  }
+
  private:
+
   DataOperator m_operator;
 };
 
