2deb1b506a0a3de7155ec8a4517d760977ded14f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-01-14 16:25:45 +0100

    [arcane,std] Corrige copyright.

--- arcane/src/arcane/std/MshMeshReader.cc
+++ arcane/src/arcane/std/MshMeshReader.cc
@@ -1,13 +1,13 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MshMeshReader.cc                                            (C) 2000-2023 */
+/* MshMeshReader.cc                                            (C) 2000-2024 */
 /*                                                                           */
-/* Lecture/Ecriture d'un fichier au format MSH.				                       */
+/* Lecture/Ecriture d'un fichier au format MSH.                              */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
