46973bdd37ad113e06c3ba1af6a50fd33427bdec
stdcm <stephane.de-chaisemartin@ifpen.fr>
2024-02-02 11:15:46 +0100

    [arcane,utils] Missing std::.

--- arcane/src/arcane/utils/HashTableMap.h
+++ arcane/src/arcane/utils/HashTableMap.h
@@ -573,7 +573,7 @@ class HashTableMapT
       //     << " bucket=" << m_nb_bucket  << " m_max_count=" << m_max_count
       //     << " memory=" << (m_buckets.capacity()*sizeof(Data*)) << '\n';
       //_print(Printable());
-      cout.flush();
+      std::cout.flush();
     }
   }
 
@@ -597,8 +597,8 @@ class HashTableMapT
 
   void _throwNotFound ARCANE_NORETURN(KeyTypeConstRef id, TrueType) const
   {
-    cout << "ERROR: can not find key=" << id << " bucket=" << _keyToBucket(id) << "\n";
-    cout.flush();
+    std::cout << "ERROR: can not find key=" << id << " bucket=" << _keyToBucket(id) << "\n";
+    std::cout.flush();
     HashTableBase::_throwNotFound();
   }
 
