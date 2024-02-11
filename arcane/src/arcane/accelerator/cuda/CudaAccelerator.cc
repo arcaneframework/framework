796ef8dec596da3bd974cb50d840a3b3bbfe7d7e
Hugo Taboada <hugo.taboada@cea.fr>
2024-02-11 18:15:19 +0100

    [arcane] Replace hardcoded 4096 bytes by a call to get system page size.
    This call supports POSIX systems (Unix, Linux, macOS) and Windows.

--- arcane/src/arcane/accelerator/cuda/CudaAccelerator.cc
+++ arcane/src/arcane/accelerator/cuda/CudaAccelerator.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* CudaAccelerator.cc                                          (C) 2000-2023 */
+/* CudaAccelerator.cc                                          (C) 2000-2024 */
 /*                                                                           */
 /* Backend 'CUDA' pour les accélérateurs.                                    */
 /*---------------------------------------------------------------------------*/
@@ -100,7 +100,7 @@ class UnifiedMemoryCudaMemoryAllocator
 {
  public:
 
-  static constexpr Int64 page_size = 4096;
+  Int64 page_size = platform::getPageSize();
 
   void initialize()
   {
