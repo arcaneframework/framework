f33d105a9d9dfe4abc241058a3da94e6bf40da17
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2024-02-13 13:21:38 +0100

    add bom encodong

--- alien/ArcaneInterface/test/AlienBench/AlienBenchModule.h
+++ alien/ArcaneInterface/test/AlienBench/AlienBenchModule.h
@@ -1,3 +1,9 @@
+// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 #ifndef ALIENBENCHMODULE_H
 #define ALIENBENCHMODULE_H
 
