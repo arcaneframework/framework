3ef2ead67052172aa6c5264464cc24c59ab3d77e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-02-16 13:46:48 +0100

    [arcane,materials] Corrige copyright.

--- arcane/src/arcane/core/materials/MatItemVector.h
+++ arcane/src/arcane/core/materials/MatItemVector.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MatItemVector.h                                             (C) 2000-2023 */
+/* MatItemVector.h                                             (C) 2000-2024 */
 /*                                                                           */
 /* Vecteur sur les entités d'un matériau.                                    */
 /*---------------------------------------------------------------------------*/
