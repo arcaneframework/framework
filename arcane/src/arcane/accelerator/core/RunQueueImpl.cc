5303b386ef597bddd07fd2b8d01afaaa05176d41
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-02-22 18:38:48 +0100

    [arcane,accelerator] Corrige copyright.

--- arcane/src/arcane/accelerator/core/RunQueueImpl.cc
+++ arcane/src/arcane/accelerator/core/RunQueueImpl.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* RunQueueImpl.cc                                             (C) 2000-2023 */
+/* RunQueueImpl.cc                                             (C) 2000-2024 */
 /*                                                                           */
 /* Gestion d'une file d'exécution sur accélérateur.                          */
 /*---------------------------------------------------------------------------*/
