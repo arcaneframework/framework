f509e88829b7a6aa060ff2a0b5ab25c98603c909
stdcm <stephane.de-chaisemartin@ifpen.fr>
2024-03-07 19:06:24 +0100

    [alien,ArcaneInterface] Add missing headers.

--- alien/ArcaneInterface/modules/arcane_tools/src/alien/arcane_tools/indexManager/BasicIndexManager.cc
+++ alien/ArcaneInterface/modules/arcane_tools/src/alien/arcane_tools/indexManager/BasicIndexManager.cc
@@ -1,3 +1,16 @@
+// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* BasicIndexManager                                         (C) 2000-2024   */
+/*                                                                           */
+/* Basic indexing between algebra and mesh worlds. Depends on Arcane         */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
 #include "alien/arcane_tools/indexManager/BasicIndexManager.h"
 
 #include <list>
--- alien/ArcaneInterface/modules/arcane_tools/src/alien/arcane_tools/indexManager/BasicIndexManager.h
+++ alien/ArcaneInterface/modules/arcane_tools/src/alien/arcane_tools/indexManager/BasicIndexManager.h
@@ -1,3 +1,15 @@
+// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* BasicIndexManager                                         (C) 2000-2024   */
+/*                                                                           */
+/* Basic indexing between algebra and mesh worlds. Depends on Arcane         */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 #ifndef ALIEN_INDEXMANAGER_BASICINDEXMANAGER_H
 #define ALIEN_INDEXMANAGER_BASICINDEXMANAGER_H
 
