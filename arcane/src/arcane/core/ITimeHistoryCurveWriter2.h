7dfd9bab0e2b0b82e945cf7d06267ab073fdca15
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-03-08 15:26:13 +0100

    [arcane,core,std] Update dates

--- arcane/src/arcane/core/ITimeHistoryCurveWriter2.h
+++ arcane/src/arcane/core/ITimeHistoryCurveWriter2.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ITimeHistoryCurveWriter2.h                                  (C) 2000-2018 */
+/* ITimeHistoryCurveWriter2.h                                  (C) 2000-2024 */
 /*                                                                           */
 /* Interface d'un écrivain d'une courbe d'un historique (Version 2).         */
 /*---------------------------------------------------------------------------*/
