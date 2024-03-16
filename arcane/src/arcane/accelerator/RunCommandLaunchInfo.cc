0e51a63a2484e64452f7c2096d822f38106e56a5
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-03-16 15:48:05 +0100

    [arcane,accelerator] Corrige copyright.

--- arcane/src/arcane/accelerator/RunCommandLaunchInfo.cc
+++ arcane/src/arcane/accelerator/RunCommandLaunchInfo.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* RunCommandLaunchInfo.cc                                     (C) 2000-2022 */
+/* RunCommandLaunchInfo.cc                                     (C) 2000-2024 */
 /*                                                                           */
 /* Informations pour l'exécution d'une 'RunCommand'.                         */
 /*---------------------------------------------------------------------------*/
