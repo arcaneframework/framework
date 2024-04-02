2ca0e6320b55ff9d792a0cc8597a4503eff021dd
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-04-02 10:20:05 +0200

    [arcane,core] Réindente et nettoie 'GroupIndexTable'.

--- arcane/src/arcane/core/GroupIndexTable.cc
+++ arcane/src/arcane/core/GroupIndexTable.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* GroupIndexTable.cc                                          (C) 2000-2011 */
+/* GroupIndexTable.cc                                          (C) 2000-2024 */
 /*                                                                           */
 /* Table de hachage entre un item et sa position dans la table.              */
 /*---------------------------------------------------------------------------*/
@@ -15,56 +15,56 @@
 #include "arcane/utils/FatalErrorException.h"
 #include "arcane/utils/PlatformUtils.h"
 
-#include "arcane/ItemGroupImpl.h"
-#include "arcane/ItemGroup.h"
-#include "arcane/GroupIndexTable.h"
+#include "arcane/core/ItemGroupImpl.h"
+#include "arcane/core/ItemGroup.h"
+#include "arcane/core/GroupIndexTable.h"
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_BEGIN_NAMESPACE
+namespace Arcane
+{
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
 GroupIndexTable::
 GroupIndexTable(ItemGroupImpl* group_impl)
-  : HashTableBase(0,false)
-  , m_group_impl(group_impl)
+: HashTableBase(0, false)
+, m_group_impl(group_impl)
 {
-  ARCANE_ASSERT((m_group_impl),("ItemGroupImpl pointer null"));
+  ARCANE_ASSERT((m_group_impl), ("ItemGroupImpl pointer null"));
 #ifdef ARCANE_ASSERT
-  m_disable_check_integrity = platform::getEnvironmentVariable("ARCANE_ENABLE_GROUPINDEXTABLE_CHECKINTEGRITY").null() ;
+  m_disable_check_integrity = platform::getEnvironmentVariable("ARCANE_ENABLE_GROUPINDEXTABLE_CHECKINTEGRITY").null();
 #endif
 }
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-void
-GroupIndexTable::
+void GroupIndexTable::
 update()
 {
   ItemGroup group(m_group_impl); // will update group if necessary
 
   const Integer group_size = group.size();
-  m_nb_bucket = this->nearestPrimeNumber(2*group_size);
+  m_nb_bucket = this->nearestPrimeNumber(2 * group_size);
   m_buckets.resize(m_nb_bucket);
   m_buckets.fill(-1);
   m_key_buffer.resize(group_size);
   m_next_buffer.resize(group_size);
 
-  ENUMERATE_ITEM(item, group) {
+  ENUMERATE_ITEM (item, group) {
     const Integer index = item.index();
     const KeyTypeConstRef key = item->localId();
     const Integer bucket = _hash(key);
-    ARCANE_ASSERT( (_lookupBucket(bucket,key) < 0), ("Already assigned key"));
+    ARCANE_ASSERT((_lookupBucket(bucket, key) < 0), ("Already assigned key"));
     m_key_buffer[index] = key;
     m_next_buffer[index] = m_buckets[bucket];
     m_buckets[bucket] = index;
   }
 
-  ARCANE_ASSERT( (_checkIntegrity()), ("GroupIndexTable integrity failed") );
+  ARCANE_ASSERT((_checkIntegrity()), ("GroupIndexTable integrity failed"));
 }
 
 /*---------------------------------------------------------------------------*/
@@ -76,88 +76,81 @@ compact(const Int32ConstArrayView* infos)
   ARCANE_UNUSED(infos);
   // Avec cette version, on suppose que l'ordre relatif des ids n'a pas changé
   // La taille du groupe n'a pas changé mais on réordonne les données
-  ARCANE_ASSERT((m_group_impl->size()==m_key_buffer.size()),("Inconsistent sizes"));
-  
+  ARCANE_ASSERT((m_group_impl->size() == m_key_buffer.size()), ("Inconsistent sizes"));
+
 #ifdef NDEBUG
   update();
 #else /* NDEBUG */
   // identique à update() mais peut faire quelque contrôle si infos!=NULL
   m_buckets.fill(-1);
   ItemGroup group(m_group_impl);
-  ENUMERATE_ITEM(iitem,group) {
+  ENUMERATE_ITEM (iitem, group) {
     const KeyTypeConstRef key = iitem.localId();
     const Integer i = iitem.index();
     const KeyTypeConstRef old_key = m_key_buffer[iitem.index()];
     const Integer bucket = _hash(key);
-    ARCANE_ASSERT( (_lookupBucket(bucket,key) < 0), ("Already assigned key"));
-    ARCANE_ASSERT( (infos==NULL || (*infos)[old_key] == key),("Inconsistent reorder translation %d vs %d vs %d",(*infos)[old_key],key,old_key));
+    ARCANE_ASSERT((_lookupBucket(bucket, key) < 0), ("Already assigned key"));
+    ARCANE_ASSERT((infos == NULL || (*infos)[old_key] == key), ("Inconsistent reorder translation %d vs %d vs %d", (*infos)[old_key], key, old_key));
     m_key_buffer[i] = key;
     m_next_buffer[i] = m_buckets[bucket];
     m_buckets[bucket] = i;
   }
-  ARCANE_ASSERT( (_checkIntegrity()), ("GroupIndexTable integrity failed") );
+  ARCANE_ASSERT((_checkIntegrity()), ("GroupIndexTable integrity failed"));
 #endif /* NDEBUG */
 }
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-Integer
-GroupIndexTable::
+Integer GroupIndexTable::
 _hash(KeyTypeConstRef id) const
 {
-  ARCANE_ASSERT((_initialized()),("GroupIndexTable not initialized"));
+  ARCANE_ASSERT((_initialized()), ("GroupIndexTable not initialized"));
   return (Integer)(KeyTraitsType::hashFunction(id) % m_nb_bucket);
 }
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-bool 
-GroupIndexTable::
+bool GroupIndexTable::
 _hasKey(KeyTypeConstRef id) const
 {
   const Integer hf = _hash(id);
-  for( Integer i = m_buckets[hf]; i>=0 ; i=m_next_buffer[i] ) 
-    {
-      if (m_key_buffer[i]==id)
-        return true;
-    }
+  for (Integer i = m_buckets[hf]; i >= 0; i = m_next_buffer[i]) {
+    if (m_key_buffer[i] == id)
+      return true;
+  }
   return false;
 }
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-Integer 
-GroupIndexTable::
+Integer GroupIndexTable::
 _lookupBucket(Integer bucket, KeyTypeConstRef id) const
-{ 
-  ARCANE_ASSERT((_initialized()),("GroupIndexTable not initialized"));
-  for( Integer i=m_buckets[bucket]; i>=0 ; i=m_next_buffer[i] )
-    {
-      if (m_key_buffer[i]==id)
-        return i;
-    }
+{
+  ARCANE_ASSERT((_initialized()), ("GroupIndexTable not initialized"));
+  for (Integer i = m_buckets[bucket]; i >= 0; i = m_next_buffer[i]) {
+    if (m_key_buffer[i] == id)
+      return i;
+  }
   return -1;
 }
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-Integer 
-GroupIndexTable::
+Integer GroupIndexTable::
 _lookup(KeyTypeConstRef id) const
-{ 
-  ARCANE_ASSERT( (_checkIntegrity(false)), ("GroupIndexTable integrity failed") );
-  return _lookupBucket(_hash(id),id);
+{
+  ARCANE_ASSERT((_checkIntegrity(false)), ("GroupIndexTable integrity failed"));
+  return _lookupBucket(_hash(id), id);
 }
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-bool 
-GroupIndexTable::
+bool GroupIndexTable::
 _initialized() const
 {
   return !m_buckets.empty();
@@ -166,8 +159,7 @@ _initialized() const
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-bool 
-GroupIndexTable::
+bool GroupIndexTable::
 _checkIntegrity(bool full) const
 {
   bool integrity = true;
@@ -181,10 +173,11 @@ _checkIntegrity(bool full) const
     integrity = false;
   }
 #ifdef ARCANE_ASSERT
-  if(!full && m_disable_check_integrity) return integrity;
+  if (!full && m_disable_check_integrity)
+    return integrity;
 #endif
   ItemGroup group(m_group_impl);
-  ENUMERATE_ITEM(item,group) {
+  ENUMERATE_ITEM (item, group) {
     const Integer hd = item.index();
     if (m_key_buffer[hd] != item.localId() || !_hasKey(item.localId())) {
       StringBuilder b;
@@ -192,7 +185,7 @@ _checkIntegrity(bool full) const
       b += item.index();
       b += " :  lid=";
       b += item.localId();
-      b +=  m_key_buffer[hd];
+      b += m_key_buffer[hd];
       b += " chk:";
       b += _hasKey(item.localId());
       throw FatalErrorException(b.toString());
@@ -205,7 +198,7 @@ _checkIntegrity(bool full) const
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_END_NAMESPACE
+} // namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
--- arcane/src/arcane/core/GroupIndexTable.h
+++ arcane/src/arcane/core/GroupIndexTable.h
@@ -1,19 +1,16 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* GroupIndexTable.h                                           (C) 2000-2011 */
+/* GroupIndexTable.h                                           (C) 2000-2024 */
 /*                                                                           */
 /* Table de hachage entre un item et sa position dans la table.              */
-/* Utile pour les variables partielles :                                     */
-/*  la position des données d'un item n'est pas son localId                  */
-/*  mais sa position dans l'enumerateur du groupe (ie dans la table)         */
 /*---------------------------------------------------------------------------*/
-#ifndef ARCANE_UTILS_GROUPINDEXTABLE_H
-#define ARCANE_UTILS_GROUPINDEXTABLE_H
+#ifndef ARCANE_CORE_GROUPINDEXTABLE_H
+#define ARCANE_CORE_GROUPINDEXTABLE_H
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
@@ -22,7 +19,8 @@
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_BEGIN_NAMESPACE
+namespace Arcane
+{
 
 class ItemGroupImpl;
 
@@ -31,41 +29,48 @@ class ItemGroupImpl;
 /*!
  * \internal
  * \brief Classe de base d'une table de hachage entre les items d'un groupe 
-          et leurs positions dans la table
+ * et leurs positions dans la table
+ *
+ * Cette table est utilisée pour les variables partielles : la position des
+ * données d'une entité n'est pas son localId()  mais sa position dans
+ * l'énumerateur du groupe (i.e: dans la table).         
  */
 class ARCANE_CORE_EXPORT GroupIndexTable
 : public HashTableBase
 {
-public: 
-  
+ public:
+
   typedef Integer KeyTypeValue;
   typedef Integer ValueType;
   typedef HashTraitsT<KeyTypeValue> KeyTraitsType;
   typedef KeyTraitsType::KeyTypeConstRef KeyTypeConstRef;
-  
-public:
-  
+
+ public:
+
   GroupIndexTable(ItemGroupImpl* group_impl);
-  
+
   ~GroupIndexTable() {}
-  
+
   void update();
- 
+
   void clear();
 
   void compact(const Int32ConstArrayView* info);
- 
+
   ValueType operator[](Integer i) const { return _lookup(i); }
 
   KeyTypeValue keyLocalId(Integer i) const { return m_key_buffer[i]; }
-  
+
   Integer size() const { return m_key_buffer.size(); }
 
-private:
-  
-  //! Fonction de hachage
-  /*! Utilise la fonction de hachage de Arcane même si quelques
-   *  collisions sont constatées avec les petites valeurs */
+ private:
+
+  /*!
+   * \brief Fonction de hachage.
+   *
+   * Utilise la fonction de hachage de Arcane même si quelques
+   * collisions sont constatées avec les petites valeurs
+   */
   Integer _hash(KeyTypeConstRef id) const;
 
   //! \a true si une valeur avec la clé \a id est présente
@@ -76,27 +81,26 @@ private:
 
   //! Recherche d'une clef dans toute la table
   Integer _lookup(KeyTypeConstRef id) const;
- 
+
   //! Teste l'initialisation de l'objet
   bool _initialized() const;
-  
-  //! Test l'intégrité de la table relativement à son groupe
-  bool _checkIntegrity(bool full=true) const;
 
-private:
-  ItemGroupImpl* m_group_impl;
+  //! Test l'intégrité de la table relativement à son groupe
+  bool _checkIntegrity(bool full = true) const;
+
+ private:
+
+  ItemGroupImpl* m_group_impl = nullptr;
   UniqueArray<KeyTypeValue> m_key_buffer; //! Table des clés associées
-  UniqueArray<Integer> m_next_buffer;     //! Table des index suivant associés
-  UniqueArray<Integer> m_buckets;         //! Tableau des buckets 
-#ifdef ARCANE_ASSERT
-  bool m_disable_check_integrity;
-#endif
+  UniqueArray<Integer> m_next_buffer; //! Table des index suivant associés
+  UniqueArray<Integer> m_buckets; //! Tableau des buckets
+  bool m_disable_check_integrity = false;
 };
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_END_NAMESPACE
+} // namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
