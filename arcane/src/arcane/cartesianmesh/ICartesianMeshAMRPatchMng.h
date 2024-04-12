0e5df8fe73ee9fb3fc89a98000e9cefae55ebe8d
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-04-12 17:38:32 +0200

    [arcane,cartesianmesh][AMR] Update headers

--- arcane/src/arcane/cartesianmesh/ICartesianMeshAMRPatchMng.h
+++ arcane/src/arcane/cartesianmesh/ICartesianMeshAMRPatchMng.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ICartesianMeshAMRPatchMng.h                                 (C) 2000-2023 */
+/* ICartesianMeshAMRPatchMng.h                                 (C) 2000-2024 */
 /*                                                                           */
 /* Interface de gestionnaire de l'AMR par patch d'un maillage cartésien.     */
 /*---------------------------------------------------------------------------*/
