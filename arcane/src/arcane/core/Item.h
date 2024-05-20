73b7efb04ebcf5fa9955463279a04782afd8e660
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-05-20 10:49:24 +0200

    [arcane,core,mesh] Update headers

--- arcane/src/arcane/core/Item.h
+++ arcane/src/arcane/core/Item.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* Item.h                                                      (C) 2000-2023 */
+/* Item.h                                                      (C) 2000-2024 */
 /*                                                                           */
 /* Informations sur les éléments du maillage.                                */
 /*---------------------------------------------------------------------------*/
