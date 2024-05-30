18a28066479f94705dc763da41f85a7a08c22b99
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-05-30 18:19:10 +0200

    [arcane,materials] Supprime constructeur pas utilisé.

--- arcane/src/arcane/materials/MeshMaterialVariableIndexer.cc
+++ arcane/src/arcane/materials/MeshMaterialVariableIndexer.cc
@@ -52,22 +52,6 @@ MeshMaterialVariableIndexer(ITraceMng* tm, const String& name)
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-MeshMaterialVariableIndexer::
-MeshMaterialVariableIndexer(const MeshMaterialVariableIndexer& rhs)
-: TraceAccessor(rhs)
-, m_index(rhs.m_index)
-, m_max_index_in_multiple_array(rhs.m_max_index_in_multiple_array)
-, m_name(rhs.m_name)
-, m_cells(rhs.m_cells)
-, m_matvar_indexes(rhs.m_matvar_indexes)
-, m_local_ids(rhs.m_local_ids)
-{
-  _init();
-}
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
@@ -107,7 +91,7 @@ endUpdate(const ComponentItemListBuilderOld& builder)
     }
   }
 
-  // NOTE: a priori, ici on est sur que m_max_index_in_multiple array vaut
+  // NOTE: a priori, ici on est sur que m_max_index_in_multiple_array vaut
   // nb_partial+1
   {
     Int32 max_index_in_multiple = (-1);
