e73be0fb9e300b0a8dccefa39f9dbbb562c8d1fd
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-07-04 16:20:11 +0200

    [arcane,impl] Update header

--- arcane/src/arcane/impl/TimeLoopMng.cc
+++ arcane/src/arcane/impl/TimeLoopMng.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* TimeLoopMng.cc                                              (C) 2000-2023 */
+/* TimeLoopMng.cc                                              (C) 2000-2024 */
 /*                                                                           */
 /* Gestionnaire de la boucle en temps.                                       */
 /*---------------------------------------------------------------------------*/
