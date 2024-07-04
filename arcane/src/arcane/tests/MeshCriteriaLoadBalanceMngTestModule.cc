4cd5c962ccd13a9fb27f705a9bfe29d2ad67a082
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-07-04 08:19:24 +0200

    [arcane,tests] Improve MeshCriteriaLoadBalanceMng test

--- arcane/src/arcane/tests/MeshCriteriaLoadBalanceMngTestModule.cc
+++ arcane/src/arcane/tests/MeshCriteriaLoadBalanceMngTestModule.cc
@@ -14,7 +14,9 @@
 #include "arcane/core/ILoadBalanceMng.h"
 #include "arcane/core/IMesh.h"
 #include "arcane/core/IMeshModifier.h"
+#include "arcane/core/IParallelMng.h"
 #include "arcane/core/ITimeLoopMng.h"
+#include "arcane/core/ServiceBuilder.h"
 #include "arcane/impl/MeshCriteriaLoadBalanceMng.h"
 #include "arcane/tests/MeshCriteriaLoadBalanceMngTest_axl.h"
 
@@ -47,9 +49,10 @@ class MeshCriteriaLoadBalanceMngTestModule
   void exit() override;
 
  private:
-  UniqueArray<Ref<VariableCellReal>> m_density_meshes_ref;
-  Ref<VariableCellReal> m_density_mesh0_ref;
-  Ref<VariableCellReal> m_density_mesh1_ref;
+
+  UniqueArray<Ref<VariableCellInt32>> m_density_meshes_ref;
+  UniqueArray<Int32> m_sum;
+  UniqueArray<Ref<IMeshPartitioner>> m_partitioners;
 };
 
 /*---------------------------------------------------------------------------*/
@@ -68,17 +71,38 @@ void MeshCriteriaLoadBalanceMngTestModule::
 init()
 {
   m_density_meshes_ref.resize(subDomain()->meshes().size());
+  m_sum.resize(subDomain()->meshes().size());
+  m_partitioners.resize(subDomain()->meshes().size());
 
   for(Integer imesh = 0; imesh < subDomain()->meshes().size(); ++imesh){
     IMesh* mesh = subDomain()->meshes()[imesh];
-    m_density_meshes_ref[imesh] = makeRef(new VariableCellReal(VariableBuildInfo(mesh, "Density")));
+    m_density_meshes_ref[imesh] = makeRef(new VariableCellInt32(VariableBuildInfo(mesh, "Density")));
 
-    VariableCellReal& density = *(m_density_meshes_ref[imesh].get());
+    Int32 sum{};
+
+    VariableCellInt32& density = *(m_density_meshes_ref[imesh].get());
 
     mesh->modifier()->setDynamic(true);
     ENUMERATE_ (Cell, icell, mesh->ownCells()){
       density[icell] = icell->uniqueId().asInt32();
+      sum += icell->uniqueId().asInt32();
     }
+    m_sum[imesh] = parallelMng()->reduce(IParallelMng::eReduceType::ReduceSum, sum);
+
+    Int32 max = parallelMng()->reduce(IParallelMng::eReduceType::ReduceMax, sum);
+
+    Real target = Real(m_sum[imesh]) / parallelMng()->commSize();
+
+    Real lb_quality = (max - target) * 100 / (m_sum[imesh] - target); // TODO div 0
+
+    debug() << "Initial Balancing"
+            << " -- Mesh : " << mesh->name()
+            << " -- NbCells : " << mesh->ownCells().size()
+            << " -- Local sum density : " << sum
+            << " -- All domains sum density : " << m_sum[imesh]
+            << " -- Quality of LoadBalance (between 0 and 100, lower is better) : " << lb_quality;
+
+    m_partitioners[imesh] = ServiceBuilder<IMeshPartitioner>::createReference(subDomain(), "DefaultPartitioner", mesh);
   }
 }
 
@@ -90,7 +114,7 @@ loop()
 {
   //for(Integer imesh = 0; imesh < subDomain()->meshes().size(); ++imesh){
   for (Integer imesh = 0; imesh < 2; ++imesh) {
-    VariableCellReal& density = *(m_density_meshes_ref[imesh].get());
+    VariableCellInt32& density = *(m_density_meshes_ref[imesh].get());
     IMesh* mesh = subDomain()->meshes()[imesh];
 
     MeshCriteriaLoadBalanceMng mesh_criteria = MeshCriteriaLoadBalanceMng(subDomain(), mesh->handle());
@@ -99,23 +123,34 @@ loop()
 
   for (Integer imesh = 0; imesh < subDomain()->meshes().size(); ++imesh) {
     IMesh* mesh = subDomain()->meshes()[imesh];
-    VariableCellReal& density = *(m_density_meshes_ref[imesh].get());
+    VariableCellInt32& density = *(m_density_meshes_ref[imesh].get());
 
-    Real sum = {};
+    Int32 sum{};
     ENUMERATE_ (Cell, icell, mesh->ownCells()) {
       sum += density[icell];
     }
+    Int32 sum_sum = parallelMng()->reduce(IParallelMng::eReduceType::ReduceSum, sum);
+
+    Int32 max = parallelMng()->reduce(IParallelMng::eReduceType::ReduceMax, sum);
+
+    Real target = Real(sum_sum) / parallelMng()->commSize();
+
+    Real lb_quality = (Real(max) - target) * 100 / (sum_sum - target);
+
     debug() << "Actual Balancing"
             << " -- Mesh : " << mesh->name()
             << " -- NbCells : " << mesh->ownCells().size()
-            << " -- Sum density : " << sum;
+            << " -- Local sum density : " << sum
+            << " -- All domains sum density : " << sum_sum
+            << " -- Original density : " << m_sum[imesh]
+            << " -- Quality of LoadBalance (between 0 and 100, lower is better) : " << lb_quality;
 
-    ENUMERATE_ (Cell, icell, mesh->ownCells()) {
-      debug() << "CellUniqueId : " << icell->uniqueId();
-    }
+    //    ENUMERATE_ (Cell, icell, mesh->ownCells()) {
+    //      debug() << "CellUniqueId : " << icell->uniqueId();
+    //    }
+    ARCANE_ASSERT((sum_sum == m_sum[imesh]), ("Different sum"));
+    subDomain()->timeLoopMng()->registerActionMeshPartition((IMeshPartitionerBase*)m_partitioners[imesh].get());
   }
-  subDomain()->timeLoopMng()->registerActionMeshPartition((IMeshPartitionerBase*)options()->partitioner0());
-  subDomain()->timeLoopMng()->registerActionMeshPartition((IMeshPartitionerBase*)options()->partitioner1());
 }
 
 /*---------------------------------------------------------------------------*/
--- arcane/tests/testMeshCriteriaLoadBalanceMng-1.arc
+++ arcane/tests/testMeshCriteriaLoadBalanceMng-1.arc
@@ -12,20 +12,37 @@
   <meshes>
     <mesh>
       <generator name="Cartesian2D" >
-        <nb-part-x>2</nb-part-x>
+        <nb-part-x>4</nb-part-x>
         <nb-part-y>2</nb-part-y>
         <origin>0.0 0.0</origin>
-        <x><n>3</n><length>3.0</length></x>
-        <y><n>3</n><length>3.0</length></y>
+        <x>
+          <n>5</n>
+          <length>5.0</length>
+        </x>
+        <y>
+          <n>3</n>
+          <length>3.0</length>
+        </y>
       </generator>
     </mesh>
     <mesh>
-      <generator name="Cartesian2D" >
+      <generator name="Cartesian3D">
         <nb-part-x>2</nb-part-x>
         <nb-part-y>2</nb-part-y>
-        <origin>0.0 0.0</origin>
-        <x><n>3</n><length>3.0</length></x>
-        <y><n>3</n><length>3.0</length></y>
+        <nb-part-z>2</nb-part-z>
+        <origin>0.0 0.0 0.0</origin>
+        <x>
+          <n>5</n>
+          <length>5.0</length>
+        </x>
+        <y>
+          <n>5</n>
+          <length>5.0</length>
+        </y>
+        <z>
+          <n>4</n>
+          <length>4.0</length>
+        </z>
       </generator>
     </mesh>
   </meshes>
