fc2525b31c5cdd0ae6bdf626dda7422a5a09628c
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-07-06 09:03:34 +0200

    [arcane,core] Corrige copyright.

--- arcane/src/arcane/core/MeshVariableArrayRef.h
+++ arcane/src/arcane/core/MeshVariableArrayRef.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MeshVariableArrayRef.h                                      (C) 2000-2023 */
+/* MeshVariableArrayRef.h                                      (C) 2000-2024 */
 /*                                                                           */
 /* Classe gérant une variable vectorielle sur une entité du maillage.        */
 /*---------------------------------------------------------------------------*/
