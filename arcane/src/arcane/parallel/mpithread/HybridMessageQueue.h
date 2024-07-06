90524b32b8397002b2391d6bc15d4e43c47f16f6
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-07-06 11:55:19 +0200

    [arcane] Corrige copyright.

--- arcane/src/arcane/parallel/mpithread/HybridMessageQueue.h
+++ arcane/src/arcane/parallel/mpithread/HybridMessageQueue.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* HybridMessageQueue.h                                        (C) 2000-2022 */
+/* HybridMessageQueue.h                                        (C) 2000-2024 */
 /*                                                                           */
 /* File de messages pour une implémentation hybride MPI/Mémoire partagée.    */
 /*---------------------------------------------------------------------------*/
--- arcane/src/arcane/parallel/thread/SharedMemoryMessageQueue.h
+++ arcane/src/arcane/parallel/thread/SharedMemoryMessageQueue.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* SharedMemoryMessageQueue.h                                  (C) 2000-2019 */
+/* SharedMemoryMessageQueue.h                                  (C) 2000-2024 */
 /*                                                                           */
 /* Implémentation d'une file de messages en mémoire partagée.                */
 /*---------------------------------------------------------------------------*/
