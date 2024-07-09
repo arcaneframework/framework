44c35573e4712aefb9066ac1e6aa4a491a0d3393
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-07-09 15:09:11 +0200

    [arcane,core] Corrige copyright.

--- arcane/src/arcane/core/ItemVectorView.cc
+++ arcane/src/arcane/core/ItemVectorView.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ItemVectorView.cc                                           (C) 2000-2023 */
+/* ItemVectorView.cc                                           (C) 2000-2024 */
 /*                                                                           */
 /* Vue sur une liste pour obtenir des informations sur les entités.          */
 /*---------------------------------------------------------------------------*/
