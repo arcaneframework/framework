c239c8fec7c8343498ed92f1c33a4ba309c95746
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-07-10 13:09:09 +0200

    [arcane] Corrige copyright.

--- arcane/src/arcane/tests/UtilsUnitTest.cc
+++ arcane/src/arcane/tests/UtilsUnitTest.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* UtilsUnitTest.cc                                            (C) 2000-2023 */
+/* UtilsUnitTest.cc                                            (C) 2000-2024 */
 /*                                                                           */
 /* Test des fonctions utilitaires de Arcane.                                 */
 /*---------------------------------------------------------------------------*/
