fcd500ed9a385f50458027da96981d5eec45bfea
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-07-16 16:28:32 +0200

    [arccore,trace] Utilise 'std::atomic' pour 'TraceMng::m_nb_flush'.
    Ce n'est utilisé que pour des flush donc ce n'est pas très important mais
    ca permet d'éviter des 'race condition' pour les sanitizers.

--- arccore/src/trace/arccore/trace/TraceMng.cc
+++ arccore/src/trace/arccore/trace/TraceMng.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* TraceMng.cc                                                 (C) 2000-2021 */
+/* TraceMng.cc                                                 (C) 2000-2024 */
 /*                                                                           */
 /* Gestionnaire des traces.                                                  */
 /*---------------------------------------------------------------------------*/
@@ -443,7 +443,7 @@ class TraceMng
   TraceClass m_default_trace_class;
   TraceClass m_current_msg_class;
   ReferenceCounter<ITraceStream> m_listing_stream;
-  Integer m_nb_flush;
+  std::atomic<Int64> m_nb_flush;
   String m_error_file_name;
   String m_log_file_name;
   String m_trace_id;
