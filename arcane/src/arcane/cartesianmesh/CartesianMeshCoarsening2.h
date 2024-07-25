34e8debd8d201b11b1e3c0531cb75d3a1b427bdf
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-07-25 10:50:07 +0200

    [arcane] Corrige copyright.

--- arcane/src/arcane/cartesianmesh/CartesianMeshCoarsening2.h
+++ arcane/src/arcane/cartesianmesh/CartesianMeshCoarsening2.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* CartesianMeshCoarsening2.h                                  (C) 2000-2023 */
+/* CartesianMeshCoarsening2.h                                  (C) 2000-2024 */
 /*                                                                           */
 /* Déraffinement d'un maillage cartésien.                                    */
 /*---------------------------------------------------------------------------*/
