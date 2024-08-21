e93a105b5303bd823b6b0ddd3953ebdc1ebd5193
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-08-21 14:24:45 +0200

    [arccon] Remove support for 'mkbundle'.
    This tool was only available with 'mono'.

--- arccon/build-system/ArcconDotNet.cmake
+++ arccon/build-system/ArcconDotNet.cmake
@@ -72,53 +72,6 @@ endfunction()
 
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
-# Fonction pour appeler 'mkbundle' sur un exe.
-#
-# NOTE: Avec l'utilisation de 'coreclr', cette function ne doit plus être
-# utilisée.
-#
-# 'mkbundle' est un outil de 'mono' qui permet de transformer un exe .NET en
-# en executable C classique qui est autonome et n'a pas besoin d'avoir 'mono'
-# installé pour fonctionner.
-#
-# Usage:
-#   arccon_dotnet_mkbundle(
-#     BUNDLE out_exe
-#     EXE    dotnet_exe
-#     DLLs   dotnet_dlls_to_embed
-#   )
-function(arccon_dotnet_mkbundle)
-  set(options)
-  set(oneValueArgs BUNDLE EXE)
-  set(multiValueArgs DLLs)
-  cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-  if(ARGS_UNPARSED_ARGUMENTS)
-    logFatalError("unparsed arguments '${ARGS_UNPARSED_ARGUMENTS}'")
-  endif()
-  get_filename_component(File ${ARGS_BUNDLE} NAME)
-  if(WIN32)
-    add_custom_command(
-      OUTPUT  ${ARGS_BUNDLE}
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different ${ARGS_EXE} ${ARGS_BUNDLE}
-      DEPENDS ${ARGS_EXE} ${ARGS_DLLs}
-      )
-  else()
-    # mkbundle ne peut être lancé en parallèle dans un même répertoire
-    # on utilise un répertoire temporaire et WORKING_DIRECTORY de add_custom_command
-    # L'option '--skip-scan' permet à mkbundle de ne pas lever d'exceptions si une des assembly
-    # n'est pas lisible.
-    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/dotnet/tmp/${File})
-    add_custom_command(
-      OUTPUT  ${ARGS_BUNDLE}
-      COMMAND ${Mkbundle_EXEC} ${ARGS_EXE} ${ARGS_DLLs} -o ${ARGS_BUNDLE} --deps --static --skip-scan
-      DEPENDS ${ARGS_EXE} ${ARGS_DLLs} ${Mkbundle_EXEC}
-      WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/dotnet/tmp/${File}
-      )
-  endif()
-endfunction()
-
-# ----------------------------------------------------------------------------
-# ----------------------------------------------------------------------------
 #
 # Copie à l'installation un répertoire où ont été publiés des
 # fichier '.NET'.
