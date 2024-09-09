cddd99df9d84fb9efd276e516d698c51724a1370
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-09-09 20:50:23 +0200

    [arcane,mesh] Corrige copyright.

--- arcane/src/arcane/mesh/NewWithLegacyConnectivity.h
+++ arcane/src/arcane/mesh/NewWithLegacyConnectivity.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ConnectivityNewWithDependenciesInfo.h                       (C) 2000-2023 */
+/* ConnectivityNewWithDependenciesInfo.h                       (C) 2000-2024 */
 /*                                                                           */
 /* Info for new connectivity mode (with dependencies)                        */
 /*---------------------------------------------------------------------------*/
@@ -18,7 +18,7 @@
 
 #include "arcane/utils/ArcaneGlobal.h"
 
-#include "arcane/IIncrementalItemConnectivity.h"
+#include "arcane/core/IIncrementalItemConnectivity.h"
 
 #include "arcane/mesh/MeshGlobal.h"
 #include "arcane/mesh/ConnectivityNewWithDependenciesTypes.h"
