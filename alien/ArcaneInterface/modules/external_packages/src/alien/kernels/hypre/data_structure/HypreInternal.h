76046d98feb4e722e2c944ac69c20e43f891f227
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2024-09-20 14:43:10 +0200

    header correction

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/hypre/data_structure/HypreInternal.h
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/hypre/data_structure/HypreInternal.h
@@ -1,3 +1,9 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 #pragma once
 
 #define MPICH_SKIP_MPICXX 1
