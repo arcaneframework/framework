eb3c476b1f44011a5152c5ff34512fc47aab5da5
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-09-24 15:42:45 +0200

    [arcane,tests] Corrige copyright.

--- arcane/src/arcane/utils/HashTableMap2.h
+++ arcane/src/arcane/utils/HashTableMap2.h
@@ -1,4 +1,9 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 
 // Version initiale issue du commit bdebddbdce1b473bbc189178fd523ef4a876ea01 (27 aout 2024)
 // emhash8::HashMap for C++14/17
