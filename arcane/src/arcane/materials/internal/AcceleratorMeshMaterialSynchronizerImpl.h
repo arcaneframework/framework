7c7a8362a52f08bbd5de876d01a876fb046df409
Florian REYNIER <florian.reynier.ocre@cea.fr>
2024-09-26 10:54:32 +0200

    updating remaining copyrigths

--- arcane/src/arcane/materials/internal/AcceleratorMeshMaterialSynchronizerImpl.h
+++ arcane/src/arcane/materials/internal/AcceleratorMeshMaterialSynchronizerImpl.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MeshMaterialSynchronizer.h                                  (C) 2000-2023 */
+/* MeshMaterialSynchronizer.h                                  (C) 2000-2024 */
 /*                                                                           */
 /* Synchronisation de la liste des matériaux/milieux des entités.            */
 /*---------------------------------------------------------------------------*/
