c0b55393421deea42e1f4722f9a5828af52daaec
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-10-06 10:10:47 +0200

    [arcane,std] Dans 'MetisGraphGather' et 'MetisWrapper', utilise 'IParallelMng' au lieu de 'MPI_Comm'.

--- arcane/src/arcane/std/internal/MetisGraphGather.h
+++ arcane/src/arcane/std/internal/MetisGraphGather.h
@@ -15,8 +15,10 @@
 /*---------------------------------------------------------------------------*/
 
 #include "arcane/utils/Array.h"
-#include "arcane/utils/ArrayView.h"
 #include "arcane/utils/String.h"
+#include "arcane/utils/TraceAccessor.h"
+
+#include "arcane/core/ArcaneTypes.h"
 
 #include "arcane/std/internal/MetisGraph.h"
 
@@ -33,15 +35,19 @@ namespace Arcane
 /*---------------------------------------------------------------------------*/
 
 class MetisGraphGather
+: public TraceAccessor
 {
+ public:
+
+  explicit MetisGraphGather(IParallelMng* pm);
+
  public:
 
   /*!
    * \brief Effectue un regroupement du graphe ParMetis "my_graph" sur le processeur de
    * rang 0 dans le communicateur "comm". Le graph résultat est "graph".
    */
-  void gatherGraph(const bool need_part, const String& comm_name, MPI_Comm comm,
-                   ConstArrayView<idx_t> vtxdist, const int ncon, MetisGraphView my_graph,
+  void gatherGraph(const bool need_part, ConstArrayView<idx_t> vtxdist, const int ncon, MetisGraphView my_graph,
                    MetisGraph& graph);
 
   /*!
@@ -49,8 +55,18 @@ class MetisGraphGather
    * communicateur "comm" sur tous les processeurs de ce communicateur. Le resultat
    * est "my_part", qui doit deja etre dimensionne avant appel.
    */
-  void scatterPart(MPI_Comm comm, ConstArrayView<idx_t> vtxdist, ConstArrayView<idx_t> part,
-                   ArrayView<idx_t> my_part);
+  void scatterPart(ConstArrayView<idx_t> vtxdist, ConstArrayView<idx_t> part, ArrayView<idx_t> my_part);
+
+ private:
+
+  IParallelMng* m_parallel_mng = nullptr;
+  Int32 m_my_rank = A_NULL_RANK;
+  Int32 m_nb_rank = A_NULL_RANK;
+
+ private:
+
+  template <class SourceType, class TargetType>
+  void _convertVector(const int size, ConstArrayView<SourceType> src, ArrayView<TargetType> dest);
 };
 
 /*---------------------------------------------------------------------------*/
