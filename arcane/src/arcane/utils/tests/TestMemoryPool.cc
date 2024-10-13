38f1cc6ac3edbb1b47a9cd740681fed1c24eb648
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-10-13 15:06:42 +0200

    [arcane,tests] Ajoute test simple pour 'MemoryPool'.

---
+++ arcane/src/arcane/utils/tests/TestMemoryPool.cc
@@ -0,0 +1,44 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include "arcane/utils/internal/MemoryPool.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+using namespace Arcane;
+using namespace Arcane::impl;
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+class MyMemoryPoolAllocator
+: public IMemoryPoolAllocator
+{
+  void* allocateMemory(size_t size) override { return std::malloc(size); }
+  void freeMemory(void* address,size_t) override { std::free(address); }
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+TEST(MemoryPool, Misc)
+{
+  MyMemoryPoolAllocator my_allocator;
+  MemoryPool memory_pool(&my_allocator);
+
+  void* a1 = memory_pool.allocateMemory(25);
+  void* a2 = memory_pool.allocateMemory(47);
+  memory_pool.freeMemory(a1, 25);
+  memory_pool.freeMemory(a2, 47);
+  memory_pool.dumpStats();
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
