1f94cc2799d66f624a994d325930c57316668e5d
guignont <thomas.guignon@ifpen.fr>
2024-11-12 10:22:19 +0100

    Add possibility to release buffer before destructor call (#1755)
    * - add possibility to release buffer before destructor call
    
    * - fix files headers

--- arccore/src/serialize/arccore/serialize/BasicSerializer.h
+++ arccore/src/serialize/arccore/serialize/BasicSerializer.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* BasicSerializer.h                                           (C) 2000-2023 */
+/* BasicSerializer.h                                           (C) 2000-2024 */
 /*                                                                           */
 /* Implémentation simple de 'ISerializer'.                                   */
 /*---------------------------------------------------------------------------*/
@@ -389,6 +389,7 @@ class ARCCORE_SERIALIZE_EXPORT BasicSerializer
   Int64ConstArrayView sizesBuffer();
   Int64 totalSize() const;
   void preallocate(Int64 size);
+  void releaseBuffer();
   void setFromSizes();
   void printSizes(std::ostream& o) const;
 
