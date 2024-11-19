ad992d45a7de1ffae5982e9615c2781e87159590
gazoppce <cedric.gazoppi@ifpen.fr>
2024-11-19 15:57:26 +0100

    CMAKE RLM Support ; replace PROTECTION_NAME variable by FLEXLM_PROTECTION_NAME

--- arcane/cmake/FindFlexLM.cmake
+++ arcane/cmake/FindFlexLM.cmake
@@ -6,7 +6,7 @@
 # FLEXLM_LIBRARIES, the libraries to link against to use FlexlmAPI.
 # FLEXLM_LIBRARY_DIRS, the library path to link against to use FlexlmAPI.
 # FLEXLM_FOUND, If false, do not try to use FlexlmAPI.
-# PROTECTION_NAME, return the protection name : FLEXLM
+# FLEXLM_PROTECTION_NAME, return the protection name : FLEXLM
 
 if(NOT FLEXLM_ROOT)
   set(FLEXLM_ROOT $ENV{FLEXLM_ROOT})
@@ -60,7 +60,7 @@ IF(FLEXLM_INCLUDE_DIR)
     # erreur dans une recherche de lib
   ELSE(FLEXLM_LIBRARY_FAILED)
     SET(FLEXLM_FOUND "YES")
-    SET(PROTECTION_NAME "FLEXLM")
+    SET(FLEXLM_PROTECTION_NAME "FLEXLM")
     # Biblioth�ques syst�mes suppl�mentaires
     if(WIN32)
       SET(FLEXLM_LIBRARIES ${FLEXLM_LIBRARY} oldnames.lib kernel32.lib user32.lib netapi32.lib
