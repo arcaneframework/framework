81f1f3bcda456eb7f9f6187aaa93a63fb2dff9ab
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2024-11-22 17:12:02 +0100

    header corrections

--- alien/ArcaneInterface/modules/composyx/src/alien/kernels/composyx/linear_solver/ComposyxOptionTypes.h
+++ alien/ArcaneInterface/modules/composyx/src/alien/kernels/composyx/linear_solver/ComposyxOptionTypes.h
@@ -1,4 +1,4 @@
-// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
 // Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
@@ -11,22 +11,15 @@ struct ComposyxOptionTypes
   enum eSolver
   {
     BiCGStab,
-    DDML
+    CG,
+    GMRES
   };
 
   enum ePreconditioner
   {
     None,
-    Poly,
-    Chebyshev,
-    BSSOR,
-    ILU0,
-    ILU0FP,
-    Cpr,
-    DDMLPC,
-    AMGPC,
-    CprAMG,
-    CprDDML
+    DiagPC,
+    ASPC
   };
 };
 
