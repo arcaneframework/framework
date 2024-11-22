9f95f14e956db46a5199fb08327027b5953a57d0
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2024-11-22 17:07:09 +0100

    header corrections

--- alien/ArcaneInterface/modules/composyx/src/alien/kernels/composyx/data_structure/ComposyxVector.cc
+++ alien/ArcaneInterface/modules/composyx/src/alien/kernels/composyx/data_structure/ComposyxVector.cc
@@ -1,4 +1,9 @@
-
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 #include <alien/kernels/simple_csr/SimpleCSRVector.h>
 #include <alien/kernels/simple_csr/SimpleCSRMatrix.h>
 
