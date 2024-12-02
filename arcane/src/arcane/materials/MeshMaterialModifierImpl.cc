c2ae189857f2261c317a28ad1f7516b3b2b8612e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-12-02 11:49:40 +0100

    [arcane,materials] Affiche le nom du maillage dans les statistiques de 'MeshMaterialModifier'.

--- arcane/src/arcane/materials/MeshMaterialModifierImpl.cc
+++ arcane/src/arcane/materials/MeshMaterialModifierImpl.cc
@@ -121,7 +121,7 @@ initOptimizationFlags()
     m_allow_optimize_multiple_operation = true;
   }
 
-  info() << "MeshMaterialModifier::optimization: "
+  info() << "MeshMaterialModifier::optimization:"
          << " allow?=" << m_allow_optimization
          << " allow_multiple?=" << m_allow_optimize_multiple_operation
          << " allow_multiple_mat?=" << m_allow_optimize_multiple_material
@@ -428,7 +428,8 @@ beginUpdate()
 void MeshMaterialModifierImpl::
 dumpStats()
 {
-  info() << "MeshMaterialModifierImpl statistics:";
+  info() << "MeshMaterialModifierImpl statistics mesh=" << m_material_mng->mesh()->name();
+
   info() << " Nb update : " << nb_update;
   info() << " Nb save/restore : " << nb_save_restore;
   info() << " Nb optimized add : " << nb_optimize_add;
