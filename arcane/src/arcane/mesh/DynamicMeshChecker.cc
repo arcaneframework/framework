81399f06215717c11843e4eae7998cff9e77ffda
sdcm <stephane.de-chaisemartin@ifpen.fr>
2024-12-03 10:09:42 +0100

    [arcane,mesh] Fix copyright.

--- arcane/src/arcane/mesh/DynamicMeshChecker.cc
+++ arcane/src/arcane/mesh/DynamicMeshChecker.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* DynamicMeshChecker.cc                                       (C) 2000-2023 */
+/* DynamicMeshChecker.cc                                       (C) 2000-2024 */
 /*                                                                           */
 /* Classe fournissant des méthodes de vérification sur le maillage.          */
 /*---------------------------------------------------------------------------*/
