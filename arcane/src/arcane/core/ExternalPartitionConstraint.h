d3015475fdfbeecd90d494f40b591ebc411f0ed9
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2024-12-26 19:36:42 +0100

    [arcane,core] Corrige copyright.

--- arcane/src/arcane/core/ExternalPartitionConstraint.h
+++ arcane/src/arcane/core/ExternalPartitionConstraint.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ExternalPartitionConstraint.h                               (C) 2000-2014 */
+/* ExternalPartitionConstraint.h                               (C) 2000-2024 */
 /*                                                                           */
 /* Informations sur les contraintes pour le partitionnement.                 */
 /*---------------------------------------------------------------------------*/
