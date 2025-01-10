7f01bd0204a74a081e132a3c341b2b383e82db0e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-01-10 09:55:05 +0100

    [arccore,base] Supprime inclusion du fichier obsolète '<cstdalign>'.

--- arccore/src/base/arccore/base/Float128.h
+++ arccore/src/base/arccore/base/Float128.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* Float128.h                                                  (C) 2000-2024 */
+/* Float128.h                                                  (C) 2000-2025 */
 /*                                                                           */
 /* Type flottant 128bit.                                                     */
 /*---------------------------------------------------------------------------*/
@@ -15,8 +15,6 @@
 /*---------------------------------------------------------------------------*/
 
 #include "arccore/base/ArccoreGlobal.h"
-
-#include <cstdalign>
 
 // Tous les compilateurs Linux supportés par Arccore ont le type '__float128'
 #if defined(ARCCORE_OS_LINUX)
