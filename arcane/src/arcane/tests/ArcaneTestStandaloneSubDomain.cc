c830c76662e52c7cbb1ad54c09229cd3edea4520
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-01-16 10:39:02 +0100

    [arcane:tests] Add a test for the new CommandLineArguments constructor

--- arcane/src/arcane/tests/ArcaneTestStandaloneSubDomain.cc
+++ arcane/src/arcane/tests/ArcaneTestStandaloneSubDomain.cc
@@ -100,6 +100,27 @@ int _testStandaloneSubDomainLauncher2(const CommandLineArguments& cmd_line_args)
   return 0;
 }
 
+int _testStandaloneSubDomainLauncher3()
+{
+  std::cout << "TEST3: StandaloneSubDomain\n";
+  ArcaneLauncher::init(CommandLineArguments{});
+  StandaloneSubDomain launcher{ ArcaneLauncher::createStandaloneSubDomain(String{}) };
+  ISubDomain* sd = launcher.subDomain();
+  ITraceMng* tm = launcher.traceMng();
+  ARCANE_CHECK_POINTER(sd);
+  ARCANE_CHECK_POINTER(tm);
+  MeshReaderMng mrm(sd);
+  IMesh* mesh = mrm.readMesh("Mesh1", "plancher.msh");
+  Int32 nb_cell = mesh->nbCell();
+  tm->info() << "NB_CELL=" << nb_cell;
+  Int32 expected_nb_cell = 196;
+  if (nb_cell != expected_nb_cell) {
+    tm->error() << String::format("Bad number of cells n={0} expected={1}", nb_cell, expected_nb_cell);
+    return 1;
+  }
+  return 0;
+}
+
 } // namespace
 
 /*---------------------------------------------------------------------------*/
@@ -115,6 +136,8 @@ arcaneTestStandaloneSubDomainLauncher(const CommandLineArguments& cmd_line_args,
       r = _testStandaloneSubDomainLauncher1(cmd_line_args);
     else if (method_name == "Test2")
       r = _testStandaloneSubDomainLauncher2(cmd_line_args);
+    else if (method_name == "Test3")
+      r = _testStandaloneSubDomainLauncher3();
     else
       ARCANE_FATAL("Unknown test name='{0}'", method_name);
   }
