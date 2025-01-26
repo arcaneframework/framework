cfaf3cda77eec957d1cbcc1d2d67475af2899c33
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-01-26 17:32:34 +0100

    [arcane,utils] Déplace la conversion 'StringView' vers 'double' dans une classe spécifique.

--- arcane/src/arcane/utils/ValueConvert.cc
+++ arcane/src/arcane/utils/ValueConvert.cc
@@ -91,113 +91,124 @@ arcaneSetUseSameValueConvertForAllReal(bool v)
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
+/*!
+ * \brief Classe pour convertir une 'StringView' en 'double'.
+ */
+class StringViewToDoubleConverter
+{
+ public:
 
-namespace
+  static Int64 _getDoubleValueWithFromChars(double& v, StringView s);
+  static Int64 _getDoubleValue(double& v, StringView s);
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Converti \a s en un double.
+ *
+ * Utilise std::from_chars() si \a global_use_from_chars est vrai.
+ * Sinon, utilise strtod().
+ */
+Int64 StringViewToDoubleConverter::
+_getDoubleValue(double& v, StringView s)
 {
 #if defined(ARCANE_USE_FROMCHARS)
-  /*!
-   * \brief Converti une chaîne de caractères en un double.
-   *
-   * Converti \a s en un double et range la valeur dans \a v.
-   * Il ne doit pas y avoir de caractères blancs au début de \a s.
-   *
-   * Le comportement de cette méthode est identique à std::strtod()
-   * avec le locale 'C' si on est en C++20. Sinon il est identique
-   * à std::strtod() avec le locale actuel (ce qui peut changer par exemple
-   * le séparateur décimal). La documentation de référence est
-   * ici: https://en.cppreference.com/w/cpp/utility/from_chars.
-   *
-   * \retval (-1) si la conversion a échouée.
-   * \retval la position dans \s du dernier caratère lu plus 1.
-   */
-  Int64 _getDoubleValueWithFromChars(double& v, StringView s)
-  {
-    // ATTENTION: il ne faut pas d'espace en début de \a s
-    auto bytes = s.bytes();
-    Int64 size = bytes.size();
-    if (size == 0)
-      // NOTE: Avec la version historique d'Arcane (avant la 3.15) il
-      // n'y avait pas d'erreur retournée lorsqu'on converti une chaîne vide.
-      // A priori cela n'était jamais utilisé donc cela ne pose pas de
-      // problème de corriger ce bug.
-      return (-1);
-    const char* orig_data = reinterpret_cast<const char*>(bytes.data());
-    const char* last_ptr = nullptr;
-    std::chars_format fmt = std::chars_format::general;
-    const char* data = orig_data;
-    bool do_negatif = false;
-    const bool is_verbose = global_value_convert_verbosity > 0;
-    // std::from_chars() peut lire les valeurs au format hexadécimal
-    // mais il ne doit pas contenir le '0x' ou '0X' du début, contrairement
-    // à std::strtod(). On détecte ce cas et on commence la conversion
-    // après le '0x' ou '0X'.
-
-    // Détecte '-0x' ou '-0X'
-    if (size >= 3 && (bytes[0] == '-') && (bytes[1] == '0') && (bytes[2] == 'x' || bytes[2] == 'X')) {
-      fmt = std::chars_format::hex;
-      data += 3;
-      do_negatif = true;
-    }
-    // Détecte '0x' ou '0X'
-    else if (size >= 2 && (bytes[0] == '0') && (bytes[1] == 'x' || bytes[1] == 'X')) {
-      fmt = std::chars_format::hex;
-      data += 2;
-    }
-    // Cas général
-    {
-      auto [ptr, ec] = std::from_chars(data, data + size, v, fmt);
-      last_ptr = ptr;
-      if (is_verbose)
-        std::cout << "FromChars:TRY GET_DOUBLE data=" << data << " v=" << v << " is_ok=" << (ec == std::errc()) << "\n";
-      if (ec != std::errc())
-        return (-1);
-    }
-    // Prend en compte le signe '-' si demandé
-    if (do_negatif)
-      v = -v;
-    if (is_verbose) {
-      char* ptr2 = nullptr;
-      double v2 = ::strtod(orig_data, &ptr2);
-      std::cout << "FromChars: COMPARE GET_DOUBLE via strtod v2=" << v2 << " pos=" << (ptr2 - orig_data) << "\n";
-    }
-    return (last_ptr - orig_data);
+  if (global_use_from_chars) {
+    Int64 p = _getDoubleValueWithFromChars(v, s);
+    return p;
   }
 #endif
 
-  /*---------------------------------------------------------------------------*/
-  /*---------------------------------------------------------------------------*/
-
-  /*!
-   * \brief Converti \a s en un double.
-   *
-   * Utilise std::from_chars() si \a global_use_from_chars est vrai.
-   * Sinon, utilise strtod().
-   */
-  Int64 _getDoubleValue(double& v, StringView s)
-  {
-#if defined(ARCANE_USE_FROMCHARS)
-    if (global_use_from_chars) {
-      Int64 p = _getDoubleValueWithFromChars(v, s);
-      return p;
-    }
-#endif
-
-    const char* ptr = _stringViewData(s);
+  const char* ptr = _stringViewData(s);
 #ifdef WIN32
-    if (s == "infinity" || s == "inf") {
-      v = std::numeric_limits<double>::infinity();
-      return false;
-    }
-#endif
-    char* ptr2 = nullptr;
-    v = ::strtod(ptr, &ptr2);
-    return (ptr2 - ptr);
+  if (s == "infinity" || s == "inf") {
+    v = std::numeric_limits<double>::infinity();
+    return false;
   }
+#endif
+  char* ptr2 = nullptr;
+  v = ::strtod(ptr, &ptr2);
+  return (ptr2 - ptr);
+}
 
-  /*---------------------------------------------------------------------------*/
-  /*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*!
+ * \brief Converti une chaîne de caractères en un double.
+ *
+ * Converti \a s en un double et range la valeur dans \a v.
+ * Il ne doit pas y avoir de caractères blancs au début de \a s.
+ *
+ * Le comportement de cette méthode est identique à std::strtod()
+ * avec le locale 'C' si on est en C++20. Sinon il est identique
+ * à std::strtod() avec le locale actuel (ce qui peut changer par exemple
+ * le séparateur décimal). La documentation de référence est
+ * ici: https://en.cppreference.com/w/cpp/utility/from_chars.
+ *
+ * \retval (-1) si la conversion a échouée.
+ * \retval la position dans \s du dernier caratère lu plus 1.
+ */
+Int64 StringViewToDoubleConverter::
+_getDoubleValueWithFromChars(double& v, StringView s)
+{
+#if defined(ARCANE_USE_FROMCHARS)
+  // ATTENTION: il ne faut pas d'espace en début de \a s
+  auto bytes = s.bytes();
+  Int64 size = bytes.size();
+  if (size == 0)
+    // NOTE: Avec la version historique d'Arcane (avant la 3.15) il
+    // n'y avait pas d'erreur retournée lorsqu'on converti une chaîne vide.
+    // A priori cela n'était jamais utilisé donc cela ne pose pas de
+    // problème de corriger ce bug.
+    return (-1);
+  const char* orig_data = reinterpret_cast<const char*>(bytes.data());
+  const char* last_ptr = nullptr;
+  std::chars_format fmt = std::chars_format::general;
+  const char* data = orig_data;
+  bool do_negatif = false;
+  const bool is_verbose = global_value_convert_verbosity > 0;
+  // std::from_chars() peut lire les valeurs au format hexadécimal
+  // mais il ne doit pas contenir le '0x' ou '0X' du début, contrairement
+  // à std::strtod(). On détecte ce cas et on commence la conversion
+  // après le '0x' ou '0X'.
 
-} // namespace
+  // Détecte '-0x' ou '-0X'
+  if (size >= 3 && (bytes[0] == '-') && (bytes[1] == '0') && (bytes[2] == 'x' || bytes[2] == 'X')) {
+    fmt = std::chars_format::hex;
+    data += 3;
+    do_negatif = true;
+  }
+  // Détecte '0x' ou '0X'
+  else if (size >= 2 && (bytes[0] == '0') && (bytes[1] == 'x' || bytes[1] == 'X')) {
+    fmt = std::chars_format::hex;
+    data += 2;
+  }
+  // Cas général
+  {
+    auto [ptr, ec] = std::from_chars(data, data + size, v, fmt);
+    last_ptr = ptr;
+    if (is_verbose)
+      std::cout << "FromChars:TRY GET_DOUBLE data=" << data << " v=" << v << " is_ok=" << (ec == std::errc()) << "\n";
+    if (ec != std::errc())
+      return (-1);
+  }
+  // Prend en compte le signe '-' si demandé
+  if (do_negatif)
+    v = -v;
+  if (is_verbose) {
+    char* ptr2 = nullptr;
+    double v2 = ::strtod(orig_data, &ptr2);
+    std::cout << "FromChars: COMPARE GET_DOUBLE via strtod v2=" << v2 << " pos=" << (ptr2 - orig_data) << "\n";
+  }
+  return (last_ptr - orig_data);
+#else
+  ARCANE_THROW(NotSupportedException, "using std::from_chars() is not available on this platform");
+#endif
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
@@ -207,7 +218,7 @@ builtInGetValue(double& v, StringView s)
 {
 #if defined(ARCANE_USE_FROMCHARS)
   if (global_use_from_chars) {
-    Int64 p = _getDoubleValueWithFromChars(v, s);
+    Int64 p = StringViewToDoubleConverter::_getDoubleValueWithFromChars(v, s);
     return (p == (-1) || (p != s.size()));
   }
 #endif
@@ -339,14 +350,13 @@ builtInGetValue(Real2& v, StringView s)
     const bool is_verbose = global_value_convert_verbosity > 0;
     if (is_verbose)
       std::cout << "Try Read Real2: '" << s << "'\n";
-    Int64 p = _getDoubleValue(v.x, s);
+    Int64 p = StringViewToDoubleConverter::_getDoubleValue(v.x, s);
     if (p == (-1))
       return true;
     Span<const Byte> bytes = s.bytes();
     Int64 nb_byte = bytes.size();
     // Supprime les espaces potentiels
-    for (; p < nb_byte; ++p)
-    {
+    for (; p < nb_byte; ++p) {
 #ifdef _MSC_VER <= 1916 // Visual Studio 2017 or less
       if (!std::isspace((char)bytes[p], std::locale()))
         break;
@@ -358,7 +368,7 @@ builtInGetValue(Real2& v, StringView s)
     s = StringView(bytes.subSpan(p, nb_byte));
     if (is_verbose)
       std::cout << "VX=" << v.x << " remaining_s='" << s << "'\n";
-    p = _getDoubleValue(v.y, s);
+    p = StringViewToDoubleConverter::_getDoubleValue(v.y, s);
     return (p == (-1) || (p != s.size()));
   }
   return impl::builtInGetValueGeneric(v, s);
