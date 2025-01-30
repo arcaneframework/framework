c6993ba35ffd3f34f40ecccaaa5c1dad4743c5c8
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-01-30 19:45:36 +0100

    [arccore] Corrige copyright.

--- arccore/src/base/arccore/base/ArccoreGlobal.h
+++ arccore/src/base/arccore/base/ArccoreGlobal.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ArccoreGlobal.h                                             (C) 2000-2024 */
+/* ArccoreGlobal.h                                             (C) 2000-2025 */
 /*                                                                           */
 /* Déclarations générales de Arccore.                                        */
 /*---------------------------------------------------------------------------*/
