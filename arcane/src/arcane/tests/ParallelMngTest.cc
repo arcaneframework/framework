7a56de6d84db08909a0f799216c2c0476e54caa7
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-02-03 19:31:27 +0100

    [arcane,tests] Corrige copyright.

--- arcane/src/arcane/tests/ParallelMngTest.cc
+++ arcane/src/arcane/tests/ParallelMngTest.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ParallelMngTest.cc                                          (C) 2000-2024 */
+/* ParallelMngTest.cc                                          (C) 2000-2025 */
 /*                                                                           */
 /* Test des opérations de base du parallèlisme.                              */
 /*---------------------------------------------------------------------------*/
@@ -18,10 +18,7 @@
 #include "arcane/utils/ITraceMng.h"
 #include "arcane/utils/Array3View.h"
 #include "arcane/utils/Array2.h"
-#include "arcane/utils/Real2.h"
-#include "arcane/utils/Real3.h"
-#include "arcane/utils/Real2x2.h"
-#include "arcane/utils/Real3x3.h"
+#include "arcane/utils/NumericTypes.h"
 #include "arcane/utils/HPReal.h"
 #include "arcane/utils/ScopedPtr.h"
 
