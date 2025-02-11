6a494787e377a83ae14f44534f967360f7a73ed6
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-02-11 13:41:16 +0100

    [arcane:core] Add ParameterCaseOption support in CaseOptionExtended

--- arcane/src/arcane/core/CaseOptionExtended.cc
+++ arcane/src/arcane/core/CaseOptionExtended.cc
@@ -15,12 +15,19 @@
 
 #include "arcane/utils/ITraceMng.h"
 #include "arcane/utils/FatalErrorException.h"
+#include "arcane/utils/ApplicationInfo.h"
+#include "arcane/utils/CommandLineArguments.h"
+#include "arcane/utils/ParameterList.h"
+#include "arcane/utils/ParameterCaseOption.h"
 
+#include "arcane/core/IApplication.h"
+#include "arcane/core/ICaseMng.h"
 #include "arcane/core/CaseOptionError.h"
 #include "arcane/core/ICaseDocumentVisitor.h"
 #include "arcane/core/XmlNodeList.h"
 #include "arcane/core/ICaseOptionList.h"
 #include "arcane/core/MeshHandle.h"
+#include "arcane/core/internal/StringVariableReplace.h"
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
@@ -41,25 +48,76 @@ namespace Arcane
 void CaseOptionMultiExtended::
 _search(bool is_phase1)
 {
-  XmlNodeList elem_list = rootElement().children(name());
   ITraceMng* tm = traceMng();
+  const ParameterList& params = caseMng()->application()->applicationInfo().commandLineArguments().parameters();
+  ParameterCaseOption pco{ caseMng() };
 
+  // !!! En XML, on commence par 1 et non 0.
+  UniqueArray<Integer> option_in_param;
+
+  pco.indexesInParam(String::format("{0}/{1}", rootElement().xpathFullName(), name()), option_in_param, false);
+
+  XmlNodeList elem_list = rootElement().children(name());
   Integer size = elem_list.size();
-  _checkMinMaxOccurs(size);
-  if (size==0)
-    return;
+  bool is_optional = isOptional();
 
-  if (is_phase1){
-    _allocate(size);
-    m_values.resize(size);
+  if (size == 0 && option_in_param.empty() && is_optional) {
+    return;
+  }
+
+  Integer min_occurs = minOccurs();
+  Integer max_occurs = maxOccurs();
+
+  Integer max_in_param = 0;
+
+  if (!option_in_param.empty()) {
+    max_in_param = option_in_param[0];
+    for (Integer index : option_in_param) {
+      if (index > max_in_param)
+        max_in_param = index;
+    }
+    if (max_occurs >= 0) {
+      if (max_in_param > max_occurs) {
+        ARCANE_FATAL("Max in param > max_occurs");
+      }
+    }
+  }
+
+  if (max_occurs >= 0) {
+    if (size > max_occurs) {
+      ARCANE_FATAL("Nb in XmlNodeList > max_occurs");
+    }
+  }
+
+  Integer final_size = std::max(size, std::max(min_occurs, max_in_param));
+
+  if (is_phase1) {
+    _allocate(final_size);
+    m_values.resize(final_size);
   }
   else{
     //cerr << "** MULTI SEARCH " << size << endl;
-    for( Integer i=0; i<size; ++i ){
-      XmlNode velem = elem_list[i];
-      // Si l'option n'est pas présente dans le jeu de donnée, on prend
-      // l'option par défaut.
-      String str_val = (velem.null()) ? _defaultValue() : velem.value();
+    for (Integer i = 0; i < final_size; ++i) {
+      String str_val;
+
+      if (option_in_param.contains(i + 1)) {
+        str_val = pco.getParameterOrNull(String::format("{0}/{1}", rootElement().xpathFullName(), name()), i + 1, false);
+      }
+      else if (i < size) {
+        XmlNode velem = elem_list[i];
+        if (!velem.null()) {
+          str_val = velem.value();
+        }
+      }
+
+      if (str_val.null()) {
+        str_val = _defaultValue();
+      }
+      else {
+        // Dans un else : Le remplacement de symboles ne s'applique pas pour les valeurs par défault du .axl.
+        str_val = StringVariableReplace::replaceWithCmdLineArgs(params, str_val, true);
+      }
+
       if (str_val.null()) {
         CaseOptionError::addOptionNotFoundError(caseDocumentFragment(),A_FUNCINFO,
                                                 name(),rootElement());
