5a6ce26633980ead1d08e75c130ecf89b4da64c7
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-02-25 20:03:47 +0100

    [arcane] Supprime fichier d'en-tête inutile dans 'ArrayView.h'.

--- arcane/src/arcane/std/MetisGraphDigest.cc
+++ arcane/src/arcane/std/MetisGraphDigest.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MetisGraphDigest.cc                                         (C) 2000-2024 */
+/* MetisGraphDigest.cc                                         (C) 2000-2025 */
 /*                                                                           */
 /* Calcule une somme de contrôle globale des entrées/sorties Metis.          */
 /*---------------------------------------------------------------------------*/
@@ -13,6 +13,7 @@
 
 #include "arcane/utils/MD5HashAlgorithm.h"
 #include "arcane/utils/FatalErrorException.h"
+#include "arcane/utils/ValueConvert.h"
 
 #include "arcane/core/IParallelMng.h"
 
