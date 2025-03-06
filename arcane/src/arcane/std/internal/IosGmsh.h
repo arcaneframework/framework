3cc7657785a97b2f94e3f9cac6a8a1328ad2f49e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-03-06 20:36:56 +0100

    [arcane,std] Première version d'un écrivain de maillage au format MSH 4.1.

--- arcane/src/arcane/std/internal/IosGmsh.h
+++ arcane/src/arcane/std/internal/IosGmsh.h
@@ -1,6 +1,6 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
@@ -8,6 +8,8 @@
 #define ARCANE_STD_INTERNAL_IOSGMSH_H
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
+
+#include "arcane/core/IMeshReader.h"
 
 #define MSH_LIN_2 1 //1 2-node line.
 #define MSH_TRI_3 2 //2 3-node triangle.
