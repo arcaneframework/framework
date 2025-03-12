f52b3c017ecf32fdba2662b467ea71a8322160a1
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-03-12 19:45:59 +0100

    [arcane,core] S'assure que le groupe est bien recalculé lorsqu'on appelle 'ItemGroup::checkIsSorted()'.

--- arcane/src/arcane/core/ItemGroup.cc
+++ arcane/src/arcane/core/ItemGroup.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ItemGroup.cc                                                (C) 2000-2024 */
+/* ItemGroup.cc                                                (C) 2000-2025 */
 /*                                                                           */
 /* Groupes d'entités du maillage.                                            */
 /*---------------------------------------------------------------------------*/
@@ -653,6 +653,7 @@ _internalApi() const
 bool ItemGroup::
 checkIsSorted() const
 {
+  m_impl->_checkNeedUpdate(false);
   return m_impl->checkIsSorted();
 }
 
