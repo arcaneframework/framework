d0b4c9db57d5307d35da85ab0ac89f2c350a200d
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-03-20 16:00:32 +0100

    [arcane:cartesianmesh] Update headers

--- arcane/src/arcane/cartesianmesh/ICartesianMeshNumberingMng.h
+++ arcane/src/arcane/cartesianmesh/ICartesianMeshNumberingMng.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ICartesianMeshNumberingMng.h                                (C) 2000-2024 */
+/* ICartesianMeshNumberingMng.h                                (C) 2000-2025 */
 /*                                                                           */
 /* Interface de gestionnaire de numérotation pour maillage cartesian.        */
 /* Dans ces gestionnaires, on considère que l'on a un intervalle des         */
--- arcane/src/arcane/cartesianmesh/internal/CartesianMeshPatch.h
+++ arcane/src/arcane/cartesianmesh/internal/CartesianMeshPatch.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* CartesianMeshPatch.h                                        (C) 2000-2023 */
+/* CartesianMeshPatch.h                                        (C) 2000-2025 */
 /*                                                                           */
 /* Informations sur un patch AMR d'un maillage cartésien.                    */
 /*---------------------------------------------------------------------------*/
