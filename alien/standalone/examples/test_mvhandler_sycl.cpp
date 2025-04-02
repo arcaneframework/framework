5ba801c16e99f0771013b46e091efe37e12bf988
sdcm <stephane.de-chaisemartin@ifpen.fr>
2025-04-02 16:16:52 +0200

    [alien,standalone,ArcaneInterface] Update copyrights.

--- alien/standalone/examples/test_mvhandler_sycl.cpp
+++ alien/standalone/examples/test_mvhandler_sycl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 IFPEN-CEA
+ * Copyright 2025 IFPEN-CEA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
