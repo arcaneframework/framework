be4df69819766d25ae0ce9d86cc6301ec5d3804a
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-04-15 14:14:58 +0200

    [arcane,utils] Ajoute itérateur non 'const' pour la classe 'List'.

--- arcane/src/arcane/utils/List.h
+++ arcane/src/arcane/utils/List.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* List.h                                                      (C) 2000-2022 */
+/* List.h                                                      (C) 2000-2025 */
 /*                                                                           */
 /* Classe collection tableau.                                                */
 /*---------------------------------------------------------------------------*/
@@ -294,6 +294,8 @@ class List
 
   const_iterator begin() const { return _cast().begin(); }
   const_iterator end() const { return _cast().end(); }
+  iterator begin() { return _cast().begin(); }
+  iterator end() { return _cast().end(); }
 
   T* begin2() const { return _cast().begin2(); }
   T* end2() const { return _cast().end2(); }
