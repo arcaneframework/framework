64d118ec820478366c0328a04336b3c1100b968a
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-06-05 11:03:42 +0200

    [arcane,core] Corrige copyright.

--- arcane/src/arcane/core/ItemGroupImpl.h
+++ arcane/src/arcane/core/ItemGroupImpl.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ItemGroupImpl.h                                             (C) 2000-2024 */
+/* ItemGroupImpl.h                                             (C) 2000-2025 */
 /*                                                                           */
 /* Implémentation d'un groupe d'entités du maillage.                         */
 /*---------------------------------------------------------------------------*/
