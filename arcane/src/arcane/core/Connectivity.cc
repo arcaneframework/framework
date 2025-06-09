12341cfaccde82c27f8ad80348207cb1e7972a4a
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-06-09 11:35:12 +0200

    [arcane,core] Corrige copyright.

--- arcane/src/arcane/core/Connectivity.cc
+++ arcane/src/arcane/core/Connectivity.cc
@@ -1,18 +1,18 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* DynamicMesh.cc                                              (C) 2000-2024 */
+/* DynamicMesh.cc                                              (C) 2000-2025 */
 /*                                                                           */
 /* Classe de description de la connectivité du maillage.                     */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#include "arcane/Connectivity.h"
-#include "arcane/IMesh.h"
+#include "arcane/core/Connectivity.h"
+#include "arcane/core/IMesh.h"
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
