245605597e3fba8caa0dd519dbb08193940285bc
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2025-06-23 15:44:54 +0200

    set bomb encodong

--- alien/ArcaneInterface/modules/ifpen_solvers/src/alien/kernels/hts/eigen_solver/HTSInternalEigenSolver.cc
+++ alien/ArcaneInterface/modules/ifpen_solvers/src/alien/kernels/hts/eigen_solver/HTSInternalEigenSolver.cc
@@ -1,4 +1,4 @@
-// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
 // Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
