eb8e86f5ed14b7047909ae57e9b3a0d7378ad4a1
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-07-15 16:05:02 +0200

    [arcane:core,impl] Add a IMachineMemoryWindowBase implementation in the SequentialParallelMng (SequentialMachineMemoryWindowBase)

--- arcane/src/arcane/core/internal/ParallelMngInternal.cc
+++ arcane/src/arcane/core/internal/ParallelMngInternal.cc
@@ -109,8 +109,8 @@ createMachineMemoryWindowBase(Integer nb_elem_local, Integer sizeof_one_elem)
 {
   ARCANE_UNUSED(nb_elem_local);
   ARCANE_UNUSED(sizeof_one_elem);
-  ARCANE_NOT_YET_IMPLEMENTED("MachineWindows are not available in SequentialParallelMng, HybridParallelMng and SharedMemoryParallelMng");
-  ARCANE_FATAL("MachineWindows are available only with MpiParallelMng");
+  ARCANE_NOT_YET_IMPLEMENTED("MachineWindows are not available in HybridParallelMng and SharedMemoryParallelMng");
+  ARCANE_FATAL("MachineWindows are available only with MpiParallelMng and SequentialParallelMng");
 }
 
 /*---------------------------------------------------------------------------*/
