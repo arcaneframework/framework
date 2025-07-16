33d94b09d3f8792cc1cae788d7b39c4cb9e3d763
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-07-16 16:00:25 +0200

    [arccore:message_passing] Add forward declaration of IMachineMemoryWindowBase in MessagePassingGlobal.h and include IMachineMemoryWindowBase.h in MessagePassingGlobal.cc

--- arccore/src/message_passing/arccore/message_passing/MessagePassingGlobal.cc
+++ arccore/src/message_passing/arccore/message_passing/MessagePassingGlobal.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MessagePassingGlobal.cc                                     (C) 2000-2024 */
+/* MessagePassingGlobal.cc                                     (C) 2000-2025 */
 /*                                                                           */
 /* Définitions globales de la composante 'message_passing' de 'Arccore'.     */
 /*---------------------------------------------------------------------------*/
@@ -22,6 +22,7 @@
 #include "arccore/message_passing/ISerializeMessageList.h"
 #include "arccore/message_passing/ISerializeDispatcher.h"
 #include "arccore/message_passing/IProfiler.h"
+#include "arccore/message_passing/IMachineMemoryWindowBase.h"
 #include "arccore/message_passing/internal/SubRequestCompletionInfo.h"
 
 /*---------------------------------------------------------------------------*/
