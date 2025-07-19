eae159d9d91d9eec567c354f1be67e7f4397812d
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-07-19 12:04:15 +0200

    [arcane,config] Ajoute 'Findzstd.cmake' pour trouver le package 'zstd'.

---
+++ arcane/cmake/Modules/Findzstd.cmake
@@ -0,0 +1,34 @@
+﻿#
+# Find the 'zstd' includes and library
+#
+# This module defines
+# zstd_INCLUDE_DIR, where to find headers,
+# zstd_LIBRARIES, the libraries to link against to use zstd.
+# zstd_FOUND, If false, do not try to use zstd.
+
+arccon_return_if_package_found(zstd)
+
+# TODO: utiliser pkg_check_module()
+
+# Il n'y a pas de find_package correspondant à 'zstd' dans 'CMake'
+find_library(zstd_LIBRARY zstd)
+find_path(zstd_INCLUDE_DIR zstd.h)
+
+message(STATUS "zstd_INCLUDE_DIR = ${zstd_INCLUDE_DIR}")
+message(STATUS "zstd_LIBRARY     = ${zstd_LIBRARY}")
+
+set(zstd_FOUND FALSE)
+if(zstd_INCLUDE_DIR AND zstd_LIBRARY)
+  set(zstd_FOUND TRUE)
+  set(zstd_LIBRARIES ${zstd_LIBRARY} )
+  set(zstd_INCLUDE_DIRS ${zstd_INCLUDE_DIR})
+endif()
+
+arccon_register_package_library(zstd zstd)
+
+# ----------------------------------------------------------------------------
+# Local Variables:
+# tab-width: 2
+# indent-tabs-mode: nil
+# coding: utf-8-with-signature
+# End:
