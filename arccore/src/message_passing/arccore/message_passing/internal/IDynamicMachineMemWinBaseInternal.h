0ca838843e4ea7e6bc3934f0e121838db9b91435
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-07-25 12:51:39 +0200

    [arccore:message_passing,message_passing_mpi] Add new class MpiDynamicMachineMemWinBaseInternal to create and manage dynamic memory window

---
+++ arccore/src/message_passing/arccore/message_passing/internal/IDynamicMachineMemWinBaseInternal.h
@@ -0,0 +1,64 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* IDynamicMachineMemWinBaseInternal.h                          (C) 2000-2025 */
+/*                                                                           */
+/* TODO.                        */
+/*---------------------------------------------------------------------------*/
+
+#ifndef ARCCORE_MESSAGEPASSING_INTERNAL_IDYNAMICMACHINEMEMWINBASEINTERNAL_H
+#define ARCCORE_MESSAGEPASSING_INTERNAL_IDYNAMICMACHINEMEMWINBASEINTERNAL_H
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/message_passing/MessagePassingGlobal.h"
+#include "arccore/collections/Array.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arcane::MessagePassing
+{
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+class ARCCORE_MESSAGEPASSING_EXPORT IDynamicMachineMemWinBaseInternal
+{
+ public:
+
+  virtual ~IDynamicMachineMemWinBaseInternal() = default;
+
+ public:
+
+  virtual Int32 sizeofOneElem() const = 0;
+
+  virtual Span<std::byte> segment() const = 0;
+  virtual Span<std::byte> segment(Int32 rank) const = 0;
+
+  virtual void add(Span<const std::byte> elem) = 0;
+
+  virtual void exchangeSegmentWith(Int32 rank) = 0;
+
+  virtual ConstArrayView<Int32> machineRanks() const = 0;
+  virtual void syncAdd() = 0;
+  virtual void barrier() = 0;
+
+  virtual void reserve(Int64 new_capacity) = 0;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // End namespace Arccore::MessagePassing
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif  
+
---
+++ arccore/src/message_passing_mpi/arccore/message_passing_mpi/internal/MpiDynamicMachineMemWinBaseInternal.h
@@ -0,0 +1,97 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* MpiDynamicMachineMemWinBaseInternal.h                        (C) 2000-2025 */
+/*                                                                           */
+/* TODO                                                          */
+/*---------------------------------------------------------------------------*/
+
+#ifndef ARCCORE_MESSAGEPASSINGMPI_INTERNAL_MPIDYNAMICMACHINEMEMWINBASEINTERNAL_H
+#define ARCCORE_MESSAGEPASSINGMPI_INTERNAL_MPIDYNAMICMACHINEMEMWINBASEINTERNAL_H
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#include "arccore/collections/Array.h"
+#include "arccore/message_passing/internal/IDynamicMachineMemWinBaseInternal.h"
+
+#include "arccore/message_passing_mpi/MessagePassingMpiGlobal.h"
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+namespace Arcane::MessagePassing::Mpi
+{
+
+class ARCCORE_MESSAGEPASSINGMPI_EXPORT MpiDynamicMachineMemWinBaseInternal
+: public IDynamicMachineMemWinBaseInternal
+{
+ public:
+
+  explicit MpiDynamicMachineMemWinBaseInternal(Int64 sizeof_segment, Int32 sizeof_type, const MPI_Comm& comm_machine, Int32 comm_machine_rank, Int32 comm_machine_size, ConstArrayView<Int32> machine_ranks);
+
+  ~MpiDynamicMachineMemWinBaseInternal() override;
+
+ public:
+
+  Int32 sizeofOneElem() const override;
+
+  Span<std::byte> segment() const override;
+  Span<std::byte> segment(Int32 rank) const override;
+
+  void add(Span<const std::byte> elem) override;
+
+  void exchangeSegmentWith(Int32 rank) override;
+
+  ConstArrayView<Int32> machineRanks() const override;
+
+  void syncAdd() override;
+  void barrier() override;
+
+  void reserve(Int64 new_capacity) override;
+
+ private:
+
+  bool _checkNeedRealloc() const;
+  void _reallocBarrier(Int64 new_sizeof);
+  void _realloc(Int64 new_sizeof);
+
+  Int32 _worldToMachine(Int32 world) const;
+
+ private:
+
+  UniqueArray<MPI_Win> m_all_mpi_win;
+  Span<std::byte> m_reserved_part_span;
+
+  MPI_Win m_win_need_resize;
+  Span<bool> m_need_resize;
+
+  MPI_Win m_win_actual_sizeof;
+  Span<Int64> m_sizeof_used_part;
+
+  MPI_Win m_win_owner_segments;
+  Span<Int32> m_owner_segments;
+  Int32 m_owner_segment;
+
+  MPI_Comm m_comm_machine;
+  Int32 m_comm_machine_size;
+  Int32 m_comm_machine_rank;
+
+  Int32 m_sizeof_type;
+
+  ConstArrayView<Int32> m_machine_ranks;
+};
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arcane::MessagePassing::Mpi
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+#endif
