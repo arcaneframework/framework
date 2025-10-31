064e1f48faf9518694560911c73ade37761a50bb
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-10-31 10:56:36 +0100

    [arcane] Fix copyright.

--- arcane/src/arcane/tests/MiniWeatherModule.cc
+++ arcane/src/arcane/tests/MiniWeatherModule.cc
@@ -1,16 +1,15 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* MiniWeatherModule.cc                                        (C) 2000-2021 */
+/* MiniWeatherModule.cc                                        (C) 2000-2025 */
 /*                                                                           */
 /* Module pour la miniapplication MiniWeather.                               */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
-
 
 #include "arcane/core/BasicModule.h"
 #include "arcane/core/ModuleFactory.h"
