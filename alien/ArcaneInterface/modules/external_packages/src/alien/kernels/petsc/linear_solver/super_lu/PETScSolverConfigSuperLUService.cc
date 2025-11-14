9c7507ad81afbc4247f73d43732d2c393d173edf
TUNC Xavier <tuncx@irlin362128.ifp.fr>
2025-11-14 11:39:25 +0100

    Fixed header date

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/linear_solver/super_lu/PETScSolverConfigSuperLUService.cc
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/linear_solver/super_lu/PETScSolverConfigSuperLUService.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* PETScSolverConfigSuperLUService.cc                    (C) 2000-2024       */
+/* PETScSolverConfigSuperLUService.cc                    (C) 2000-2025       */
 /*                                                                           */
 /* SuperLU Solver from PETSc                                                 */
 /*---------------------------------------------------------------------------*/
