692cf8e475d19cbb61066d99b85c32e82fe9668f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-11-14 15:42:32 +0100

    [arcane,accelerator] Add hierarchical parallelism test with several number of blocks and block size.

--- arcane/src/arcane/tests/accelerator/AcceleratorLocalMemoryUnitTest.cc
+++ arcane/src/arcane/tests/accelerator/AcceleratorLocalMemoryUnitTest.cc
@@ -55,10 +55,12 @@ class AcceleratorLocalMemoryUnitTest
  private:
 
   ax::Runner m_runner;
+  RunQueue m_queue;
 
  public:
 
   void _executeTest1();
+  void _doTest(Int32 block_size, Int32 nb_block);
 };
 
 /*---------------------------------------------------------------------------*/
@@ -94,6 +96,7 @@ initializeTest()
 void AcceleratorLocalMemoryUnitTest::
 executeTest()
 {
+  m_queue = makeQueue(m_runner);
   _executeTest1();
 }
 
@@ -103,79 +106,92 @@ executeTest()
 void AcceleratorLocalMemoryUnitTest::
 _executeTest1()
 {
-  auto queue = makeQueue(m_runner);
+  _doTest(32, 149);
+  _doTest(32 * 4, 137);
+  _doTest(32 * 9, 275);
+  _doTest(512, 311);
+  _doTest(1024, 957);
+}
 
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+void AcceleratorLocalMemoryUnitTest::
+_doTest(Int32 block_size, Int32 nb_block)
+{
   // Test simple du parallélisme hiérarchique et de l'utilisation
   // de la mémoire locale.
 
   // Tous les WorkItem d'un groupe incrémentent un compteur
   // en mémoire partagé. Le dernier WorkItem du groupe recopie
-  // ensuite ce tableau en mémoire locale.
+  // ensuite ce tableau en mémoire globale.
 
-  // NOTE: pour l'instant ce test suppose que la taille
-  // d'un WorkGroup est fixée en dur à 256 qui doit aussi être le
-  // nombre de thread par bloc sur accélérateur.
+  // NOTE: sur accélérateur, la taille d'un WorkGroup doit être
+  // un multiple de 32 et inférieur au nombre maximum de thread d'un bloc
+  // (en général 1024).
+  info() << "DO_LOOP2 LocalMemory nb_block=" << nb_block << " block_size=" << block_size;
+
+  auto command = makeCommand(m_queue);
+  ax::LocalMemory<Int64, 33> local_data_int64(command, 33);
+  ax::LocalMemory<Int32> local_data_int32(command, 50);
+  command.addNbThreadPerBlock(block_size);
+  const Int32 out_array_size = nb_block;
+
+  NumArray<Int64, MDDim1> out_array(out_array_size);
+  out_array.fillHost(0);
+  auto out_span = viewInOut(command, out_array);
+
+  Accelerator::Impl::WorkGroupLoopRange loop_range(nb_block, block_size);
+
+  command << RUNCOMMAND_LAUNCH(work_group, loop_range, local_data_int32, local_data_int64)
   {
-    Int32 loop_size = 1024 * 1024;
-    info() << "DO_LOOP2 LocalMemory size=" << loop_size;
-    auto command = makeCommand(queue);
-    ax::LocalMemory<Int64,33> local_data_int64(command, 50);
-    ax::LocalMemory<Int32> local_data_int32(command, 50);
-    const Int32 out_array_size = loop_size / 256;
-    NumArray<Int64, MDDim1> out_array(out_array_size);
-    out_array.fillHost(0);
-    auto out_span = viewInOut(command, out_array);
-    Accelerator::Impl::WorkGroupLoopRange loop_range(loop_size);
-    command << RUNCOMMAND_LAUNCH (work_group, loop_range, local_data_int32, local_data_int64)
-    {
-      auto local_span_int32 = local_data_int32.span();
-      auto local_span_int64 = local_data_int64.span();
-      auto work_item0 = work_group.item0();
-      if (work_item0.rankInGroup() == 0) {
-        local_span_int32.fill(0);
-        local_span_int64.fill(0);
-      }
-
-      work_item0.sync();
-
-      // Sur accélérateur, nbItem() vaut toujours 1.
-      for (Int32 g = 0; g < work_group.nbItem(); ++g) {
-        auto work_item = work_group.item(g);
-        Int32 i = work_item();
-        ax::doAtomicAdd(&local_span_int32[i % local_span_int32.size()], 1);
-        ax::doAtomicAdd(&local_span_int32[i % local_span_int64.size()], 10);
-        if constexpr (work_item.isDevice()) {
-          // Pour tester le 'constexpr' uniquement sur le device
-          auto xy = work_item.x();
-          int xy2 = xy;
-          if (work_item.rankInGroup() == 0)
-            ax::doAtomicAdd(&local_span_int32[0], xy2);
-        }
-      }
-
-      work_item0.sync();
-
-      // Recopie le tableau partagé dans le tableau de sortie.
-      if (work_item0.rankInGroup() == 0) {
-        Int32 group_index = work_item0.groupRank();
-        for (Int32 s : local_span_int32)
-          out_span[group_index] += s;
-        for (Int64 s : local_span_int64)
-          out_span[group_index] += s;
-      }
-    };
-
-    bool is_accelerator = queue.isAcceleratorPolicy();
-    for (Int32 i = 0, n = out_array_size; i < n; ++i) {
-      Int64 out_value = out_span[i];
-      const Int32 base_value = 256 + 256*10;
-      // Sur accélérateur on ajoute 2 car il y a un ajout dans le 'constexpr' de la lambda
-      Int64 expected_value = (is_accelerator) ? (base_value+2) : base_value;
-      if (i < 10)
-        info() << "DO_LOOP2 LocalMemory out[" << i << "]=" << out_value;
-      if (out_value != expected_value)
-        ARCANE_FATAL("Bad value for index '{0}' expected={1} v={2}", i, expected_value, out_value);
+    auto local_span_int32 = local_data_int32.span();
+    auto local_span_int64 = local_data_int64.span();
+    auto work_item0 = work_group.item0();
+    if (work_item0.rankInGroup() == 0) {
+      local_span_int32.fill(0);
+      local_span_int64.fill(0);
     }
+
+    work_item0.sync();
+
+    // Sur accélérateur, nbItem() vaut toujours 1.
+    for (Int32 g = 0; g < work_group.nbItem(); ++g) {
+      auto work_item = work_group.item(g);
+      Int32 i = work_item();
+      ax::doAtomicAdd(&local_span_int32[i % local_span_int32.size()], 1);
+      ax::doAtomicAdd(&local_span_int32[i % local_span_int64.size()], 10);
+      if constexpr (work_item.isDevice()) {
+        // Pour tester le 'constexpr' uniquement sur le device
+        auto xy = work_item.x();
+        int xy2 = xy;
+        if (work_item.rankInGroup() == 0)
+          ax::doAtomicAdd(&local_span_int32[0], xy2);
+      }
+    }
+
+    work_item0.sync();
+
+    // Recopie le tableau partagé dans le tableau de sortie.
+    if (work_item0.rankInGroup() == 0) {
+      Int32 group_index = work_item0.groupRank();
+      for (Int32 s : local_span_int32)
+        out_span[group_index] += s;
+      for (Int64 s : local_span_int64)
+        out_span[group_index] += s;
+    }
+  };
+
+  bool is_accelerator = m_queue.isAcceleratorPolicy();
+  for (Int32 i = 0, n = out_array_size; i < n; ++i) {
+    Int64 out_value = out_span[i];
+    const Int32 base_value = block_size + block_size * 10;
+    // Sur accélérateur on ajoute 2 car il y a un ajout dans le 'constexpr' de la lambda
+    Int64 expected_value = (is_accelerator) ? (base_value + 2) : base_value;
+    if (i < 10)
+      info() << "DO_LOOP2 LocalMemory out[" << i << "]=" << out_value;
+    if (out_value != expected_value)
+      ARCANE_FATAL("Bad value for index '{0}' expected={1} v={2}", i, expected_value, out_value);
   }
 }
 
