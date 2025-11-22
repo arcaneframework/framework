f466d35a6599a2e58325908bd5e893fa1223a15c
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-11-22 15:25:32 +0100

    [arccore+arccane] Fix copyright.

--- arccore/src/base/tests/TestException.cc
+++ arccore/src/base/tests/TestException.cc
@@ -1,6 +1,6 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
