f231c515184c790be6291549bf517c6b223f57db
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-12-01 10:21:25 +0100

    [arccore+arcane] Fix copyright and missing commited files.

--- arcane/src/arcane/accelerator/Reduce.h
+++ arcane/src/arcane/accelerator/Reduce.h
@@ -1,23 +1,4 @@
-﻿#include "arccore/base/ArrayView.h"
-#include "arccore/base/String.h"
-
-#include "arccore/common/accelerator/IReduceMemoryImpl.h"
-#include "arccore/common/accelerator/RunCommandLaunchInfo.h"
-
-#include "arcane/accelerator/AcceleratorGlobal.h"
-#include "arcane/accelerator/CommonUtils.h"
-#include "arcane/accelerator/RunCommandLoop.h"
-#include "arccore/accelerator/Reduce.h"
-
-#include <limits.h>
-#include <float.h>
-#include <atomic>
-#include <iostream>
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-
+﻿#include "arccore/accelerator/Reduce.h"
 #include "arccore/accelerator/GenericReducer.h"
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
+#include "arcane/accelerator/RunCommandLoop.h"
+#include "arcane/accelerator/CommonUtils.h"
--- arcane/src/arcane/accelerator/RunCommandLoop.h
+++ arcane/src/arcane/accelerator/RunCommandLoop.h
@@ -1,4 +1,5 @@
 ﻿#include "arccore/accelerator/RunCommandLoop.h"
 #include "arcane/accelerator/AcceleratorGlobal.h"
 // Pour compatibilité avec l'existant
+#include "arcane/utils/ForLoopRanges.h"
 #include "arcane/utils/ConcurrencyUtils.h"
