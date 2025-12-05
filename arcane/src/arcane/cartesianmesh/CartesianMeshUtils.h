5228172317a857ec7b2ee5c34a74f222f2f43e87
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2025-12-05 13:39:36 +0100

    [arcane:cartesianmesh,tests] Fix compilation with windows and update headers

--- arcane/src/arcane/cartesianmesh/CartesianMeshUtils.h
+++ arcane/src/arcane/cartesianmesh/CartesianMeshUtils.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* CartesianMeshUtils.h                                        (C) 2000-2024 */
+/* CartesianMeshUtils.h                                        (C) 2000-2025 */
 /*                                                                           */
 /* Fonctions utilitaires associées à 'ICartesianMesh'.                       */
 /*---------------------------------------------------------------------------*/
--- arcane/src/arcane/cartesianmesh/CellDirectionMng.cc
+++ arcane/src/arcane/cartesianmesh/CellDirectionMng.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* CellDirectionMng.cc                                         (C) 2000-2023 */
+/* CellDirectionMng.cc                                         (C) 2000-2025 */
 /*                                                                           */
 /* Infos sur les mailles d'une direction X Y ou Z d'un maillage structuré.   */
 /*---------------------------------------------------------------------------*/
