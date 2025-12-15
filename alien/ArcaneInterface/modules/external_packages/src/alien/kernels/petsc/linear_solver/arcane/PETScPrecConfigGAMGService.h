b9e6174e5056a6307c8668d3e36d9657b44cefac
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2025-12-15 12:35:55 +0100

    bad encoding

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/linear_solver/arcane/PETScPrecConfigGAMGService.h
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/linear_solver/arcane/PETScPrecConfigGAMGService.h
@@ -3,6 +3,8 @@
 // Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+
 #pragma once
 
 #include <alien/kernels/petsc/PETScPrecomp.h>
