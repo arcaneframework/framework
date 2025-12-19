524a234faa385edfc595e6fac18496aec28afd3e
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2025-12-19 16:39:48 +0100

    [arcane,std] Reformat 'HWLocProcessorAffinity.cc' and add support to create instance with dependency injection.

--- arcane/src/arcane/std/HWLocProcessorAffinity.cc
+++ arcane/src/arcane/std/HWLocProcessorAffinity.cc
@@ -1,56 +1,58 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* HWLocProcessorAffinity.cc                                   (C) 2000-2018 */
+/* HWLocProcessorAffinity.cc                                   (C) 2000-2025 */
 /*                                                                           */
 /* Gestion de l'affinité des processeurs via la bibiliothèque HWLOC.         */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#include "arcane/utils/ArcanePrecomp.h"
-
 #include "arcane/utils/IProcessorAffinityService.h"
 #include "arcane/utils/ITraceMng.h"
 #include "arcane/utils/OStringStream.h"
 
-#include "arcane/FactoryService.h"
-#include "arcane/AbstractService.h"
+#include "arcane/core/FactoryService.h"
+#include "arcane/core/AbstractService.h"
 
-extern "C"
-{
+#include "arccore/base/internal/DependencyInjection.h"
+
 #include <hwloc.h>
-}
 
 #include <cstdio>
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_BEGIN_NAMESPACE
+namespace Arcane
+{
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
 class HWLocProcessorAffinityService
-: public AbstractService
+: public TraceAccessor
 , public IProcessorAffinityService
 {
  public:
 
-  HWLocProcessorAffinityService(const ServiceBuildInfo& sbi)
-  : AbstractService(sbi) , m_is_init(false), m_topology(hwloc_topology_t())
+  explicit HWLocProcessorAffinityService(const ServiceBuildInfo& sbi)
+  : TraceAccessor(sbi.application()->traceMng())
+  {
+  }
+  explicit HWLocProcessorAffinityService(ITraceMng* tm)
+  : TraceAccessor(tm)
   {
   }
 
   //<! Libère les ressources
   ~HWLocProcessorAffinityService() override
   {
-    if (m_is_init)
-      hwloc_topology_destroy (m_topology);
+    if (m_topology)
+      hwloc_topology_destroy(m_topology);
   }
 
  public:
@@ -62,23 +64,21 @@ class HWLocProcessorAffinityService
   void printInfos() override;
   String cpuSetString() override;
   void bindThread(Int32 cpu) override;
-  
+
   Int32 numberOfCore() override;
   Int32 numberOfSocket() override;
   Int32 numberOfProcessingUnit() override;
 
  private:
-
  private:
 
-  bool m_is_init;
-  hwloc_topology_t m_topology;
+  bool m_is_init = false;
+  hwloc_topology_t m_topology = nullptr;
 
  private:
 
   void _checkInit();
-  void _outputTopology(hwloc_obj_t l,hwloc_obj_t parent,
-                       int i,OStringStream& ostr);
+  void _outputTopology(hwloc_obj_t l, hwloc_obj_t parent, int i, OStringStream& ostr);
   int _numberOf(const hwloc_obj_type_t);
 };
 
@@ -87,16 +87,16 @@ class HWLocProcessorAffinityService
 // Recopie temporaire de 'hwloc_obj_snprintf' qui n'existe plus
 // à partir de la version 2.0 de hwloc
 static int
-_internal_hwloc_obj_snprintf(char *string, size_t size,
-                             hwloc_obj *l, const char *_indexprefix, int verbose)
+_internal_hwloc_obj_snprintf(char* string, size_t size,
+                             hwloc_obj* l, const char* _indexprefix, int verbose)
 {
-  const char *indexprefix = _indexprefix ? _indexprefix : "#";
+  const char* indexprefix = _indexprefix ? _indexprefix : "#";
   char os_index[12] = "";
   char type[64];
   char attr[128];
   int attrlen;
 
-  if (l->os_index != (unsigned) -1) {
+  if (l->os_index != (unsigned)-1) {
     std::snprintf(os_index, 12, "%s%u", indexprefix, l->os_index);
   }
 
@@ -114,28 +114,28 @@ _internal_hwloc_obj_snprintf(char *string, size_t size,
 
 /* Recursively output topology in a console fashion */
 void HWLocProcessorAffinityService::
-_outputTopology(hwloc_obj_t l, hwloc_obj_t parent,int i,OStringStream& ostr)
+_outputTopology(hwloc_obj_t l, hwloc_obj_t parent, int i, OStringStream& ostr)
 {
-  const char * indexprefix = "#";
+  const char* indexprefix = "#";
   char line[512];
   if (parent && parent->arity == 1 && hwloc_bitmap_isequal(l->cpuset, parent->cpuset)) {
     ostr() << " + ";
   }
   else {
-    if (parent){
+    if (parent) {
       info() << ostr.str();
       ostr.reset();
     }
-    for( int z=0; z<2*i; ++ z)
+    for (int z = 0; z < 2 * i; ++z)
       ostr() << ' ';
     i++;
   }
-  _internal_hwloc_obj_snprintf (line, sizeof(line)-1, l, indexprefix, 0);
+  _internal_hwloc_obj_snprintf(line, sizeof(line) - 1, l, indexprefix, 0);
   ostr() << line;
   unsigned int arity = l->arity;
-  if (arity!=0 || (!i && !arity)){
-    for(unsigned int x=0; x<arity; ++x ){
-      _outputTopology (l->children[x], l, i,ostr);
+  if (arity != 0 || (!i && !arity)) {
+    for (unsigned int x = 0; x < arity; ++x) {
+      _outputTopology(l->children[x], l, i, ostr);
     }
   }
 }
@@ -150,20 +150,20 @@ _checkInit()
     return;
   m_is_init = true;
 
-  info() << "Getting hardware architecture.";
-  int err = hwloc_topology_init (&m_topology);
-  if (err!=0){
+  info() << "Getting hardware architecture hwloc_version=" << HWLOC_VERSION;
+  int err = hwloc_topology_init(&m_topology);
+  if (err != 0) {
     info() << "Error in hwloc_topology_init r=" << err;
     return;
   }
   unsigned long flags = 0;
   hwloc_topology_set_flags(m_topology, flags);
-  err = hwloc_topology_load (m_topology);
-  if (err!=0){
+
+  err = hwloc_topology_load(m_topology);
+  if (err != 0) {
     info() << "Error in hwloc_topology_load r=" << err;
     return;
   }
-  
 }
 
 /*---------------------------------------------------------------------------*/
@@ -175,8 +175,8 @@ cpuSetString()
   hwloc_bitmap_t new_cpuset = hwloc_bitmap_alloc();
   hwloc_get_cpubind(m_topology, new_cpuset, 0 | HWLOC_CPUBIND_THREAD);
   char result_s[1024];
-  hwloc_bitmap_snprintf(result_s,sizeof(result_s)-1,new_cpuset);
-  String s{StringView(result_s)};
+  hwloc_bitmap_snprintf(result_s, sizeof(result_s) - 1, new_cpuset);
+  String s{ StringView(result_s) };
   //ostr() << "R=" << result_s << '\n';
   hwloc_bitmap_free(new_cpuset);
   return s;
@@ -190,7 +190,7 @@ printInfos()
 {
   _checkInit();
   OStringStream ostrf;
-  _outputTopology (hwloc_get_root_obj(m_topology), NULL, 0,ostrf);
+  _outputTopology(hwloc_get_root_obj(m_topology), nullptr, 0, ostrf);
   info() << ostrf.str();
   info() << "Cpuset: " << cpuSetString();
 }
@@ -212,8 +212,8 @@ bindThread(Int32 cpu)
     {
       //info() << "CPUSET ITER =" << cpu_iter << " wanted=" << cpu
       //       << " is_set?=" << hwloc_cpuset_isset(new_cpuset, cpu_iter);
-      if (cpu_index==cpu)
-        hwloc_bitmap_only(new_cpuset,cpu_iter);
+      if (cpu_index == cpu)
+        hwloc_bitmap_only(new_cpuset, cpu_iter);
       ++cpu_index;
     }
     hwloc_bitmap_foreach_end();
@@ -224,7 +224,7 @@ bindThread(Int32 cpu)
   // Regarde si bind ok
   hwloc_get_cpubind(m_topology, new_cpuset, 0 | HWLOC_CPUBIND_THREAD);
   char result_s[1024];
-  hwloc_bitmap_snprintf(result_s, sizeof(result_s)-1, new_cpuset);
+  hwloc_bitmap_snprintf(result_s, sizeof(result_s) - 1, new_cpuset);
   //info() << "CPUSET V2=" << result_s;
   hwloc_bitmap_free(new_cpuset);
 }
@@ -232,44 +232,39 @@ bindThread(Int32 cpu)
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-int HWLocProcessorAffinityService::
+Int32 HWLocProcessorAffinityService::
 _numberOf(const hwloc_obj_type_t that)
 {
   _checkInit();
   int depth = hwloc_get_type_depth(m_topology, that);
-  if (depth==HWLOC_TYPE_DEPTH_UNKNOWN)
+  if (depth == HWLOC_TYPE_DEPTH_UNKNOWN)
     return -1;
   int width = hwloc_get_nbobjs_by_depth(m_topology, depth);
   return width;
 }
 
-/******************************************************************************
- * \brief Returns the number of Processing Unit, or (Logical) Processor
- * \return -1 on error
- ******************************************************************************/
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 
-int HWLocProcessorAffinityService::
+Int32 HWLocProcessorAffinityService::
 numberOfCore(void)
 {
   return _numberOf(HWLOC_OBJ_CORE);
 }
 
-/******************************************************************************
- * \brief Returns the number of Processing Unit, or (Logical) Processor
- * \return -1 on error
- ******************************************************************************/
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 
-int HWLocProcessorAffinityService::
+Int32 HWLocProcessorAffinityService::
 numberOfSocket()
 {
   return _numberOf(HWLOC_OBJ_SOCKET);
 }
 
-/******************************************************************************
- * \brief Returns the number of Processing Unit, or (Logical) Processor
- * \return -1 on error
- ******************************************************************************/
-int HWLocProcessorAffinityService::
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+Int32 HWLocProcessorAffinityService::
 numberOfProcessingUnit()
 {
   return _numberOf(HWLOC_OBJ_PU);
@@ -285,7 +280,15 @@ ARCANE_REGISTER_APPLICATION_FACTORY(HWLocProcessorAffinityService,
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-ARCANE_END_NAMESPACE
+ARCANE_DI_REGISTER_PROVIDER(HWLocProcessorAffinityService,
+                            DependencyInjection::ProviderProperty("HWLoc"),
+                            ARCANE_DI_INTERFACES(IProcessorAffinityService),
+                            ARCANE_DI_CONSTRUCTOR(ITraceMng*));
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+} // namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
