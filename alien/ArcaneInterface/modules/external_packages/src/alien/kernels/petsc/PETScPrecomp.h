9022418a6e2809b7bd2d11abdeb08a0927f38f2a
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-01-05 19:07:17 +0100

    bad encoding correction

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/PETScPrecomp.h
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/PETScPrecomp.h
@@ -1,4 +1,11 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 #pragma once
+
 
 #include <alien/utils/Precomp.h>
 
