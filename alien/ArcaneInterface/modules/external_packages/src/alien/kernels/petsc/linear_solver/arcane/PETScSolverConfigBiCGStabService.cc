f53e3d82fed6d6adb07a6a7c0fcdd0018b6fab1f
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-01-07 19:06:07 +0100

    problem prototyp PETSc correction

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/linear_solver/arcane/PETScSolverConfigBiCGStabService.cc
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/linear_solver/arcane/PETScSolverConfigBiCGStabService.cc
@@ -1,4 +1,4 @@
-// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
 // Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
