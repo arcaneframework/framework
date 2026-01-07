a5fc64baddecbd3fce99e0bed3753f44e9bd46a3
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-01-07 14:42:50 +0100

    set bomb

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/algebra/PETScInternalLinearAlgebra.h
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/petsc/algebra/PETScInternalLinearAlgebra.h
@@ -1,11 +1,10 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
-#ifndef ALIEN_KERNELS_PETSC_ALGEBRA_PETSCINTERNALLINEARALGEBRA_H
-#define ALIEN_KERNELS_PETSC_ALGEBRA_PETSCINTERNALLINEARALGEBRA_H
+#pragma once
 
 #include <alien/kernels/petsc/PETScPrecomp.h>
 #include <alien/kernels/petsc/PETScBackEnd.h>
@@ -62,4 +61,3 @@ class ALIEN_EXTERNAL_PACKAGES_EXPORT PETScInternalLinearAlgebra
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#endif /* ALIEN_KERNELS_PETSC_ALGEBRA_PETSCINTERNALLINEARALGEBRA_H */
