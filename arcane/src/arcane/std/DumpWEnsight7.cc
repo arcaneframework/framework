d6cb73d1da46ee2e9db9ccf9b013fce10f29057e
sdcm <stephane.de-chaisemartin@ifpen.fr>
2026-01-15 11:23:38 +0100

    [arcane,std+tests] Update copyright (the year has changed...).

--- arcane/src/arcane/std/DumpWEnsight7.cc
+++ arcane/src/arcane/std/DumpWEnsight7.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* DumpWEnsight7.cc                                            (C) 2000-2025 */
+/* DumpWEnsight7.cc                                            (C) 2000-2026 */
 /*                                                                           */
 /* Exportations des fichiers au format Ensight7 gold.                        */
 /*---------------------------------------------------------------------------*/
