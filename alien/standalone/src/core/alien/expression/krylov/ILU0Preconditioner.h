6058cd14234000957450e04e7ffefc49d97dba48
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-01-16 21:00:32 +0100

    encoging correction

--- alien/standalone/src/core/alien/expression/krylov/ILU0Preconditioner.h
+++ alien/standalone/src/core/alien/expression/krylov/ILU0Preconditioner.h
@@ -1,26 +1,9 @@
-﻿/*
- * Copyright 2020 IFPEN-CEA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-/*
- * ILU0Preconditioner.h
- *
- *  Created on: Sep 20, 2010
- *      Author: gratienj
- */
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 
 #pragma once
 #if defined (ALIEN_USE_EIGEN3) && !defined(ALIEN_USE_SYCL)
