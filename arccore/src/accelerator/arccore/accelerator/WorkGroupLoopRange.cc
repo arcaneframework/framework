9f2742d27c5c4472b2218d47ebba2cb47e11ccec
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2026-01-21 20:25:36 +0100

    [arccore+arcane] Fix copyright.

--- arccore/src/accelerator/arccore/accelerator/WorkGroupLoopRange.cc
+++ arccore/src/accelerator/arccore/accelerator/WorkGroupLoopRange.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* WorkGroupLoopRange.cc                                       (C) 2000-2025 */
+/* WorkGroupLoopRange.cc                                       (C) 2000-2026 */
 /*                                                                           */
 /* Boucle pour le parallélisme hiérarchique.                                 */
 /*---------------------------------------------------------------------------*/
