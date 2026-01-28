e22163457444ef9b54766af74a5a643d6ae6bd1c
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-01-28 15:22:58 +0100

    [arcane:tests] Update copyright

--- arcane/src/arcane/tests/cartesianmesh/CartesianMeshV2TestUtils.h
+++ arcane/src/arcane/tests/cartesianmesh/CartesianMeshV2TestUtils.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* CartesianMeshTestUtils.cc                                   (C) 2000-2021 */
+/* CartesianMeshTestUtils.cc                                   (C) 2000-2026 */
 /*                                                                           */
 /* Fonctions utilitaires pour les tests de 'CartesianMesh' V2.               */
 /*---------------------------------------------------------------------------*/
