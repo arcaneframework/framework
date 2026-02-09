fb17c7e1ae46c0a2b6244d6a5a55588e0a5101af
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-02-09 20:48:29 +0100

    bad header

--- alien/standalone/src/core/alien/utils/ExceptionUtils.h
+++ alien/standalone/src/core/alien/utils/ExceptionUtils.h
@@ -1,9 +1,10 @@
-/*
- * ExceptionUtils.h
- *
- *  Created on: Dec 2, 2021
- *      Author: gratienj
- */
+
+// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
 
 #pragma once
 
