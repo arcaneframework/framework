297803b5a2ef6e5883e48db0ea79997c786f7848
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2026-02-18 10:51:04 +0100

    [arcane,impl] Allow exceptions in destructor of 'ArcaneSimpleExecutor'.

--- arcane/src/arcane/impl/ArcaneSimpleExecutor.cc
+++ arcane/src/arcane/impl/ArcaneSimpleExecutor.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ArcaneSimpleExecutor.cc                                     (C) 2000-2023 */
+/* ArcaneSimpleExecutor.cc                                     (C) 2000-2026 */
 /*                                                                           */
 /* Classe permettant d'exécuter du code directement via Arcane.              */
 /*---------------------------------------------------------------------------*/
@@ -111,7 +111,7 @@ ArcaneSimpleExecutor()
 /*---------------------------------------------------------------------------*/
 
 ArcaneSimpleExecutor::
-~ArcaneSimpleExecutor()
+~ArcaneSimpleExecutor() noexcept(false)
 {
   delete m_p;
 }
