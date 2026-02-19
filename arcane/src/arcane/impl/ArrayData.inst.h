0a2e59ba298d9a6b351164564643399c7d79a90c
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-02-19 12:38:21 +0100

    [arcane:impl] Update header

--- arcane/src/arcane/impl/ArrayData.inst.h
+++ arcane/src/arcane/impl/ArrayData.inst.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ArrayData.inst.h                                            (C) 2000-2025 */
+/* ArrayData.inst.h                                            (C) 2000-2026 */
 /*                                                                           */
 /* Donnée du type 'Array'.                                                   */
 /*---------------------------------------------------------------------------*/
