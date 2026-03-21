e47d54baff4c4ed423e8828827b1621d779ceb7c
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-03-21 14:09:18 +0100

    enable device_id mng with Accelerator::Runner in Hypre plugin

--- alien/ArcaneInterface/modules/external_packages/src/alien/kernels/hypre/HypreBackEnd.h
+++ alien/ArcaneInterface/modules/external_packages/src/alien/kernels/hypre/HypreBackEnd.h
@@ -1,13 +1,9 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
-/*---------------------------------------------------------------------------*/
-/* MTLBackEnd                                                  (C) 2000-2025 */
-/*                                                                           */
-/* Tools for Hypre backend                                                   */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 #ifndef ALIEN_KERNELS_HYPRE_HYPREBACKEND_H
