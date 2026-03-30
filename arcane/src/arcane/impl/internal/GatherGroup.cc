c68044afcae42ce365077409baea72f9aceefa9f
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-03-30 11:22:18 +0200

    [arcane:impl] Update header

--- arcane/src/arcane/impl/internal/GatherGroup.cc
+++ arcane/src/arcane/impl/internal/GatherGroup.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* GatherGroup.cc                                           (C) 2000-2023 */
+/* GatherGroup.cc                                              (C) 2000-2026 */
 /*                                                                           */
 /* Classe permettant de gérer les regroupements de données sur le ou les     */
 /* sous-domaines écrivains.                                                  */
