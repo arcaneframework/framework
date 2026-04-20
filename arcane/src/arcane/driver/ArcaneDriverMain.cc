eac3ce46e96d0b3fca207eb8a2247dfee7a8b6a2
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-04-20 14:42:40 +0200

    [arcane:tests,driver] Update headers

--- arcane/src/arcane/driver/ArcaneDriverMain.cc
+++ arcane/src/arcane/driver/ArcaneDriverMain.cc
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ArcaneDriverMain.cc                                         (C) 2000-2022 */
+/* ArcaneDriverMain.cc                                         (C) 2000-2026 */
 /*                                                                           */
 /* Programme principal par défaut.                                           */
 /*---------------------------------------------------------------------------*/
