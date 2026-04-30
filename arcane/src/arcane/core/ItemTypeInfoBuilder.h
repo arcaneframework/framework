d6aac95c3867763065a99290126eaa62b7b9860f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2026-04-30 14:20:20 +0200

    [arcane,core] Add information in 'ItemTypeInfo' to know if a type is a polygon.

--- arcane/src/arcane/core/ItemTypeInfoBuilder.h
+++ arcane/src/arcane/core/ItemTypeInfoBuilder.h
@@ -1,11 +1,11 @@
 ﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2026 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 /*---------------------------------------------------------------------------*/
-/* ItemTypeInfoBuilder.h                                       (C) 2000-2025 */
+/* ItemTypeInfoBuilder.h                                       (C) 2000-2026 */
 /*                                                                           */
 /* Construction d'un type d'entité du maillage.                              */
 /*---------------------------------------------------------------------------*/
@@ -26,7 +26,6 @@ namespace Arcane
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
-
 /*!
  * \internal
  * \brief Construction des infos d'un type d'entité du maillage.
@@ -198,6 +197,7 @@ class ItemTypeInfoBuilder
 
   void _setNbEdgeAndFace(Integer nb_edge,Integer nb_face);
   void _checkDimension(Int16 dim);
+  void _checkSetIsPolygon();
 };
 
 /*---------------------------------------------------------------------------*/
